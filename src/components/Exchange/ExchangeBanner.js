39365a9457d61e6d2f14521cc79543ce522246f4
Midas Dev <midasdev711@gmail.com>
2021-11-12 18:47:19 -0500

    fix warnings in exchange banner

--- src/components/Exchange/ExchangeBanner.js
+++ src/components/Exchange/ExchangeBanner.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback, useMemo } from 'react'
+import React from 'react'
 
 import './ExchangeBanner.css';
 
@@ -9,7 +9,7 @@ export default function ExchangeBanner(props) {
 
   return (
     <div className="ExchangeBanner">
-      <p className="ExchangeBanner-text">Trade on GMX and win <span className="ExchangeBanner-price">$250.000</span> in prizes! Live until November 30th , <a href="https://medium.com/@gmx.io/gmx-trading-competition-win-250-000-usd-in-prizes-1346504b96f6" target="_blank" className="ExchangeBanner-link">click here</a> to learn more.</p>
+      <p className="ExchangeBanner-text">Trade on GMX and win <span className="ExchangeBanner-price">$250.000</span> in prizes! Live until November 30th , <a href="https://medium.com/@gmx.io/gmx-trading-competition-win-250-000-usd-in-prizes-1346504b96f6" target="_blank" className="ExchangeBanner-link" rel="noreferrer">click here</a> to learn more.</p>
       <span className="ExchangeBanner-close" onClick={e => { hideBanner() }}>
         <svg xmlns="http://www.w3.org/2000/svg" width="11.25" height="11.25" viewBox="0 0 11.25 11.25">
           <path id="ic_close" d="M11-2.565,6.818-6.75,11-10.935a.844.844,0,0,0,0-1.193.844.844,0,0,0-1.193,0L5.625-7.943,1.44-12.128a.844.844,0,0,0-1.193,0,.844.844,0,0,0,0,1.193L4.432-6.75.247-2.565a.844.844,0,0,0,0,1.193.844.844,0,0,0,1.193,0L5.625-5.557,9.81-1.372a.844.844,0,0,0,1.193,0A.844.844,0,0,0,11-2.565Z" transform="translate(0 12.375)" fill="#fff"/>
