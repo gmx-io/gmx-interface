bf3f75e27600996d001697c87fe8847954534e69
Midas Dev <midasdev711@gmail.com>
2021-12-01 07:16:41 -0500

    update footer

--- src/Footer.js
+++ src/Footer.js
@@ -12,23 +12,25 @@ import mediumIcon from './img/ic_medium.svg'
 export default function Footer() {
   return(
     <div className="Footer">
-      <div className="Footer-logo"><img src={logoImg} alt="MetaMask" /></div>
-      <div className="Footer-social-link-block">
-        <a className="App-social-link" href="https://twitter.com/GMX_IO" target="_blank" rel="noopener noreferrer">
-          <img src={twitterIcon} alt="Twitter" />
-        </a>
-        <a className="App-social-link" href="https://medium.com/@gmx.io" target="_blank" rel="noopener noreferrer">
-          <img src={mediumIcon} alt="Medium" />
-        </a>
-        <a className="App-social-link" href="https://github.com/gmx-io" target="_blank" rel="noopener noreferrer">
-          <img src={githubIcon} alt="Github" />
-        </a>
-        <a className="App-social-link" href="https://t.me/GMX_IO" target="_blank" rel="noopener noreferrer">
-          <img src={telegramIcon} alt="Telegram" />
-        </a>
-        <a className="App-social-link" href="https://discord.gg/cxjZYR4gQK" target="_blank" rel="noopener noreferrer">
-          <img src={discordIcon} alt="Discord" />
-        </a>
+      <div className="Footer-wrapper">
+        <div className="Footer-logo"><img src={logoImg} alt="MetaMask" /></div>
+        <div className="Footer-social-link-block">
+          <a className="App-social-link" href="https://twitter.com/GMX_IO" target="_blank" rel="noopener noreferrer">
+            <img src={twitterIcon} alt="Twitter" />
+          </a>
+          <a className="App-social-link" href="https://medium.com/@gmx.io" target="_blank" rel="noopener noreferrer">
+            <img src={mediumIcon} alt="Medium" />
+          </a>
+          <a className="App-social-link" href="https://github.com/gmx-io" target="_blank" rel="noopener noreferrer">
+            <img src={githubIcon} alt="Github" />
+          </a>
+          <a className="App-social-link" href="https://t.me/GMX_IO" target="_blank" rel="noopener noreferrer">
+            <img src={telegramIcon} alt="Telegram" />
+          </a>
+          <a className="App-social-link" href="https://discord.gg/cxjZYR4gQK" target="_blank" rel="noopener noreferrer">
+            <img src={discordIcon} alt="Discord" />
+          </a>
+        </div>
       </div>
     </div>
   )
