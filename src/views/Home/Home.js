d02311204a685564ec3d9b8ea7f7a6ab8b416714
Midas Dev <midasdev711@gmail.com>
2021-12-03 13:22:44 -0500

    remove warning

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Footer from "../../Footer"
 import { Link, NavLink } from 'react-router-dom'
 // import { FiPlus, FiMinus } from "react-icons/fi"
