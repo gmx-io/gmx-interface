d87afad1ffbe31321165a4e9fea1a06ec258a8eb
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2021-12-29 00:46:16 +0300

    remove sentry

--- src/index.js
+++ src/index.js
@@ -11,3 +11,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.info))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
