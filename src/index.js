da172ce51a223cc18c8448c07dd6d6dbf401f244
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2021-12-29 00:45:16 +0300

    remove sentry

--- src/index.js
+++ src/index.js
@@ -4,26 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import * as Sentry from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
-
-Sentry.init({
-  dsn: "https://4030b752292e4ac2b9cb6d0bb2f0e58b@o821372.ingest.sentry.io/5809462",
-  integrations: [new Integrations.BrowserTracing()],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-
-  beforeSend: (event) => {
-    if (window.location.hostname === 'localhost') {
-      return null;
-    }
-    return event;
-  },
-});
-
 
 ReactDOM.render(
   <React.StrictMode>
@@ -31,8 +11,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.info))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
