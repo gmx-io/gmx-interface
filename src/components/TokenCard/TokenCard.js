c2f2722d2e8d3d3b64c8fa03975a4f27daa2ab5e
Midas Dev <midasdev711@gmail.com>
2022-01-18 05:17:29 -0500

    update layout

---
+++ src/components/TokenCard/TokenCard.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import gmxBigIcon from '../../img/ic_gmx_custom.svg'
+import glpBigIcon from '../../img/ic_glp_custom.svg'
+
+import {
+  ARBITRUM,
+  AVALANCHE,
+} from '../../Helpers'
+
+import APRLabel from '../APRLabel/APRLabel'
+
+export default function TokenCard() {
+  return (
+    <div className="Home-token-card-options">
+      <div className="Home-token-card-option">
+        <div className="Home-token-card-option-icon">
+          <img src={gmxBigIcon} alt="gmxBigIcon" /> GMX
+        </div>
+        <div className="Home-token-card-option-info">
+          <div className="Home-token-card-option-title">GMX is the utility and governance token, and also accrues 30% of the platform's generated fees.</div>
+          <div className="Home-token-card-option-apr">Arbitrum APR: <APRLabel chainId={ARBITRUM} label="gmxAprTotal" />, Avalanche APR: <APRLabel chainId={AVALANCHE} label="gmxAprTotal" key="AVALANCHE" /></div>
+          <div className="Home-token-card-option-action">
+            <Link to="/buy_gmx" className="default-btn buy">Buy</Link>
+            <Link to="/earn" className="default-btn">Stake</Link>
+            <a href="https://gmxio.gitbook.io/gmx/tokenomics" target="_blank" rel="noreferrer" className="default-btn read-more">Read more</a>
+          </div>
+        </div>
+      </div>
+      <div className="Home-token-card-option">
+        <div className="Home-token-card-option-icon">
+          <img src={glpBigIcon} alt="glpBigIcon" /> GLP
+        </div>
+        <div className="Home-token-card-option-info">
+          <div className="Home-token-card-option-title">GLP is the platform's liquidity provider token. Accrues 70% of its generated fees.</div>
+          <div className="Home-token-card-option-apr">Arbitrum APR: <APRLabel chainId={ARBITRUM} label="glpAprTotal" key="ARBITRUM" />, Avalanche APR: <APRLabel chainId={AVALANCHE} label="glpAprTotal" key="AVALANCHE" /></div>
+          <div className="Home-token-card-option-action">
+            <Link to="/buy_glp" className="default-btn buy">Buy</Link>
+            <Link to="/earn" className="default-btn">Stake</Link>
+            <a href="https://gmxio.gitbook.io/gmx/glp" target="_blank" rel="noreferrer" className="default-btn read-more">Read more</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/views/SellGlp/SellGlp.js
+++ src/views/SellGlp/SellGlp.js
@@ -1,10 +1,30 @@
 import React from 'react'
 
 import GlpSwap from '../../components/Glp/GlpSwap'
+import statsBigIcon from '../../img/ic_stats_big.svg'
+import Footer from "../../Footer"
 
 export default function SellGlp(props) {
-  return (<GlpSwap
-    {...props}
-    isBuying={false}
-  />)
+  return (
+    <div className="Page page-layout">
+      <div className="section-title-block">
+        <div className="section-title-icon">
+          <img src={statsBigIcon} alt="statsBigIcon" />
+        </div>
+        <div className="section-title-content">
+          <div className="section-title-content__title">
+            Buy GMX or GLP
+          </div>
+          <div className="section-title-content__description">
+            Total Stats start from 01 Sep 2021. For detailed stats: <a href="https://stats.gmx.io/" target="_blank" rel="noopener noreferrer">https://stats.gmx.io</a>.
+          </div>
+        </div>
+      </div>
+      <GlpSwap
+        {...props}
+        isBuying={false}
+      />
+      <Footer />
+    </div>
+  )
 }
