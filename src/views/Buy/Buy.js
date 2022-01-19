6ef7c6b1912d962e0f89fc0adf5b0b7250409245
Midas Dev <midasdev711@gmail.com>
2022-01-19 06:19:32 -0500

    fix issues in buy pages

--- src/views/Buy/Buy.js
+++ src/views/Buy/Buy.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import Footer from "../../Footer"
 import './Buy.css';
 import TokenCard from '../../components/TokenCard/TokenCard'
-import statsBigIcon from '../../img/ic_stats_big.svg'
+import buyGMXIcon from '../../img/buy_gmx.svg'
 
 export default function BuyGMXGLP(props) {
   useEffect(() => {
@@ -14,7 +14,7 @@ export default function BuyGMXGLP(props) {
       <div className="BuyGMXGLP-container default-container">
         <div className="section-title-block">
           <div className="section-title-icon">
-            <img src={statsBigIcon} alt="statsBigIcon" />
+            <img src={buyGMXIcon} alt="buyGMXIcon" />
           </div>
           <div className="section-title-content">
             <div className="section-title-content__title">
