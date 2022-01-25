a911c873a71179da8508df8dbc6464a50a9a48fd
Phi <phi@lattefi.app>
2022-01-25 17:49:19 +0800

    update links

--- src/views/Stake/StakeV2.js
+++ src/views/Stake/StakeV2.js
@@ -1289,7 +1289,7 @@ export default function StakeV2({ setPendingTxns, connectWallet }) {
               </div>
               <div className="App-card-divider"></div>
               <div className="App-card-options">
-                <Link className="App-button-option App-card-option" to="/buy">Buy GMX</Link>
+                <Link className="App-button-option App-card-option" to="/buy_gmx">Buy GMX</Link>
                 {active && <button className="App-button-option App-card-option" onClick={() => showStakeGmxModal()}>Stake</button>}
                 {active && <button className="App-button-option App-card-option" onClick={() => showUnstakeGmxModal()}>Unstake</button>}
                 {active && <Link className="App-button-option App-card-option" to="/begin_account_transfer">Transfer Account</Link>}
