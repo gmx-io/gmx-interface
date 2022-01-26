92c07dce636e35dea36737a8fbe084ad00e223ad
Midas Dev <midasdev711@gmail.com>
2022-01-26 07:48:25 -0500

    update sell glp link

--- src/views/SellGlp/SellGlp.js
+++ src/views/SellGlp/SellGlp.js
@@ -1,31 +1,11 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-import GlpSwap from '../../components/Glp/GlpSwap'
-import statsBigIcon from '../../img/ic_stats_big.svg'
-import Footer from "../../Footer"
+import { useEffect } from 'react'
 
 export default function SellGlp(props) {
+  useEffect(() => {
+    window.location.href = '/buy_glp#redeem';
+  }, [])
   return (
     <div className="Page page-layout">
-      <div className="section-title-block">
-        <div className="section-title-icon">
-          <img src={statsBigIcon} alt="statsBigIcon" />
-        </div>
-        <div className="section-title-content">
-          <div className="section-title-content__title">
-            Buy or redeem GLP
-          </div>
-          <div className="section-title-content__description">
-            Purchase GLP tokens to earn AVAX fees from swaps and leverages trading. Note that there is a minimum holding time of 15 minutes after a purchase. View <Link to="/earn">staking</Link> page.
-          </div>
-        </div>
-      </div>
-      <GlpSwap
-        {...props}
-        isBuying={false}
-      />
-      <Footer />
     </div>
   )
 }
