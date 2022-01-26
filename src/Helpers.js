619c41c996477d22ea20065a04a237fad6836ae8
Phi <phi@lattefi.app>
2022-01-26 15:15:53 +0800

    update rpc

--- src/Helpers.js
+++ src/Helpers.js
@@ -40,7 +40,7 @@ const CHAIN_NAMES_MAP = {
 }
 
 const ARBITRUM_RPC_PROVIDERS = [
-  "https://arb-mainnet.g.alchemy.com/v2/ha7CFsr1bx5ZItuR6VZBbhKozcKDY4LZ"
+  "https://rpc.ankr.com/arbitrum"
 ]
 const AVALANCHE_RPC_PROVIDERS = [
   "https://api.avax.network/ext/bc/C/rpc"
