428b014d7a570800a0557d0b90291cb0572b1e0d
Midas Dev <midasdev711@gmail.com>
2022-01-27 13:09:18 -0500

    update token card

--- src/components/TokenCard/TokenCard.js
+++ src/components/TokenCard/TokenCard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { Link } from 'react-router-dom'
 
 import gmxBigIcon from '../../img/ic_gmx_custom.svg'
@@ -7,11 +7,31 @@ import glpBigIcon from '../../img/ic_glp_custom.svg'
 import {
   ARBITRUM,
   AVALANCHE,
+  switchNetwork,
+  useChainId
 } from '../../Helpers'
+
+import { useWeb3React } from '@web3-react/core'
 
 import APRLabel from '../APRLabel/APRLabel'
 
 export default function TokenCard() {
+  const { chainId } = useChainId()
+  const { active } = useWeb3React()
+
+  const changeNetwork = useCallback(network => {
+    if (network === chainId) {
+      return
+    }
+    if (!active) {
+      setTimeout(() => {
+        return switchNetwork(network, active)
+      }, 500)
+    } else {
+      return switchNetwork(network, active)
+    }
+  }, [chainId, active])
+
   return (
     <div className="Home-token-card-options">
       <div className="Home-token-card-option">
@@ -22,8 +42,9 @@ export default function TokenCard() {
           <div className="Home-token-card-option-title">GMX is the utility and governance token, and also accrues 30% of the platform's generated fees.</div>
           <div className="Home-token-card-option-apr">Arbitrum APR: <APRLabel chainId={ARBITRUM} label="gmxAprTotal" />, Avalanche APR: <APRLabel chainId={AVALANCHE} label="gmxAprTotal" key="AVALANCHE" /></div>
           <div className="Home-token-card-option-action">
-            <Link to="/buy_gmx" className="default-btn buy">Buy</Link>
-            <a href="https://gmxio.gitbook.io/gmx/tokenomics" target="_blank" rel="noreferrer" className="default-btn">Read more</a>
+            <Link to="/buy_gmx" className="default-btn buy" onClick={() => changeNetwork(ARBITRUM)}>Buy on Arbitrum</Link>
+            <Link to="/buy_gmx" className="default-btn buy" onClick={() => changeNetwork(AVALANCHE)}>Buy on Avalanche</Link>
+            <a href="https://gmxio.gitbook.io/gmx/tokenomics" target="_blank" rel="noreferrer" className="default-btn read-more">Read more</a>
           </div>
         </div>
       </div>
@@ -35,8 +56,9 @@ export default function TokenCard() {
           <div className="Home-token-card-option-title">GLP is the platform's liquidity provider token. Accrues 70% of its generated fees.</div>
           <div className="Home-token-card-option-apr">Arbitrum APR: <APRLabel chainId={ARBITRUM} label="glpAprTotal" key="ARBITRUM" />, Avalanche APR: <APRLabel chainId={AVALANCHE} label="glpAprTotal" key="AVALANCHE" /></div>
           <div className="Home-token-card-option-action">
-            <Link to="/buy_glp" className="default-btn buy">Buy</Link>
-            <a href="https://gmxio.gitbook.io/gmx/glp" target="_blank" rel="noreferrer" className="default-btn">Read more</a>
+            <Link to="/buy_glp" className="default-btn buy" onClick={() => changeNetwork(ARBITRUM)}>Buy on Arbitrum</Link>
+            <Link to="/buy_glp" className="default-btn buy" onClick={() => changeNetwork(AVALANCHE)}>Buy on Avalanche</Link>
+            <a href="https://gmxio.gitbook.io/gmx/glp" target="_blank" rel="noreferrer" className="default-btn read-more">Read more</a>
           </div>
         </div>
       </div>
