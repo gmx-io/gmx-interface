744d8918f143804c8c3f549fc77dfc3e127c1424
Phi <phi@lattefi.app>
2022-01-28 17:26:39 +0800

    update order min price movement text

--- src/components/Exchange/PositionSeller.js
+++ src/components/Exchange/PositionSeller.js
@@ -500,7 +500,7 @@ export default function PositionSeller(props) {
             profit
           </a> of {deltaStr}. <br/>
           Profit price: {position.isLong ? ">" : "<"} ${formatAmount(profitPrice, USD_DECIMALS, 2, true)}.
-          This rule only applies for the next {getTimeRemaining(minProfitExpiration)}, until {formatDateTime(minProfitExpiration)}.
+          This rule applies for the next {getTimeRemaining(minProfitExpiration)}, until {formatDateTime(minProfitExpiration)}.
         </div>
       )
     }
