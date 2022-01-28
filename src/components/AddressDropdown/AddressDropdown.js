f90e45b950d6de10037d5a6ccce2ba25dc234e51
vipin <vipineth@gmail.com>
2022-01-29 00:43:54 +0530

    add address dropdown

---
+++ src/components/AddressDropdown/AddressDropdown.js
@@ -0,0 +1,62 @@
+import "./AddressDropdown.css";
+import { Menu, Transition } from "@headlessui/react";
+import { shortenAddress } from "../../Helpers";
+import { FiCopy, FiExternalLink } from "react-icons/fi";
+import { BiLogIn } from "react-icons/bi";
+import { useCopyToClipboard } from "react-use";
+
+function AddressDropdown({
+  account,
+  small,
+  accountUrl,
+  disconnectAccountAndCloseSettings
+}) {
+  const [userAddress, copyToClipboard] = useCopyToClipboard();
+  return (
+    <Menu>
+      <Menu.Button as="div">
+        <p className="App-cta small transparent App-header-user-account">
+          {shortenAddress(account, small ? 11 : 13)}
+        </p>
+      </Menu.Button>
+      <Menu.Items as="div" className="menu-items">
+        <Menu.Item>
+          {({ active }) => (
+            <div className="menu-item" onClick={() => copyToClipboard(account)}>
+              <FiCopy fontSize={16} />
+              <p>Copy Address</p>
+            </div>
+          )}
+        </Menu.Item>
+
+        <Menu.Item>
+          {({ active }) => (
+            <a
+              href={accountUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="menu-item"
+            >
+              <FiExternalLink fontSize={16} />
+              <p>View in Explorer</p>
+            </a>
+          )}
+        </Menu.Item>
+
+        <Menu.Item>
+          {({ active }) => (
+            <div
+              className="menu-item"
+              onClick={disconnectAccountAndCloseSettings}
+            >
+              <BiLogIn fontSize={16} />
+              <p>Disconnect</p>
+            </div>
+          )}
+        </Menu.Item>
+      </Menu.Items>
+    </Menu>
+  );
+}
+
+export default AddressDropdown;
