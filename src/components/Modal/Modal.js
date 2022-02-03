2382fa41671ed89a0b6085565ca3cede0c5023da
vipin <vipineth@gmail.com>
2022-02-03 12:29:22 +0530

    add support for escape to close the modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,41 +1,59 @@
-import React from 'react'
+import React, { useEffect } from "react";
 import cx from "classnames";
-import { motion, AnimatePresence } from "framer-motion"
+import { motion, AnimatePresence } from "framer-motion";
 
-import { FaTimes } from 'react-icons/fa'
+import { FaTimes } from "react-icons/fa";
 
-import './Modal.css';
+import "./Modal.css";
 
 export default function Modal(props) {
-  const { isVisible, setIsVisible, className, zIndex } = props
+  const { isVisible, setIsVisible, className, zIndex } = props;
+
+  useEffect(() => {
+    function close(e) {
+      if (e.keyCode === 27) {
+        setIsVisible(false);
+      }
+    }
+    window.addEventListener("keydown", close);
+    return () => window.removeEventListener("keydown", close);
+  }, []);
 
   const fadeVariants = {
     hidden: { opacity: 0 },
     visible: { opacity: 1 }
-  }
+  };
 
   return (
     <AnimatePresence>
-      {isVisible && <motion.div
+      {isVisible && (
+        <motion.div
           className={cx("Modal", className)}
-          style={{zIndex}}
+          style={{ zIndex }}
           initial="hidden"
           animate="visible"
           exit="hidden"
           variants={fadeVariants}
           transition={{ duration: 0.2 }}
         >
-        <div className="Modal-backdrop" onClick={() => setIsVisible(false)}></div>
-        <div className="Modal-content App-box-solid">
-          <div className="Modal-title-bar">
-            <div className="Modal-title">
-              {props.label}
+          <div
+            className="Modal-backdrop"
+            onClick={() => setIsVisible(false)}
+          ></div>
+          <div className="Modal-content App-box-solid">
+            <div className="Modal-title-bar">
+              <div className="Modal-title">{props.label}</div>
+              <div
+                className="Modal-close-button"
+                onClick={() => setIsVisible(false)}
+              >
+                <FaTimes className="Modal-close-icon" />
+              </div>
             </div>
-            <div className="Modal-close-button" onClick={() => setIsVisible(false)}><FaTimes className="Modal-close-icon" /></div>
+            {props.children}
           </div>
-          {props.children}
-        </div>
-      </motion.div>}
+        </motion.div>
+      )}
     </AnimatePresence>
-  )
+  );
 }
