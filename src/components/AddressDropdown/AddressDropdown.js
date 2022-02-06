680071ff037a9e537db168bf88850da616c6a2c7
vipin <vipineth@gmail.com>
2022-02-06 20:13:11 +0530

    show toast message when address is copied

--- src/components/AddressDropdown/AddressDropdown.js
+++ src/components/AddressDropdown/AddressDropdown.js
@@ -1,6 +1,6 @@
 import "./AddressDropdown.css";
 import { Menu } from "@headlessui/react";
-import { shortenAddress } from "../../Helpers";
+import { helperToast, shortenAddress } from "../../Helpers";
 import { useCopyToClipboard } from "react-use";
 import externalLink from "../../img/ic_new_link_16.svg";
 import copy from "../../img/ic_copy_16.svg";
@@ -27,7 +27,13 @@ function AddressDropdown({
 
       <Menu.Items as="div" className="menu-items">
         <Menu.Item>
-          <div className="menu-item" onClick={() => copyToClipboard(account)}>
+          <div
+            className="menu-item"
+            onClick={() => {
+              copyToClipboard(account);
+              helperToast.success("Address copied to your clipboard");
+            }}
+          >
             <img src={copy} alt="Copy user address" />
             <p>Copy Address</p>
           </div>
