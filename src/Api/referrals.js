262e8f69a01f64d8ef765521f3065ed96fccd917
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2022-04-08 17:45:01 +0300

    add last day stats, encode/decode code, normalize data

--- src/Api/referrals.js
+++ src/Api/referrals.js
@@ -5,6 +5,7 @@ import { useState, useEffect } from "react";
 import {
   ARBITRUM,
   // AVALANCHE,
+  bigNumberify
 } from "../Helpers";
 import {
   arbitrumReferralsGraphClient
@@ -22,11 +23,16 @@ function getGraphClient(chainId) {
 const DISTRIBUTION_TYPE_REBATES = "1"
 const DISTRIBUTION_TYPE_DISCOUNT = "2"
 
-export function decodeReferralCode(bytesCode) {
+export function decodeReferralCode(hexCode) {
   try {
-    ethers.utils.parseBytes32String(bytesCode)
+    ethers.utils.parseBytes32String(hexCode)
   } catch (ex) {
-    return bytesCode
+    let code = ''
+    hexCode = hexCode.substring(2)
+    for (let i = 0; i < 32; i++) {
+      code += String.fromCharCode(parseInt(hexCode.substring(i * 2, i * 2 + 2), 16))
+    }
+    return code.trim()
   }
 }
 
@@ -38,6 +44,7 @@ export function encodeReferralCode(code) {
 }
 
 export function useReferralsData(chainId, account) {
+  const startOfDayTimestamp = Math.floor(parseInt(Date.now() / 1000) / 86400) * 86400
   const query = gql(`{
     distributions(
       first: 1000,
@@ -69,10 +76,26 @@ export function useReferralsData(chainId, account) {
       totalRebateUsd,
       discountUsd
     }
+    lastDayStats: referralVolumeStats(
+      first: 1000
+      where: {
+        period: daily
+        referrer: "__ACCOUNT__"
+        timestamp: __TIMESTAMP__
+      }
+    ) {
+      referralCode,
+      volume,
+      trades,
+      tradedReferralsCount,
+      totalRebateUsd,
+      discountUsd
+    }
   }`
     .replaceAll("__ACCOUNT__", (account || "").toLowerCase())
     .replaceAll("__DISTRIBUTION_TYPE_REBATES__", DISTRIBUTION_TYPE_REBATES)
     .replaceAll("__DISTRIBUTION_TYPE_DISCOUNT__", DISTRIBUTION_TYPE_DISCOUNT)
+    .replaceAll("__TIMESTAMP__", startOfDayTimestamp)
   );
 
   const [data, setData] = useState();
@@ -82,21 +105,37 @@ export function useReferralsData(chainId, account) {
       const rebateDistributions = []
       const discountDistributions = []
       res.data.distributions.forEach(d => {
+        const item = {
+          timestamp: parseInt(d.timestamp),
+          transactionHash: d.transactionHash,
+          receiver: ethers.utils.getAddress(d.receiver),
+          amount: bigNumberify(d.amount),
+          typeId: d.typeId,
+          token: ethers.utils.getAddress(d.token)
+        }
         if (d.typeId === DISTRIBUTION_TYPE_REBATES) {
-          rebateDistributions.push(d)
+          rebateDistributions.push(item)
         } else {
-          discountDistributions.push(d)
+          discountDistributions.push(item)
         }
       })
+
+      function prepareStatsItem(e) {
+        return {
+          volume: bigNumberify(e.volume),
+          trades: parseInt(e.trades),
+          tradedReferralsCount: parseInt(e.tradedReferralsCount),
+          totalRebateUsd: bigNumberify(e.totalRebateUsd),
+          discountUsd: bigNumberify(e.discountUsd),
+          referralCode: decodeReferralCode(e.referralCode)
+        }
+      }
+
       setData({
         rebateDistributions,
         discountDistributions,
-        totalStats: res.data.totalStats.map(e => {
-          return {
-            ...e,
-            referralCode: decodeReferralCode(e.referralCode)
-          }
-        })
+        totalStats: res.data.totalStats.map(prepareStatsItem),
+        lastDayStats: res.data.lastDayStats.map(prepareStatsItem)
       })
     }).catch(console.warn);
   }, [setData, query, chainId]);
