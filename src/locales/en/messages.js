b9d009b7c7c28a97cebaa033c43a9e075848b457
Midas Dev <midasdev711@gmail.com>
2022-04-28 06:39:19 -0400

    integrate lingui/react

---
+++ src/locales/en/messages.js
@@ -0,0 +1,24 @@
+module.exports = {
+  messages: {
+    "Dashboard": "Dashboard",
+    "Earn": "Earn",
+    "Buy": "Buy",
+    "Referrals": "Referrals",
+    "Ecosystem": "Ecosystem",
+    "About": "About",
+    "Settings": "Settings",
+    "HOME": "HOME",
+    "EARN": "EARN",
+    "ABOUT": "ABOUT",
+    "Home": "Home",
+    "Trade": "Trade",
+    "Connect": "Connect",
+    "Connect Wallet": "Connect Wallet",
+    "MetaMask not detected.": "MetaMask not detected.",
+    "Install MetaMask": "Install MetaMask",
+    ", and use GMX with its built-in browser": ", and use GMX with its built-in browser",
+    " to start using GMX": " to start using GMX",
+    "Coinbase Wallet not detected.": "Coinbase Wallet not detected.",
+    "Install Coinbase Wallet": "Install Coinbase Wallet"
+  }
+}
