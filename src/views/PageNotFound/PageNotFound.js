cda5c9919062c61aa61d25efd4cad3952a3e7adb
vipin <vipineth@gmail.com>
2022-05-01 11:40:50 +0530

    add seo in PageNotFound

--- src/views/PageNotFound/PageNotFound.js
+++ src/views/PageNotFound/PageNotFound.js
@@ -1,21 +1,25 @@
 import { Link } from "react-router-dom";
+import SEO from "../../components/Common/SEO";
 import Footer from "../../Footer";
+import { getPageTitle } from "../../Helpers";
 import PageNotFoundImg from "../../img/page-not-found.svg";
 import "./PageNotFound.css";
 
 function PageNotFound() {
   return (
-    <div className="page-layout">
-      <div className="page-not-found">
-        <img src={PageNotFoundImg} alt="Page not found!" />
-        <h2>Oops, page not found!</h2>
-        <p className="go-back">
-          <span>Return to </span>
-          <Link to="/">Homepage</Link> <span>or </span> <Link to="/trade">Trade</Link>
-        </p>
+    <SEO title={getPageTitle("Page not found")}>
+      <div className="page-layout">
+        <div className="page-not-found">
+          <img src={PageNotFoundImg} alt="Page not found!" />
+          <h2>Oops, page not found!</h2>
+          <p className="go-back">
+            <span>Return to </span>
+            <Link to="/">Homepage</Link> <span>or </span> <Link to="/trade">Trade</Link>
+          </p>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </SEO>
   );
 }
 
