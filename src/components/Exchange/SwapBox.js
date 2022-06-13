f35c3b27b1137c0c3bde6aba7d7e258231a89729
Phi <phi@lattefi.app>
2022-06-13 16:48:22 +0800

    fix swapbox

--- src/components/Exchange/SwapBox.js
+++ src/components/Exchange/SwapBox.js
@@ -368,8 +368,6 @@ export default function SwapBox(props) {
     );
   };
 
-  const hasMaxAvailableShort = isShort && toTokenInfo.hasMaxAvailableShort;
-
   const fromBalance = fromTokenInfo ? fromTokenInfo.balance : bigNumberify(0);
   const toBalance = toTokenInfo ? toTokenInfo.balance : bigNumberify(0);
 
@@ -2296,7 +2294,7 @@ export default function SwapBox(props) {
             </div>
           </div>
           {renderAvailableLongLiquidity()}
-          {hasMaxAvailableShort && (
+          {isShort && toTokenInfo.hasMaxAvailableShort && (
             <div className="Exchange-info-row">
               <div className="Exchange-info-label">Available Liquidity</div>
               <div className="align-right">
