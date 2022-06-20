30a8a97fc81c56507ca9055bd963714cc18d879d
vipin <vipineth@gmail.com>
2022-06-20 09:55:25 +0530

    add scroll for smaller screen

--- src/components/Exchange/OrdersToa.js
+++ src/components/Exchange/OrdersToa.js
@@ -35,29 +35,35 @@ export default function OrdersToa(props) {
 
   return (
     <Modal setIsVisible={setIsVisible} isVisible={true} label="Enable Orders" className="Orders-toa" zIndex="1000">
-      Note that orders are not guaranteed to be executed.
-      <br />
-      <br />
-      This can occur in a few situations including but not exclusive to:
-      <br />
-      <ul>
-        <li>Insufficient liquidity to execute the order</li>
-        <li>The mark price which is an aggregate of exchange prices did not reach the specified price</li>
-        <li>The specified price was reached but not long enough for it to be executed</li>
-        <li>No keeper picked up the order for execution</li>
-      </ul>
-      <div>Additionally, trigger orders are market orders and are not guaranteed to settle at the trigger price.</div>
-      <br />
-      <div className="Orders-toa-accept-rules">
-        <Checkbox isChecked={isChecked} setIsChecked={setIsChecked}>
-          <span className="muted">
-            Accept that orders are not guaranteed to execute and trigger orders may not settle at the trigger price
-          </span>
-        </Checkbox>
+      <div>
+        Note that orders are not guaranteed to be executed.
+        <br />
+        <br />
+        This can occur in a few situations including but not exclusive to:
+        <br />
+        <ul>
+          <li>Insufficient liquidity to execute the order</li>
+          <li>The mark price which is an aggregate of exchange prices did not reach the specified price</li>
+          <li>The specified price was reached but not long enough for it to be executed</li>
+          <li>No keeper picked up the order for execution</li>
+        </ul>
+        <div>Additionally, trigger orders are market orders and are not guaranteed to settle at the trigger price.</div>
+        <br />
+        <div className="Orders-toa-accept-rules">
+          <Checkbox isChecked={isChecked} setIsChecked={setIsChecked}>
+            <span className="muted">
+              Accept that orders are not guaranteed to execute and trigger orders may not settle at the trigger price
+            </span>
+          </Checkbox>
+        </div>
+        <button
+          disabled={!isPrimaryEnabled()}
+          className="App-cta Confirmation-box-button"
+          onClick={onConfirmationClick}
+        >
+          {getPrimaryText()}
+        </button>
       </div>
-      <button disabled={!isPrimaryEnabled()} className="App-cta Confirmation-box-button" onClick={onConfirmationClick}>
-        {getPrimaryText()}
-      </button>
     </Modal>
   );
 }
