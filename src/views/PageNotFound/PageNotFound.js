fea9abcb4f6300a32f4c7dd1131496645b59fc81
Phi <phi@lattefi.app>
2022-06-23 10:20:25 +0800

    update page not found page

--- src/views/PageNotFound/PageNotFound.js
+++ src/views/PageNotFound/PageNotFound.js
@@ -2,20 +2,20 @@ import { Link } from "react-router-dom";
 import SEO from "../../components/Common/SEO";
 import Footer from "../../Footer";
 import { getPageTitle } from "../../Helpers";
-import PageNotFoundImg from "../../img/page-not-found.svg";
 import "./PageNotFound.css";
 
 function PageNotFound() {
   return (
     <SEO title={getPageTitle("Page not found")}>
       <div className="page-layout">
-        <div className="page-not-found">
-          <img src={PageNotFoundImg} alt="Page not found!" />
-          <h2>Oops, page not found!</h2>
-          <p className="go-back">
-            <span>Return to </span>
-            <Link to="/">Homepage</Link> <span>or </span> <Link to="/trade">Trade</Link>
-          </p>
+        <div className="page-not-found-container">
+          <div className="page-not-found">
+            <h2>Page not found</h2>
+            <p className="go-back">
+              <span>Return to </span>
+              <Link to="/">Homepage</Link> <span>or </span> <Link to="/trade">Trade</Link>
+            </p>
+          </div>
         </div>
         <Footer />
       </div>
