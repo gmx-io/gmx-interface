d550f3a8c6ee5e6e3d431639d5f2f19262dc899b
vipin <vipineth@gmail.com>
2022-06-28 00:28:06 +0530

    change tweet share message

--- src/components/Referrals/referralsHelper.js
+++ src/components/Referrals/referralsHelper.js
@@ -131,6 +131,6 @@ export function getCodeError(value) {
 
 export function getTwitterShareUrl(referralCode) {
   const message =
-    "Join me on the GMX Odyssey; you just need to mint GLP, spot swap, and open a leverage trade to enter and win a prize!";
+    "Join me on the GMX Odyssey; you just need to mint GLP, spot swap, and open a leveraged trade to enter and win a prize!";
   return `http://twitter.com/share?text=${message}&url=https://gmx.io?ref=${referralCode}&hashtags=GMX`;
 }
