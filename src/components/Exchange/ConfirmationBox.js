4f02f84ceafecd2adbb815525f37548b30dc9ca4
Phi <phi@lattefi.app>
2022-06-29 11:40:43 +0800

    update high fee warning threshold

--- src/components/Exchange/ConfirmationBox.js
+++ src/components/Exchange/ConfirmationBox.js
@@ -253,7 +253,7 @@ export default function ConfirmationBox(props) {
   }, [isMarketOrder, spread]);
 
   const renderFeeWarning = useCallback(() => {
-    if (orderOption === LIMIT || !feeBps || feeBps <= 50) {
+    if (orderOption === LIMIT || !feeBps || feeBps <= 60) {
       return null;
     }
 
