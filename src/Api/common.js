0e57bd64965e58ad712eadd6e106807d6ebe79ca
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2022-07-01 02:06:21 +0300

    use prices subgraphs from gmx-io org

--- src/Api/common.js
+++ src/Api/common.js
@@ -3,11 +3,11 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 export const chainlinkClient = createClient("https://api.thegraph.com/subgraphs/name/deividask/chainlink");
 export const arbitrumGraphClient = createClient("https://api.thegraph.com/subgraphs/name/gmx-io/gmx-stats");
 export const arbitrumPricesGraphClient = createClient(
-  "https://api.thegraph.com/subgraphs/name/gdev8317/gmx-arbitrum-prices"
+  "https://api.thegraph.com/subgraphs/name/gmx-io/gmx-arbitrum-prices"
 );
 export const avalancheGraphClient = createClient("https://api.thegraph.com/subgraphs/name/gmx-io/gmx-avalanche-stats");
 export const avalanchePricesGraphClient = createClient(
-  "https://api.thegraph.com/subgraphs/name/gdev8317/gmx-avalanche-prices"
+  "https://api.thegraph.com/subgraphs/name/gmx-io/gmx-avalanche-prices"
 );
 export const nissohGraphClient = createClient("https://api.thegraph.com/subgraphs/name/nissoh/gmx-vault");
 export const arbitrumReferralsGraphClient = createClient(
