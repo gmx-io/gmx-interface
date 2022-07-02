cdae72c6f30a929cbf347a9716a6bcded170ffb7
vipin <vipineth@gmail.com>
2022-07-02 13:32:14 +0530

    add list of limit orders

--- src/components/Exchange/ConfirmationBox.js
+++ src/components/Exchange/ConfirmationBox.js
@@ -107,6 +107,7 @@ export default function ConfirmationBox(props) {
   const [savedSlippageAmount] = useLocalStorageSerializeKey([chainId, SLIPPAGE_BPS_KEY], DEFAULT_SLIPPAGE_AMOUNT);
   const [isProfitWarningAccepted, setIsProfitWarningAccepted] = useState(false);
   const [isTriggerWarningAccepted, setIsTriggerWarningAccepted] = useState(false);
+  const [isLimitOrdersVisible, setIsLimitOrdersVisible] = useState(false);
 
   let minOut;
   let fromTokenUsd;
@@ -145,6 +146,16 @@ export default function ConfirmationBox(props) {
         return order;
       }
     }
+  }, [orders, chainId, isLong, toToken.address, toToken.isNative]);
+
+  const existingOrders = useMemo(() => {
+    const wrappedToken = getWrappedToken(chainId);
+    return orders.filter((order) => {
+      if (order.type !== INCREASE) return false;
+      const sameToken =
+        order.indexToken === wrappedToken.address ? toToken.isNative : order.indexToken === toToken.address;
+      return order.isLong === isLong && sameToken;
+    });
   }, [orders, chainId, isLong, toToken.address, toToken.isNative]);
 
   const existingTriggerOrders = useMemo(() => {
@@ -359,6 +370,41 @@ export default function ConfirmationBox(props) {
       4,
       true
     );
+    if (existingOrders?.length > 1) {
+      return (
+        <div>
+          <div className="Confirmation-box-warning">
+            <span>
+              You have multiple existing Increase {existingOrder.isLong ? "Long" : "Short"} {indexToken.symbol} limit
+              orders{" "}
+            </span>
+            <span onClick={() => setIsLimitOrdersVisible((p) => !p)} className="view-orders">
+              ({isLimitOrdersVisible ? "hide" : "view"})
+            </span>
+          </div>
+          {isLimitOrdersVisible && (
+            <ul className="order-list">
+              {existingOrders.map((order) => {
+                const { account, index, type, triggerAboveThreshold, triggerPrice } = order;
+                const id = `${account}-${index}`;
+                const triggerPricePrefix = triggerAboveThreshold ? TRIGGER_PREFIX_ABOVE : TRIGGER_PREFIX_BELOW;
+                const indexToken = getToken(chainId, order.indexToken);
+                return (
+                  <li key={id} className="font-sm">
+                    <p>
+                      {type === INCREASE ? "Increase" : "Decrease"} {indexToken.symbol} {isLong ? "Long" : "Short"}
+                    </p>
+                    <p>
+                      &nbsp;{triggerPricePrefix} ${formatAmount(triggerPrice, USD_DECIMALS, 2, true)}
+                    </p>
+                  </li>
+                );
+              })}
+            </ul>
+          )}
+        </div>
+      );
+    }
     return (
       <div className="Confirmation-box-warning">
         You have an active Limit Order to Increase {existingOrder.isLong ? "Long" : "Short"} {sizeInToken}{" "}
@@ -366,7 +412,7 @@ export default function ConfirmationBox(props) {
         {formatAmount(existingOrder.triggerPrice, USD_DECIMALS, 2, true)}
       </div>
     );
-  }, [existingOrder, isSwap, chainId]);
+  }, [existingOrder, isSwap, chainId, existingOrders, isLong, isLimitOrdersVisible]);
 
   const renderExistingTriggerErrors = useCallback(() => {
     if (isSwap || decreaseOrdersThatWillBeExecuted?.length < 1) {
@@ -381,20 +427,21 @@ export default function ConfirmationBox(props) {
           that might execute immediately after you open this position. Please cancel the {orderText} or accept the
           confirmation to continue.
         </div>
-        <ul className="trigger-order-list">
-          {decreaseOrdersThatWillBeExecuted.map((order, i) => {
-            const triggerPricePrefix = order.triggerAboveThreshold ? TRIGGER_PREFIX_ABOVE : TRIGGER_PREFIX_BELOW;
+        <ul className="order-list">
+          {decreaseOrdersThatWillBeExecuted.map((order) => {
+            const { account, index, type, triggerAboveThreshold, triggerPrice } = order;
+            const id = `${account}-${index}`;
+            const triggerPricePrefix = triggerAboveThreshold ? TRIGGER_PREFIX_ABOVE : TRIGGER_PREFIX_BELOW;
             const indexToken = getToken(chainId, order.indexToken);
             return (
-              <li key={`${order.account}-${i}`}>
+              <li key={id}>
                 <p>
-                  {order.type === INCREASE ? "Increase" : "Decrease"} {indexToken.symbol}{" "}
-                  {order.isLong ? "Long" : "Short"}
-                  &nbsp;{triggerPricePrefix} ${formatAmount(order.triggerPrice, USD_DECIMALS, 2, true)}
+                  {type === INCREASE ? "Increase" : "Decrease"} {indexToken.symbol} {isLong ? "Long" : "Short"}
+                  &nbsp;{triggerPricePrefix} ${formatAmount(triggerPrice, USD_DECIMALS, 2, true)}
                 </p>
                 <button
                   onClick={() =>
-                    cancelDecreaseOrder(chainId, library, order.index, {
+                    cancelDecreaseOrder(chainId, library, index, {
                       successMsg: "Order cancelled",
                       failMsg: "Cancel failed",
                       sentMsg: "Cancel submitted",
@@ -411,7 +458,7 @@ export default function ConfirmationBox(props) {
         </ul>
       </>
     );
-  }, [decreaseOrdersThatWillBeExecuted, isSwap, chainId, library, pendingTxns, setPendingTxns]);
+  }, [decreaseOrdersThatWillBeExecuted, isSwap, chainId, library, pendingTxns, setPendingTxns, isLong]);
 
   const renderExistingTriggerWarning = useCallback(() => {
     if (
