6d9bdfddac1b3aa4c105e59a3fffd98eb6844559
vipin <vipineth@gmail.com>
2022-07-24 09:18:41 +0530

    add individual chain data to dashboard overview

--- src/Helpers.js
+++ src/Helpers.js
@@ -2751,7 +2751,7 @@ export function getOrderError(account, order, positionsMap, position) {
   }
 }
 
-export function arrayFetcher(...urlArr) {
+export function arrayURLFetcher(...urlArr) {
   const fetcher = (url) => fetch(url).then((res) => res.json());
   return Promise.all(urlArr.map(fetcher));
 }
