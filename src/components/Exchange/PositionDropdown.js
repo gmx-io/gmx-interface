e22544b9096395819e845b40a0d3afb7eaf963d1
vipin <vipineth@gmail.com>
2022-07-27 22:10:40 +0530

    remove breakpoint

--- src/components/Exchange/PositionDropdown.js
+++ src/components/Exchange/PositionDropdown.js
@@ -3,11 +3,9 @@ import { HiDotsVertical } from "react-icons/hi";
 import { AiOutlineEdit } from "react-icons/ai";
 import { BiSelectMultiple } from "react-icons/bi";
 import { FiShare2 } from "react-icons/fi";
-import { createBreakpoint } from "react-use";
 import "./PositionDropdown.css";
 
 function PositionDropdown({ handleEditCollateral, handleShare, handleMarketSelect }) {
-  const useBreakpoint = createBreakpoint({ L: 600, M: 550, S: 400 });
   return (
     <Menu>
       <Menu.Button as="div">
