b16c528660bc078808d8cd8e2110a9a33c77db1a
vipin <vipineth@gmail.com>
2022-07-29 22:43:43 +0530

    update referral to upload on cloudinary

--- src/components/Exchange/PositionShare.js
+++ src/components/Exchange/PositionShare.js
@@ -1,5 +1,7 @@
-import { useRef, useState } from "react";
-import { toPng, toBlob } from "html-to-image";
+import { useEffect, useRef, useState } from "react";
+import { toPng } from "html-to-image";
+import cx from "classnames";
+import { useCopyToClipboard } from "react-use";
 import Modal from "../Modal/Modal";
 import gmxLogo from "../../img/gmx-logo-with-name.svg";
 import "./PositionShare.css";
@@ -8,45 +10,47 @@ import { formatAmount, USD_DECIMALS } from "../../Helpers";
 import { useAffiliateCodes } from "../../Api/referrals";
 // canvasWidth: 1200, canvasHeight: 675,
 const config = { quality: 0.5 };
-const buttonStatus = {
-  INIT: "Copy and share on Twitter",
-  COPIED: "Copied!",
-  SHARE: "Share on Twitter",
-  ERROR: "Something went wrong!",
-};
+const UPLOAD_URL = "https://gmxs.vercel.app/api/upload";
 
-function generateTweetLink(referralCode = "") {
-  return `https://twitter.com/intent/tweet?text=Checkout%20my%20ETH%20position%20on%20%40GMX_IO!%0A%0A%20Use%20my%20link%20to%20trade%20https%3A%2F%2Fapp.gmx.io%2Ftrade${
-    referralCode ? `%3Fref%3D${referralCode}` : ""
-  }.%0A%0A%20%5Bpaste%20image%20you%20copied%20or%20delete%20this%5D`;
+function getShareURL(imageInfo, ref) {
+  if (!imageInfo) return;
+  let url = `https://gmxs.vercel.app/api/s?v=${imageInfo.version}&id=${imageInfo.id}`;
+  if (ref.success) {
+    url = url + `&ref=${ref.code}`;
+  }
+  return url;
 }
 
-function SharePositionModal({
-  setIsPositionShareModalOpen,
-  isPositionShareModalOpen,
-  positionToShare,
-  account,
-  chainId,
-}) {
-  const [status, setStatus] = useState("INIT");
-  const codes = useAffiliateCodes(chainId, account);
+function PositionShare({ setIsPositionShareModalOpen, isPositionShareModalOpen, positionToShare, account, chainId }) {
+  const userAffiliateCode = useAffiliateCodes(chainId, account);
+  const [copyText, setCopyText] = useState("Copy");
+  const [uploadedImageInfo, setUploadedImageInfo] = useState();
+  const [copyState, copyToClipboard] = useCopyToClipboard();
   const positionRef = useRef();
-  function handleCopy() {
-    const element = positionRef.current;
-    try {
-      navigator.clipboard.write([
-        new window.ClipboardItem({
-          "image/png": toBlob(element),
-        }),
-      ]);
-      setStatus("COPIED");
+
+  useEffect(() => {
+    (async function () {
+      const element = positionRef.current;
+      if (!element) return;
+      const imagePng = await toPng(element);
+      if (userAffiliateCode.success) {
+        const imageInfo = await fetch(UPLOAD_URL, { method: "POST", body: imagePng }).then((res) => res.json());
+        setUploadedImageInfo(imageInfo);
+      }
+    })();
+  }, [userAffiliateCode]);
+
+  useEffect(() => {
+    if (copyState.value) {
+      setCopyText("Copied");
       setTimeout(() => {
-        setStatus("SHARE");
-      }, 2000);
-    } catch {
-      setStatus("ERROR");
+        setCopyText("Copy");
+      }, 5000);
     }
-  }
+    if (copyState.error) {
+      setCopyText("Unable to copy");
+    }
+  }, [copyState]);
 
   async function handleDownload() {
     const element = positionRef.current;
@@ -55,30 +59,15 @@ function SharePositionModal({
     const link = document.createElement("a");
     link.download = `long-${Math.random() * 100000}.jpeg`;
     link.href = dataUrl;
-    window.linkA = link;
     document.body.appendChild(link);
     link.click();
   }
-  function renderCopyButton() {
-    if (!typeof ClipboardItem || !navigator.clipboard.write) {
-      return;
-    }
-    return status === "SHARE" ? (
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        className="default-btn"
-        href={generateTweetLink(codes.length > 0 && codes[0])}
-      >
-        {buttonStatus[status]}
-      </a>
-    ) : (
-      <button disabled={status === "COPIED"} className="default-btn" onClick={handleCopy}>
-        {buttonStatus[status]}
-      </button>
-    );
+
+  function handleCopy() {
+    if (!uploadedImageInfo) return;
+    const url = getShareURL(uploadedImageInfo, userAffiliateCode);
+    copyToClipboard(url);
   }
-  if (!positionToShare) return "Loading...";
   return (
     <Modal
       className="position-share-modal"
@@ -87,23 +76,34 @@ function SharePositionModal({
       label="Launch App"
     >
       <PositionShareCard
-        codes={codes}
+        userAffiliateCode={userAffiliateCode}
         positionRef={positionRef}
         position={positionToShare}
         chainId={chainId}
         account={account}
       />
       <div className="actions">
-        {renderCopyButton()}
-        <button className="default-btn" onClick={handleDownload}>
+        <button disabled={!uploadedImageInfo} className="default-btn mr-base" onClick={handleCopy}>
+          {copyText}
+        </button>
+        <button className="default-btn mr-base" onClick={handleDownload}>
           Download
         </button>
+        <a
+          target="_blank"
+          className={cx("default-btn tweet-link", { disabled: !uploadedImageInfo })}
+          rel="noreferrer"
+          href={getShareURL(uploadedImageInfo, userAffiliateCode)}
+        >
+          Tweet
+        </a>
       </div>
     </Modal>
   );
 }
 
-function PositionShareCard({ positionRef, position, codes }) {
+function PositionShareCard({ positionRef, position, userAffiliateCode }) {
+  const { code, success } = userAffiliateCode;
   const { deltaAfterFeesPercentageStr, isLong, leverage, indexToken, averagePrice, markPrice } = position;
   return (
     <div ref={positionRef} className="position-share">
@@ -129,12 +129,12 @@ function PositionShareCard({ positionRef, position, codes }) {
           includeMargin={true}
           size={35}
           level="M"
-          value={codes.length > 0 ? `https://gmx.io/trade?ref=${codes[0]}` : "https://gmx.io/trade"}
+          value={success ? `https://gmx.io/trade?ref=${code}` : "https://gmx.io/trade"}
         />
-        {codes.length > 0 ? (
+        {success ? (
           <div>
             <p className="label">Referral Code:</p>
-            <p className="code">{codes?.[0]}</p>
+            <p className="code">{code}</p>
           </div>
         ) : (
           <div>
@@ -146,4 +146,4 @@ function PositionShareCard({ positionRef, position, codes }) {
   );
 }
 
-export default SharePositionModal;
+export default PositionShare;
