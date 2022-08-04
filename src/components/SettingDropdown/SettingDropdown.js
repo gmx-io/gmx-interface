e41222df2e91f90b02bf9b94f12b4813b65f9074
vipin <vipineth@gmail.com>
2022-08-04 13:56:12 +0530

    remove commented code

--- src/components/SettingDropdown/SettingDropdown.js
+++ src/components/SettingDropdown/SettingDropdown.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Menu } from "@headlessui/react";
-import { i18n } from '@lingui/core'
+import { i18n } from "@lingui/core";
 import cx from "classnames";
 import "./SettingDropdown.css";
 import setting24Icon from "../../img/ic_settings_24.svg";
@@ -12,52 +12,53 @@ import englishFlag from "../../img/flag_english.svg";
 import frenchFlag from "../../img/flag-french.png";
 import spanishFlag from "../../img/flag_spanish.png";
 import checkedIcon from "../../img/ic_checked.svg";
-import { messages as enMessages } from '../../locales/en/messages';
-import { messages as esMessages } from '../../locales/es/messages';
-import { Trans, t } from '@lingui/macro'
+import { messages as enMessages } from "../../locales/en/messages";
+import { messages as esMessages } from "../../locales/es/messages";
+import { Trans, t } from "@lingui/macro";
 
 i18n.load({
-  'en': enMessages,
-  'es': esMessages
-})
-i18n.activate('en')
+  en: enMessages,
+  es: esMessages,
+});
+i18n.activate("en");
 
 export default function SettingDropdown(props) {
-  const {
-    openSettings,
-  } = props;
+  const { openSettings } = props;
 
-  let currentLanguage = localStorage.getItem('LANGUAGE_KEY')
+  let currentLanguage = localStorage.getItem("LANGUAGE_KEY");
   if (!currentLanguage) {
-    currentLanguage = 'en'
+    currentLanguage = "en";
   }
 
-  const [languageMenuHidden, setLanguageMenuHidden] = useState(true)
+  const [languageMenuHidden, setLanguageMenuHidden] = useState(true);
 
   const toggleLanguageMenu = (e) => {
-    e.preventDefault()
-    setLanguageMenuHidden(!languageMenuHidden)
-  }
+    e.preventDefault();
+    setLanguageMenuHidden(!languageMenuHidden);
+  };
 
-  const availableLanguages = [{
-    icon: englishFlag,
-    name: t`English`,
-    language: 'en'
-  }, {
-    icon: spanishFlag,
-    name: t`Spanish`,
-    language: 'es'
-  }, {
-    icon: frenchFlag,
-    name: t`French`,
-    language: 'fr'
-  }]
+  const availableLanguages = [
+    {
+      icon: englishFlag,
+      name: t`English`,
+      language: "en",
+    },
+    {
+      icon: spanishFlag,
+      name: t`Spanish`,
+      language: "es",
+    },
+    {
+      icon: frenchFlag,
+      name: t`French`,
+      language: "fr",
+    },
+  ];
 
   const selectLanguage = (lang) => {
-    console.log(lang)
-    i18n.activate(lang)
-    localStorage.setItem('LANGUAGE_KEY', lang)
-  }
+    i18n.activate(lang);
+    localStorage.setItem("LANGUAGE_KEY", lang);
+  };
 
   return (
     <Menu>
@@ -67,88 +68,65 @@ export default function SettingDropdown(props) {
         </button>
       </Menu.Button>
       <div className="settings-dropdown-menu">
-        {
-          languageMenuHidden && (
-            <Menu.Items as="div" className="menu-items settings-dropdown-menu-items">
-              <Menu.Item>
-                <div
-                  className="settings-dropdown-menu-item menu-item"
-                  onClick={() => openSettings()}
-                >
-                  <div className="settings-dropdown-menu-item__prepend">
-                    <img src={setting16Icon} alt="settings-open-icon" />
-                  </div>
-                  <span className="settings-dropdown-menu-item-label menu-item-label">
-                    <Trans>Trade settings</Trans>
-                  </span>
-                  <div className="settings-dropdown-menu-item__append">
-                    <img src={arrowright16Icon} alt="arrow-right-icon" />
-                  </div>
+        {languageMenuHidden && (
+          <Menu.Items as="div" className="menu-items settings-dropdown-menu-items">
+            <Menu.Item>
+              <div className="settings-dropdown-menu-item menu-item" onClick={() => openSettings()}>
+                <div className="settings-dropdown-menu-item__prepend">
+                  <img src={setting16Icon} alt="settings-open-icon" />
                 </div>
-              </Menu.Item>
-              <Menu.Item>
-                <div
-                  className="settings-dropdown-menu-item menu-item"
-                  onClick={toggleLanguageMenu}
-                >
-                  <div className="settings-dropdown-menu-item__prepend">
-                    <img src={language16Icon} alt="language-menu-open-icon" />
-                  </div>
-                  <span className="settings-dropdown-menu-item-label menu-item-label">
-                    <Trans>Language</Trans>
-                  </span>
-                  <div className="settings-dropdown-menu-item__append">
-                    <img src={arrowright16Icon} alt="arrow-right-icon" />
-                  </div>
+                <span className="settings-dropdown-menu-item-label menu-item-label">
+                  <Trans>Trade settings</Trans>
+                </span>
+                <div className="settings-dropdown-menu-item__append">
+                  <img src={arrowright16Icon} alt="arrow-right-icon" />
                 </div>
-              </Menu.Item>
-            </Menu.Items>
-          )
-        }
-        {
-          !languageMenuHidden &&
-          (
-            <Menu.Items as="div" className="menu-items settings-dropdown-menu-items">
-              <Menu.Item>
-                <div
-                  className="settings-dropdown-menu-item menu-item"
-                  onClick={toggleLanguageMenu}
-                >
-                  <div className="settings-dropdown-menu-item__prepend">
-                    <img src={arrowleft16Icon} alt="arrow-left-icon" />
-                  </div>
-                  <span className="settings-dropdown-menu-item-label menu-item-label">
-                    <Trans>Select Language</Trans>
-                  </span>
+              </div>
+            </Menu.Item>
+            <Menu.Item>
+              <div className="settings-dropdown-menu-item menu-item" onClick={toggleLanguageMenu}>
+                <div className="settings-dropdown-menu-item__prepend">
+                  <img src={language16Icon} alt="language-menu-open-icon" />
                 </div>
-              </Menu.Item>
-              {
-                availableLanguages.map((item, index) => {
-                  return (
-                    <Menu.Item key={index}>
-                      <div
-                        className="settings-dropdown-menu-item menu-item"
-                        onClick={() => selectLanguage(item.language)}
-                      >
-                        <div className="settings-dropdown-menu-item__prepend menu-item__prepend">
-                          <img src={item.icon} alt="language-menu-open-icon" />
-                        </div>
-                        <span className="settings-dropdown-menu-item-label menu-item-label">
-                          {item.name}
-                        </span>
-                        <div className="settings-dropdown-menu-item__append menu-item__append">
-                          {
-                            currentLanguage === item.language && <img src={checkedIcon} alt="checked-icon" />
-                          }
-                        </div>
-                      </div>
-                    </Menu.Item>
-                  )
-                })
-              }
-            </Menu.Items>
-          )
-        }
+                <span className="settings-dropdown-menu-item-label menu-item-label">
+                  <Trans>Language</Trans>
+                </span>
+                <div className="settings-dropdown-menu-item__append">
+                  <img src={arrowright16Icon} alt="arrow-right-icon" />
+                </div>
+              </div>
+            </Menu.Item>
+          </Menu.Items>
+        )}
+        {!languageMenuHidden && (
+          <Menu.Items as="div" className="menu-items settings-dropdown-menu-items">
+            <Menu.Item>
+              <div className="settings-dropdown-menu-item menu-item" onClick={toggleLanguageMenu}>
+                <div className="settings-dropdown-menu-item__prepend">
+                  <img src={arrowleft16Icon} alt="arrow-left-icon" />
+                </div>
+                <span className="settings-dropdown-menu-item-label menu-item-label">
+                  <Trans>Select Language</Trans>
+                </span>
+              </div>
+            </Menu.Item>
+            {availableLanguages.map((item, index) => {
+              return (
+                <Menu.Item key={index}>
+                  <div className="settings-dropdown-menu-item menu-item" onClick={() => selectLanguage(item.language)}>
+                    <div className="settings-dropdown-menu-item__prepend menu-item__prepend">
+                      <img src={item.icon} alt="language-menu-open-icon" />
+                    </div>
+                    <span className="settings-dropdown-menu-item-label menu-item-label">{item.name}</span>
+                    <div className="settings-dropdown-menu-item__append menu-item__append">
+                      {currentLanguage === item.language && <img src={checkedIcon} alt="checked-icon" />}
+                    </div>
+                  </div>
+                </Menu.Item>
+              );
+            })}
+          </Menu.Items>
+        )}
       </div>
     </Menu>
   );
