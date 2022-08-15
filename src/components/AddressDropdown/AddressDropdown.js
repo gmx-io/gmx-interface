ee33200ff434b7e0d4067d3c359e2373ea83b467
vipin <vipineth@gmail.com>
2022-08-15 10:49:51 +0530

    update header style

--- src/components/AddressDropdown/AddressDropdown.js
+++ src/components/AddressDropdown/AddressDropdown.js
@@ -1,108 +1,57 @@
-import React from "react";
-import cx from "classnames";
 import "./AddressDropdown.css";
 import { Menu } from "@headlessui/react";
-import { ARBITRUM, AVALANCHE, helperToast, shortenAddress, useENS } from "../../Helpers";
+import { helperToast, shortenAddress, useENS } from "../../Helpers";
 import { useCopyToClipboard, createBreakpoint } from "react-use";
 import externalLink from "../../img/ic_new_link_16.svg";
 import copy from "../../img/ic_copy_16.svg";
 import disconnect from "../../img/ic_sign_out_16.svg";
 import { FaChevronDown } from "react-icons/fa";
 import Davatar from "@davatar/react";
-import { Trans, t } from "@lingui/macro";
-import arbitrum16Icon from "../../img/ic_arbitrum_16.svg";
-import avalanche16Icon from "../../img/ic_avalanche_16.svg";
 
-function AddressDropdown(props) {
-  const { account, accountUrl, disconnectAccountAndCloseSettings, label, onNetworkSelect } = props;
+function AddressDropdown({ account, accountUrl, disconnectAccountAndCloseSettings }) {
   const useBreakpoint = createBreakpoint({ L: 600, M: 550, S: 400 });
   const breakpoint = useBreakpoint();
   const [, copyToClipboard] = useCopyToClipboard();
   const { ensName } = useENS(account);
 
-  async function handleNetworkSelect(option) {
-    await onNetworkSelect(option);
-  }
-
   return (
-    <div>
-      <Menu>
-        <Menu.Button as="div">
-          <button className="App-cta small transparent address-btn">
-            <Davatar size={20} address={account} />
-            <span className="user-address">{ensName || shortenAddress(account, breakpoint === "S" ? 9 : 13)}</span>
-            <FaChevronDown />
-          </button>
-        </Menu.Button>
-        <div>
-          <Menu.Items as="div" className="menu-items">
-            <div className="network-selector-menu-items">
-              <p className="network-selector-menu-items-title">
-                <Trans>Networks</Trans>
-              </p>
-              <Menu.Item>
-                <div className="menu-item" onClick={() => handleNetworkSelect({ value: ARBITRUM })}>
-                  <div className="menu-item__prepend">
-                    <img src={arbitrum16Icon} alt="arbitrum icon" />
-                  </div>
-                  <span className="menu-item-label">
-                    <Trans>Arbitrum</Trans>
-                  </span>
-                  <div className={cx("selected-icon", { [label]: label === "Arbitrum" })} />
-                </div>
-              </Menu.Item>
-              <Menu.Item>
-                <div className="menu-item" onClick={() => handleNetworkSelect({ value: AVALANCHE })}>
-                  <div className="menu-item__prepend">
-                    <img src={avalanche16Icon} alt="avalanche icon" />
-                  </div>
-                  <span className="menu-item-label">
-                    <Trans>Avalanche</Trans>
-                  </span>
-                  <div className={cx("selected-icon", { [label]: label === "Avalanche" })} />
-                </div>
-              </Menu.Item>
+    <Menu>
+      <Menu.Button as="div">
+        <button className="App-cta small transparent address-btn">
+          <Davatar size={20} address={account} />
+          <span className="user-address">{ensName || shortenAddress(account, breakpoint === "S" ? 9 : 13)}</span>
+          <FaChevronDown />
+        </button>
+      </Menu.Button>
+      <div>
+        <Menu.Items as="div" className="menu-items">
+          <Menu.Item>
+            <div
+              className="menu-item"
+              onClick={() => {
+                copyToClipboard(account);
+                helperToast.success("Address copied to your clipboard");
+              }}
+            >
+              <img src={copy} alt="Copy user address" />
+              <p>Copy Address</p>
             </div>
-            <Menu.Item>
-              <div
-                className="menu-item"
-                onClick={() => {
-                  copyToClipboard(account);
-                  helperToast.success(t`Address copied to your clipboard`);
-                }}
-              >
-                <div className="menu-item__prepend">
-                  <img src={copy} alt="Copy user address" />
-                </div>
-                <span className="menu-item-label">
-                  <Trans>Copy Address</Trans>
-                </span>
-              </div>
-            </Menu.Item>
-            <Menu.Item>
-              <a href={accountUrl} target="_blank" rel="noopener noreferrer" className="menu-item">
-                <div className="menu-item__prepend">
-                  <img src={externalLink} alt="Open address in explorer" />
-                </div>
-                <span className="menu-item-label">
-                  <Trans>View in Explorer</Trans>
-                </span>
-              </a>
-            </Menu.Item>
-            <Menu.Item>
-              <div className="menu-item" onClick={disconnectAccountAndCloseSettings}>
-                <div className="menu-item__prepend">
-                  <img src={disconnect} alt="Disconnect the wallet" />
-                </div>
-                <span className="menu-item-label">
-                  <Trans>Disconnect</Trans>
-                </span>
-              </div>
-            </Menu.Item>
-          </Menu.Items>
-        </div>
-      </Menu>
-    </div>
+          </Menu.Item>
+          <Menu.Item>
+            <a href={accountUrl} target="_blank" rel="noopener noreferrer" className="menu-item">
+              <img src={externalLink} alt="Open address in explorer" />
+              <p>View in Explorer</p>
+            </a>
+          </Menu.Item>
+          <Menu.Item>
+            <div className="menu-item" onClick={disconnectAccountAndCloseSettings}>
+              <img src={disconnect} alt="Disconnect the wallet" />
+              <p>Disconnect</p>
+            </div>
+          </Menu.Item>
+        </Menu.Items>
+      </div>
+    </Menu>
   );
 }
 
--- src/components/SettingDropdown/SettingDropdown.js
+++ src/components/SettingDropdown/SettingDropdown.js
@@ -1,6 +1,5 @@
 import React, { useRef, useState } from "react";
 import { Menu } from "@headlessui/react";
-import { FaNetworkWired } from "react-icons/fa";
 import cx from "classnames";
 import "./SettingDropdown.css";
 import setting24Icon from "../../img/ic_settings_24.svg";
@@ -11,15 +10,11 @@ import arrowleft16Icon from "../../img/ic_arrowleft16.svg";
 import checkedIcon from "../../img/ic_checked.svg";
 import { Trans } from "@lingui/macro";
 import { defaultLocale, dynamicActivate, locales } from "../../utils/i18n";
-import { ARBITRUM, AVALANCHE, importImage, LANGUAGE_LOCALSTORAGE_KEY } from "../../Helpers";
-import arbitrum16Icon from "../../img/ic_arbitrum_16.svg";
-import avalanche16Icon from "../../img/ic_avalanche_16.svg";
+import { importImage, LANGUAGE_LOCALSTORAGE_KEY } from "../../Helpers";
 
 const LANGUAGE_SUB_MENU = "LANGUAGE";
-const NETWORK_SUB_MENU = "NETWORK";
 
-export default function SettingDropdown(props) {
-  const { openSettings, onNetworkSelect, label } = props;
+export default function SettingDropdown({ openSettings }) {
   let currentLanguage = useRef(localStorage.getItem(LANGUAGE_LOCALSTORAGE_KEY) || defaultLocale);
   const [activeSubMenu, setActiveSubMenu] = useState(null);
 
@@ -32,10 +27,6 @@ export default function SettingDropdown(props) {
     setActiveSubMenu(null);
   }
 
-  async function handleNetworkSelect(option) {
-    await onNetworkSelect(option);
-  }
-
   return (
     <Menu>
       <Menu.Button as="div">
@@ -43,135 +34,80 @@ export default function SettingDropdown(props) {
           <img src={setting24Icon} alt="settings-dropdown-icon" />
         </button>
       </Menu.Button>
-      <div className="settings-dropdown-menu">
-        {!activeSubMenu && (
-          <Menu.Items as="div" className="menu-items settings-dropdown-menu-items">
-            <Menu.Item>
-              <div className="settings-dropdown-menu-item menu-item" onClick={() => openSettings()}>
-                <div className="settings-dropdown-menu-item__prepend">
-                  <img src={setting16Icon} alt="settings-open-icon" />
-                </div>
-                <span className="settings-dropdown-menu-item-label menu-item-label">
-                  <Trans>Trade Settings</Trans>
-                </span>
-                <div className="settings-dropdown-menu-item__append">
-                  <img src={arrowright16Icon} alt="arrow-right-icon" />
-                </div>
+
+      {!activeSubMenu && (
+        <Menu.Items as="div" className="menu-items settings-dropdown-menu-items">
+          <Menu.Item>
+            <div className="settings-dropdown-menu-item menu-item" onClick={() => openSettings()}>
+              <div className="settings-dropdown-menu-item__prepend">
+                <img src={setting16Icon} alt="settings-open-icon" />
               </div>
-            </Menu.Item>
-            <Menu.Item>
-              <div
-                className="settings-dropdown-menu-item menu-item"
-                onClick={(e) => {
-                  openMenu(e, NETWORK_SUB_MENU);
-                }}
-              >
-                <div className="settings-dropdown-menu-item__prepend">
-                  <FaNetworkWired />
-                </div>
-                <span className="settings-dropdown-menu-item-label menu-item-label">
-                  <Trans>Switch Network</Trans>
-                </span>
-                <div className="settings-dropdown-menu-item__append">
-                  <img src={arrowright16Icon} alt="arrow-right-icon" />
-                </div>
+              <span className="settings-dropdown-menu-item-label menu-item-label">
+                <Trans>Trade Settings</Trans>
+              </span>
+              <div className="settings-dropdown-menu-item__append">
+                <img src={arrowright16Icon} alt="arrow-right-icon" />
               </div>
-            </Menu.Item>
-            <Menu.Item>
-              <div
-                className="settings-dropdown-menu-item menu-item"
-                onClick={(e) => {
-                  openMenu(e, LANGUAGE_SUB_MENU);
-                }}
-              >
-                <div className="settings-dropdown-menu-item__prepend">
-                  <img src={language16Icon} alt="language-menu-open-icon" />
-                </div>
-                <span className="settings-dropdown-menu-item-label menu-item-label">
-                  <Trans>Language</Trans>
-                </span>
-                <div className="settings-dropdown-menu-item__append">
-                  <img src={arrowright16Icon} alt="arrow-right-icon" />
-                </div>
+            </div>
+          </Menu.Item>
+          <Menu.Item>
+            <div
+              className="settings-dropdown-menu-item menu-item"
+              onClick={(e) => {
+                openMenu(e, LANGUAGE_SUB_MENU);
+              }}
+            >
+              <div className="settings-dropdown-menu-item__prepend">
+                <img src={language16Icon} alt="language-menu-open-icon" />
               </div>
-            </Menu.Item>
-          </Menu.Items>
-        )}
-        {activeSubMenu === LANGUAGE_SUB_MENU && (
-          <Menu.Items as="div" className="menu-items settings-dropdown-menu-items">
-            <Menu.Item>
-              <div className="settings-dropdown-menu-item menu-item" onClick={closeMenu}>
-                <div className="settings-dropdown-menu-item__prepend">
-                  <img src={arrowleft16Icon} alt="arrow-left-icon" />
-                </div>
-                <span className="settings-dropdown-menu-item-label menu-item-label">
-                  <Trans>Select Language</Trans>
-                </span>
+              <span className="settings-dropdown-menu-item-label menu-item-label">
+                <Trans>Language</Trans>
+              </span>
+              <div className="settings-dropdown-menu-item__append">
+                <img src={arrowright16Icon} alt="arrow-right-icon" />
               </div>
-            </Menu.Item>
-            {Object.keys(locales).map((item) => {
-              const image = importImage(`flag_${item}.svg`);
-              return (
-                <Menu.Item key={item}>
-                  <div
-                    className="settings-dropdown-menu-item menu-item"
-                    onClick={() => {
-                      currentLanguage.current = item;
-                      dynamicActivate(item);
-                    }}
-                  >
-                    <div className="settings-dropdown-menu-item__prepend menu-item__prepend">
-                      <img className="language-image" src={image} alt="language-menu-open-icon" />
-                    </div>
-                    <span className="settings-dropdown-menu-item-label menu-item-label">
-                      <Trans>{locales[item]}</Trans>
-                    </span>
-                    <div className="settings-dropdown-menu-item__append menu-item__append">
-                      {currentLanguage.current === item && <img src={checkedIcon} alt="checked-icon" />}
-                    </div>
+            </div>
+          </Menu.Item>
+        </Menu.Items>
+      )}
+      {activeSubMenu === LANGUAGE_SUB_MENU && (
+        <Menu.Items as="div" className="menu-items settings-dropdown-menu-items">
+          <Menu.Item>
+            <div className="settings-dropdown-menu-item menu-item" onClick={closeMenu}>
+              <div className="settings-dropdown-menu-item__prepend">
+                <img src={arrowleft16Icon} alt="arrow-left-icon" />
+              </div>
+              <span className="settings-dropdown-menu-item-label menu-item-label">
+                <Trans>Select Language</Trans>
+              </span>
+            </div>
+          </Menu.Item>
+          {Object.keys(locales).map((item) => {
+            const image = importImage(`flag_${item}.svg`);
+            return (
+              <Menu.Item key={item}>
+                <div
+                  className="settings-dropdown-menu-item menu-item"
+                  onClick={() => {
+                    currentLanguage.current = item;
+                    dynamicActivate(item);
+                  }}
+                >
+                  <div className="settings-dropdown-menu-item__prepend menu-item__prepend">
+                    <img className="language-image" src={image} alt="language-menu-open-icon" />
                   </div>
-                </Menu.Item>
-              );
-            })}
-          </Menu.Items>
-        )}
-        {activeSubMenu === NETWORK_SUB_MENU && (
-          <Menu.Items as="div" className="menu-items settings-dropdown-menu-items">
-            <Menu.Item>
-              <div className="settings-dropdown-menu-item menu-item" onClick={closeMenu}>
-                <div className="settings-dropdown-menu-item__prepend">
-                  <img src={arrowleft16Icon} alt="arrow-left-icon" />
+                  <span className="settings-dropdown-menu-item-label menu-item-label">
+                    <Trans>{locales[item]}</Trans>
+                  </span>
+                  <div className="settings-dropdown-menu-item__append menu-item__append">
+                    {currentLanguage.current === item && <img src={checkedIcon} alt="checked-icon" />}
+                  </div>
                 </div>
-                <span className="settings-dropdown-menu-item-label menu-item-label">
-                  <Trans>Select Network</Trans>
-                </span>
-              </div>
-            </Menu.Item>
-            <Menu.Item>
-              <div className="menu-item" onClick={() => handleNetworkSelect({ value: ARBITRUM })}>
-                <div className="menu-item__prepend">
-                  <img src={arbitrum16Icon} alt="arbitrum icon" />
-                </div>
-                <span className="menu-item-label">
-                  <Trans>Arbitrum</Trans>
-                </span>
-                <div className={cx("selected-icon", { [label]: label === "Arbitrum" })} />
-              </div>
-            </Menu.Item>
-            <Menu.Item>
-              <div className="menu-item" onClick={() => handleNetworkSelect({ value: AVALANCHE })}>
-                <div className="menu-item__prepend">
-                  <img src={avalanche16Icon} alt="arbitrum icon" />
-                </div>
-                <span className="menu-item-label">
-                  <Trans>Avalanche</Trans>
-                </span>
-                <div className={cx("selected-icon", { [label]: label === "Avalanche" })} />
-              </div>
-            </Menu.Item>
-          </Menu.Items>
-        )}
-      </div>
+              </Menu.Item>
+            );
+          })}
+        </Menu.Items>
+      )}
     </Menu>
   );
 }
