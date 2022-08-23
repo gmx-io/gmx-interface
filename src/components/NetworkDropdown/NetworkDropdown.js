ddd37fe69345c898a20593ef56311f714b6e0d61
vipin <vipineth@gmail.com>
2022-08-23 11:51:30 +0530

    update network selector style

--- src/components/NetworkDropdown/NetworkDropdown.js
+++ src/components/NetworkDropdown/NetworkDropdown.js
@@ -1,17 +1,31 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Menu } from "@headlessui/react";
 import { Trans } from "@lingui/macro";
 import cx from "classnames";
 import "./NetworkDropdown.css";
-import language16Icon from "../../img/ic_language16.svg";
+import language24Icon from "../../img/ic_language24.svg";
 import arbitrumIcon from "../../img/ic_arbitrum_24.svg";
 import avaxIcon from "../../img/ic_avalanche_24.svg";
 import checkedIcon from "../../img/ic_checked.svg";
+import arrowleft16Icon from "../../img/ic_arrowleft16.svg";
+import arrowright16Icon from "../../img/ic_arrowright16.svg";
 import { importImage, isHomeSite, LANGUAGE_LOCALSTORAGE_KEY } from "../../Helpers";
 import { defaultLocale, dynamicActivate, locales } from "../../utils/i18n";
 
-export default function SettingsDropdown({ networkOptions, selectorLabel, onNetworkSelect }) {
+const LANGUAGE_SUBMENU = "LANGUAGE";
+
+export default function NetworkDropdown({ networkOptions, selectorLabel, onNetworkSelect }) {
   const currentLanguage = useRef(localStorage.getItem(LANGUAGE_LOCALSTORAGE_KEY) || defaultLocale);
+  const [activeSubMenu, setActiveSubMenu] = useState(null);
+
+  function openMenu(e, name) {
+    e.preventDefault();
+    setActiveSubMenu(name);
+  }
+  function closeMenu(e) {
+    e.preventDefault();
+    setActiveSubMenu(null);
+  }
   return (
     <Menu>
       <Menu.Button as="div" className="network-dropdown">
@@ -25,16 +39,16 @@ export default function SettingsDropdown({ networkOptions, selectorLabel, onNetw
         )}
 
         <button className={cx("btn-primary small transparent network-dropdown-icon")}>
-          <img src={language16Icon} alt={locales[currentLanguage]} />
+          <img src={language24Icon} alt={locales[currentLanguage]} />
         </button>
       </Menu.Button>
-      <div>
+
+      {!activeSubMenu && (
         <Menu.Items as="div" className="menu-items network-dropdown-items">
           {!isHomeSite() && (
             <>
-              <div className="network-dropdown-headings">Network</div>
               <div className="network-dropdown-list">
-                <NetworkDropdown
+                <NetworkMenuItems
                   networkOptions={networkOptions}
                   selectorLabel={selectorLabel}
                   onNetworkSelect={onNetworkSelect}
@@ -42,17 +56,31 @@ export default function SettingsDropdown({ networkOptions, selectorLabel, onNetw
               </div>
             </>
           )}
-          <div className="network-dropdown-headings">Language</div>
-          <div className="network-dropdown-list">
-            <LanguageDropdown currentLanguage={currentLanguage} />
-          </div>
+          <Menu.Item>
+            <div className="network-dropdown-menu-item menu-item" onClick={(e) => openMenu(e, LANGUAGE_SUBMENU)}>
+              <div className="menu-item-group">
+                <div className="menu-item-icon">
+                  <img src={language24Icon} alt="" />
+                </div>
+                <span className="network-dropdown-item-label">Language</span>
+              </div>
+              <div className="menu-item-icon">
+                <img src={arrowright16Icon} alt="arrow-right-icon" />
+              </div>
+            </div>
+          </Menu.Item>
         </Menu.Items>
-      </div>
+      )}
+      {activeSubMenu === LANGUAGE_SUBMENU && (
+        <Menu.Items as="div" className="menu-items network-dropdown-items">
+          <LanguageDropdown currentLanguage={currentLanguage} closeMenu={closeMenu} />
+        </Menu.Items>
+      )}
     </Menu>
   );
 }
 
-function NetworkDropdown({ networkOptions, selectorLabel, onNetworkSelect }) {
+function NetworkMenuItems({ networkOptions, selectorLabel, onNetworkSelect }) {
   async function handleNetworkSelect(option) {
     await onNetworkSelect(option);
   }
@@ -65,13 +93,13 @@ function NetworkDropdown({ networkOptions, selectorLabel, onNetworkSelect }) {
           onClick={() => handleNetworkSelect({ value: network.value })}
         >
           <div className="menu-item-group">
-            <div className="network-dropdown-menu-item-img">
+            <div className="menu-item-icon">
               <img src={networkIcon} alt={network.label} />
             </div>
             <span className="network-dropdown-item-label">{network.label}</span>
           </div>
           <div className="network-dropdown-menu-item-img">
-            {selectorLabel === network.label && <img src={checkedIcon} alt="checked-icon" />}
+            <div className={cx("active-dot", { [selectorLabel]: selectorLabel === network.label })} />
           </div>
         </div>
       </Menu.Item>
@@ -79,9 +107,17 @@ function NetworkDropdown({ networkOptions, selectorLabel, onNetworkSelect }) {
   });
 }
 
-function LanguageDropdown({ currentLanguage }) {
+function LanguageDropdown({ currentLanguage, closeMenu }) {
   return (
     <>
+      <Menu.Item>
+        <div className="network-dropdown-menu-item menu-item" onClick={closeMenu}>
+          <div className="menu-item-group">
+            <img src={arrowleft16Icon} alt="arrow-left-icon" />
+            <Trans>Select Language</Trans>
+          </div>
+        </div>
+      </Menu.Item>
       {Object.keys(locales).map((item) => {
         const image = importImage(`flag_${item}.svg`);
         return (
@@ -94,10 +130,10 @@ function LanguageDropdown({ currentLanguage }) {
               }}
             >
               <div className="menu-item-group">
-                <img className="network-dropdown-language-img" src={image} alt="language-menu-open-icon" />
-                <span className="network-dropdown-item-label menu-item-label">
-                  <Trans>{locales[item]}</Trans>
-                </span>
+                <div className="menu-item-icon">
+                  <img src={image} alt="language-menu-open-icon" />
+                </div>
+                <span className="network-dropdown-item-label menu-item-label">{locales[item]}</span>
               </div>
               <div className="network-dropdown-menu-item-img">
                 {currentLanguage.current === item && <img src={checkedIcon} alt={locales[item]} />}
