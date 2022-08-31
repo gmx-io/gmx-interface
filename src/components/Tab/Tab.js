a3b2cb2853bbcaad2e9a7a1eaa7b884242f58dc3
vipin <vipineth@gmail.com>
2022-08-31 14:12:40 +0530

    remove console

--- src/components/Tab/Tab.js
+++ src/components/Tab/Tab.js
@@ -6,7 +6,6 @@ import "./Tab.css";
 
 export default function Tab(props) {
   const { options, option, setOption, onChange, type = "block", className, optionLabels, icons } = props;
-  console.log({ options });
   const onClick = (opt) => {
     if (setOption) {
       setOption(opt);
