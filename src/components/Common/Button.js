cdace22e7183324e451018024e98a28f5849bb86
vipin <vipineth@gmail.com>
2022-08-31 22:31:24 +0530

    fix icon on buy gms and update button

--- src/components/Common/Button.js
+++ src/components/Common/Button.js
@@ -1,29 +1,14 @@
 import cx from "classnames";
 import "./Button.css";
 
-export default function Button({ href, imgSrc, children, onClick, className, size = "lg", align = "center", ...rest }) {
+export function ButtonWithExternalLink({ href, imgSrc, children, className, size = "lg", align = "center", ...rest }) {
   let classNames = cx("btn btn-primary", align === "left" ? "btn-left" : "btn-center", `btn-${size}`, className);
-  if (typeof children === "object") {
-    return (
-      <a className={classNames} href={href} target="_blank" rel="noopener noreferrer" {...rest}>
-        {children}
-      </a>
-    );
-  }
 
-  if (href) {
-    return (
-      <a className={classNames} href={href} target="_blank" rel="noopener noreferrer" {...rest}>
-        {imgSrc && <img className="btn-image" src={imgSrc} alt={children} />}
-        <span className="btn-label">{children}</span>
-      </a>
-    );
-  }
   return (
-    <button className={classNames} onClick={onClick} {...rest}>
+    <a className={classNames} href={href} target="_blank" rel="noopener noreferrer" {...rest}>
       {imgSrc && <img className="btn-image" src={imgSrc} alt={children} />}
       <span className="btn-label">{children}</span>
-    </button>
+    </a>
   );
 }
 
