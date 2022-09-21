14d1c2121586320d3ec86c53cf471361646dad87
Morazzela <morazzela@protonmail.com>
2022-09-21 03:05:51 +0200

    small loading fix

--- src/components/Leaderboard/IndividualLeaderboard.tsx
+++ src/components/Leaderboard/IndividualLeaderboard.tsx
@@ -1,11 +1,10 @@
 import { useIndividualStats } from "../../domain/leaderboard/graph";
 import { useChainId } from "../../lib/legacy";
-import Loader from "../Common/Loader";
 import LeaderboardTable from "./LeaderboardTable";
 
 export function IndividualLeaderboard() {
   const { chainId } = useChainId()
   const { data, loading } = useIndividualStats(chainId);
 
-  return loading ? <Loader/> : <LeaderboardTable stats={data}/>
+  return <LeaderboardTable loading={loading} stats={data}/>
 }
