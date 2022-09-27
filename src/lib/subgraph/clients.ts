c680f6e7ec8d4d553a4a8f18133f32fc7b7d2c63
divhead <i.divhead@gmail.com>
2022-09-27 23:42:05 +0400

    (refactor step three): subgraph types

--- src/lib/subgraph/clients.ts
+++ src/lib/subgraph/clients.ts
@@ -1,19 +1,15 @@
 import { createClient } from "./utils";
-
 import { SUBGRAPH_URLS } from "../../config/subgraph";
 import { ARBITRUM, ARBITRUM_TESTNET, AVALANCHE } from "../../config/chains";
 
 export const chainlinkClient = createClient(SUBGRAPH_URLS.chainLink);
-
 export const arbitrumGraphClient = createClient(SUBGRAPH_URLS.arbitrumStats);
 export const avalancheGraphClient = createClient(SUBGRAPH_URLS.avalancheStats);
-
 export const nissohGraphClient = createClient(SUBGRAPH_URLS.nissohVault);
-
 export const arbitrumReferralsGraphClient = createClient(SUBGRAPH_URLS.arbitrumReferrals);
 export const avalancheReferralsGraphClient = createClient(SUBGRAPH_URLS.avalancheReferrals);
 
-export function getGmxGraphClient(chainId) {
+export function getGmxGraphClient(chainId: number) {
   if (chainId === ARBITRUM) {
     return arbitrumGraphClient;
   } else if (chainId === AVALANCHE) {
@@ -21,5 +17,6 @@ export function getGmxGraphClient(chainId) {
   } else if (chainId === ARBITRUM_TESTNET) {
     return null;
   }
+
   throw new Error(`Unsupported chain ${chainId}`);
 }
--- src/lib/subgraph/utils.ts
+++ src/lib/subgraph/utils.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-export function createClient(uri) {
+export function createClient(uri: string) {
   return new ApolloClient({
     uri,
     cache: new InMemoryCache(),
