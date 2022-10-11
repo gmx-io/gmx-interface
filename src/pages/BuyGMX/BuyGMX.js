6b7bbeab71c7174257626211c122cdffbb15d1ae
vipin <vipineth@gmail.com>
2022-10-11 22:20:34 +0530

    add uniswap widget to buy gmx

--- src/pages/BuyGMX/BuyGMX.js
+++ src/pages/BuyGMX/BuyGMX.js
@@ -1,6 +1,8 @@
 import React, { useCallback } from "react";
 import Footer from "components/Footer/Footer";
 import "./BuyGMX.css";
+import { SwapWidget, darkTheme } from "@uniswap/widgets";
+import "@uniswap/widgets/fonts.css";
 
 import { useWeb3React } from "@web3-react/core";
 
@@ -19,6 +21,57 @@ import Button from "components/Common/Button";
 import { ARBITRUM, AVALANCHE } from "config/chains";
 import { switchNetwork } from "lib/wallets";
 import { useChainId } from "lib/chains";
+import Card from "components/Common/Card";
+
+const uniswapWidgetTheme = {
+  ...darkTheme,
+  fontFamily: "Relative, sans-serif",
+  borderRadius: 0.4,
+  darkTheme: true,
+  container: "#20253f",
+  interactive: "#16182e",
+  module: "#222633",
+  accent: "#2d42fc",
+};
+
+const TOKEN_LIST = [
+  {
+    name: "GMX",
+    address: "0xfc5A1A6EB076a2C7aD06eD22C90d7E710E35ad0a",
+    symbol: "GMX",
+    decimals: 18,
+    chainId: 42161,
+    logoURI:
+      "https://github.com/trustwallet/assets/blob/master/blockchains/arbitrum/assets/0xfc5A1A6EB076a2C7aD06eD22C90d7E710E35ad0a/logo.png?raw=true",
+  },
+  {
+    name: "Dai Stablecoin",
+    address: "0xda10009cbd5d07dd0cecc66161fc93d7c9000da1",
+    symbol: "DAI",
+    decimals: 18,
+    chainId: 42161,
+    logoURI:
+      "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x6B175474E89094C44Da98b954EedeAC495271d0F/logo.png",
+  },
+  {
+    name: "Tether USD",
+    address: "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9",
+    symbol: "USDT",
+    decimals: 6,
+    chainId: 42161,
+    logoURI:
+      "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xdAC17F958D2ee523a2206206994597C13D831ec7/logo.png",
+  },
+  {
+    name: "USD Coin",
+    address: "0xff970a61a04b1ca14834a43f5de4533ebddb5cc8",
+    symbol: "USDC",
+    decimals: 6,
+    chainId: 42161,
+    logoURI:
+      "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png",
+  },
+];
 
 export default function BuyGMX() {
   const { chainId } = useChainId();
@@ -41,19 +94,96 @@ export default function BuyGMX() {
           <div className="section-title-block">
             <div className="section-title-content">
               <div className="Page-title">
-                <Trans>Buy / Transfer ETH</Trans>
+                <Trans>Buy GMX on Arbitrum</Trans>
               </div>
               <div className="Page-description">
-                <Trans>ETH is needed on Arbitrum to purchase GMX.</Trans>
+                <Trans>Choose to buy from decentralized or centralized exchanges.</Trans>
                 <br />
                 <Trans>
-                  To purchase GMX on <span onClick={() => onNetworkSelect(AVALANCHE)}>Avalanche</span>, please change
-                  your network.
+                  To purchase GMX on the Avalanche blockchain, please{" "}
+                  <span onClick={() => onNetworkSelect(AVALANCHE)}>change your network</span>.
                 </Trans>
               </div>
             </div>
           </div>
         )}
+        <div className="cards-row">
+          <Card title="Buy GMX on Uniswap Arbitrum">
+            <div>
+              <p className="card-description">
+                If you already have ETH on Arbitrum, you can directly purchase GMX on Uniswap. Set your network to
+                Arbitrum and use the widget below:
+              </p>
+              <div className="Uniswap">
+                <SwapWidget
+                  tokenList={TOKEN_LIST}
+                  width="100%"
+                  theme={uniswapWidgetTheme}
+                  defaultChainId={ARBITRUM}
+                  defaultOutputTokenAddress={{ [ARBITRUM]: "0xfc5A1A6EB076a2C7aD06eD22C90d7E710E35ad0a" }}
+                />
+              </div>
+            </div>
+          </Card>
+          <Card title="Buy GMX Bonds">
+            <div>
+              <p className="card-description">
+                GMX bonds can be bought on Olympus Pro on Arbitrum using ETH with a discount and a short vesting period.
+                Set your network to Arbitrum, then click the button below:
+              </p>
+              <div className="buy-gmx">
+                <Button size="xl" imgSrc={ohmArbitrum} href="https://pro.olympusdao.finance/#/partners/GMX">
+                  <Trans>Olympus Pro</Trans>
+                </Button>
+              </div>
+            </div>
+          </Card>
+        </div>
+        <div className="cards-row">
+          <Card title="Buy GMX from any network, token, or FIAT using centralized exchanges.">
+            <div className="App-card-content">
+              <div className="BuyGMXGLP-description">
+                <Trans>Buy GMX using multiple tokens from any network:</Trans>
+              </div>
+              <div className="bridge-options">
+                <Button
+                  href="https://synapseprotocol.com/?inputCurrency=ETH&outputCurrency=ETH&outputChain=42161"
+                  align="left"
+                  imgSrc={Synapse}
+                >
+                  <Trans>Synapse</Trans>
+                </Button>
+                <Button href="https://app.multichain.org/#/router" align="left" imgSrc={Multiswap}>
+                  <Trans>Multiswap</Trans>
+                </Button>
+                <Button
+                  href="https://app.hop.exchange/send?token=ETH&sourceNetwork=ethereum&destNetwork=arbitrum"
+                  align="left"
+                  imgSrc={Hop}
+                >
+                  <Trans>Hop</Trans>
+                </Button>
+                <Button href="https://binance.com/" align="left" imgSrc={Binance}>
+                  <Trans>Binance</Trans>
+                </Button>
+              </div>
+            </div>
+          </Card>
+          <Card title="Buy GMX Bonds">
+            <div>
+              <p className="card-description">
+                GMX bonds can be bought on Olympus Pro on Arbitrum using ETH with a discount and a short vesting period.
+                Set your network to Arbitrum, then click the button below:
+              </p>
+              <div className="buy-gmx">
+                <Button size="xl" imgSrc={ohmArbitrum} href="https://pro.olympusdao.finance/#/partners/GMX">
+                  <Trans>Olympus Pro</Trans>
+                </Button>
+              </div>
+            </div>
+          </Card>
+        </div>
+
         {chainId === ARBITRUM && (
           <div className="BuyGMXGLP-panel">
             <div className="App-card no-height">
@@ -120,7 +250,7 @@ export default function BuyGMX() {
           <div className="section-title-block">
             <div className="section-title-content">
               <div className="Page-title">
-                <Trans>Buy / Transfer AVAX</Trans>
+                <Trans>Buy GMX on Avalanche</Trans>
               </div>
               <div className="Page-description">
                 <Trans>Avax is needed on Avalanche to purchase GMX.</Trans>
--- src/pages/Jobs/Jobs.js
+++ src/pages/Jobs/Jobs.js
@@ -47,35 +47,35 @@ function NoJob() {
 
 // function JobCard() {
 //   return (
-//     <Card title="Senior front-end developer (Full-time position)">
-//       <div className="body-para">
-//         <p className="subheading">What you will do:</p>
-//         <ul>
-//           <li>Work closely with the GMX team on the GMX front-end website.</li>
-//           <li>Collaborate and discuss features to be worked on.</li>
-//           <li>Remote full-time position, flexible working hours.</li>
-//         </ul>
-//         <div className="mt-lg">
-//           <p className="subheading">What we are looking for:</p>
-//           <ul>
-//             <li>Required skills: HTML5, CSS3, React, Ethers, Web3 JS.</li>
-//             <li>Bonus skills: Node JS.</li>
-//             <li>5+ years of experience.</li>
-//             <li>Previous DeFi experience and knowledge.</li>
-//             <li>Must speak fluent English and available to start right away.</li>
-//             <li>Comfortable making changes to the interface following our current design guidelines.</li>
-//           </ul>
-//           <p>The salary is 60,000 to 120,000 USD + 1,000 to 3,000 GMX a year.</p>
-//           <p className="jobs-contact">
-//             If the job suits you, please get in touch with{" "}
-//             <a target="_blank" href="mailto:jobs@gmx.io" rel="noopener noreferrer">
-//               jobs@gmx.io
-//             </a>{" "}
-//             using the following email subject: Application for Senior front-end developer: [Your name]
-//           </p>
-//         </div>
-//       </div>
-//     </Card>
+// <Card title="Senior front-end developer (Full-time position)">
+//   <div className="body-para">
+//     <p className="subheading">What you will do:</p>
+//     <ul>
+//       <li>Work closely with the GMX team on the GMX front-end website.</li>
+//       <li>Collaborate and discuss features to be worked on.</li>
+//       <li>Remote full-time position, flexible working hours.</li>
+//     </ul>
+//     <div className="mt-lg">
+//       <p className="subheading">What we are looking for:</p>
+//       <ul>
+//         <li>Required skills: HTML5, CSS3, React, Ethers, Web3 JS.</li>
+//         <li>Bonus skills: Node JS.</li>
+//         <li>5+ years of experience.</li>
+//         <li>Previous DeFi experience and knowledge.</li>
+//         <li>Must speak fluent English and available to start right away.</li>
+//         <li>Comfortable making changes to the interface following our current design guidelines.</li>
+//       </ul>
+//       <p>The salary is 60,000 to 120,000 USD + 1,000 to 3,000 GMX a year.</p>
+//       <p className="jobs-contact">
+//         If the job suits you, please get in touch with{" "}
+//         <a target="_blank" href="mailto:jobs@gmx.io" rel="noopener noreferrer">
+//           jobs@gmx.io
+//         </a>{" "}
+//         using the following email subject: Application for Senior front-end developer: [Your name]
+//       </p>
+//     </div>
+//   </div>
+// </Card>
 //   );
 // }
 
