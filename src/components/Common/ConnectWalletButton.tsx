bdaba53b08ccc33ac7d7df3418b019aed067ead6
vipin <vipineth@gmail.com>
2022-10-28 14:09:18 +0530

    add connectwalletbutton css file

--- src/components/Common/ConnectWalletButton.tsx
+++ src/components/Common/ConnectWalletButton.tsx
@@ -1,6 +1,5 @@
 import { ReactNode } from "react";
-import cx from "classnames";
-import "./Button.css";
+import "./ConnectWalletButton.scss";
 
 type Props = {
   imgSrc: string;
@@ -10,10 +9,9 @@ type Props = {
 };
 
 export default function ConnectWalletButton({ imgSrc, children, onClick, className }: Props) {
-  let classNames = cx("btn btn-primary btn-sm connect-wallet", className);
   return (
-    <button className={classNames} onClick={onClick}>
-      {imgSrc && <img className="btn-image" src={imgSrc} alt="Connect Wallet" />}
+    <button className="connect-wallet-btn" onClick={onClick}>
+      {imgSrc && <img className="btn-icon" src={imgSrc} alt="Connect Wallet" />}
       <span className="btn-label">{children}</span>
     </button>
   );
