388d546d2c466242f8c18214449d917dc64bed27
divhead <i.divhead@gmail.com>
2022-11-17 06:28:05 +0400

    (synthetics-pools): fix balances

--- src/domain/tokens/useTokenBalances.ts
+++ src/domain/tokens/useTokenBalances.ts
@@ -1,22 +1,21 @@
 import { useWeb3React } from "@web3-react/core";
 import { getContract } from "config/contracts";
-import ReaderV2 from "abis/ReaderV2.json";
+import Reader from "abis/ReaderV2.json";
 import { useChainId } from "lib/chains";
 import { contractFetcher } from "lib/contracts";
-import { PLACEHOLDER_ACCOUNT } from "lib/legacy";
 import useSWR from "swr";
-import { BigNumber } from "ethers";
 
 export function useTokenBalances(p: { tokenAddresses: string[] }) {
   const { chainId } = useChainId();
   const { library, active, account } = useWeb3React();
   const readerAddress = getContract(chainId, "Reader");
 
-  const cacheKey = [`getTokenBalances`, active, chainId, readerAddress, account || PLACEHOLDER_ACCOUNT];
-
-  const { data, ...state } = useSWR<BigNumber[]>(cacheKey, {
-    fetcher: contractFetcher(library, ReaderV2, [p.tokenAddresses]),
-  });
+  const { data, ...state } = useSWR<any>(
+    active ? [active, chainId, readerAddress, "getTokenBalances", account] : null,
+    {
+      fetcher: contractFetcher(library, Reader, [p.tokenAddresses]),
+    }
+  );
 
   return {
     tokenBalances: data,
