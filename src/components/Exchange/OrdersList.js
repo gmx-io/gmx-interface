ebd043f26787b1921a03fb4ba51e0a1d9ab34ab3
vipin <vipineth@gmail.com>
2022-11-28 16:39:47 +0530

    hide user actions on actions page

--- src/components/Exchange/OrdersList.js
+++ src/components/Exchange/OrdersList.js
@@ -62,7 +62,7 @@ export default function OrdersList(props) {
     const isAllOrdersSelected = cancelOrderIdList?.length > 0 && cancelOrderIdList?.length === orders.length;
     return (
       <tr className="Exchange-list-header">
-        {orders.length > 0 && (
+        {!hideActions && orders.length > 0 && (
           <th>
             <div className="checkbox-inline ">
               <Checkbox
@@ -102,7 +102,7 @@ export default function OrdersList(props) {
         </th>
       </tr>
     );
-  }, [cancelOrderIdList, orders, setCancelOrderIdList]);
+  }, [cancelOrderIdList, orders, setCancelOrderIdList, hideActions]);
 
   const renderEmptyRow = useCallback(() => {
     if (orders && orders.length) {
@@ -158,22 +158,24 @@ export default function OrdersList(props) {
 
         return (
           <tr className="Exchange-list-item" key={orderId}>
-            <td>
-              <div className="checkbox-inline ">
-                <Checkbox
-                  isChecked={cancelOrderIdList?.includes(orderId)}
-                  setIsChecked={() => {
-                    setCancelOrderIdList((prevState) => {
-                      if (prevState.includes(orderId)) {
-                        return prevState.filter((i) => i !== orderId);
-                      } else {
-                        return prevState.concat(orderId);
-                      }
-                    });
-                  }}
-                />
-              </div>
-            </td>
+            {!hideActions && (
+              <td>
+                <div className="checkbox-inline ">
+                  <Checkbox
+                    isChecked={cancelOrderIdList?.includes(orderId)}
+                    setIsChecked={() => {
+                      setCancelOrderIdList((prevState) => {
+                        if (prevState.includes(orderId)) {
+                          return prevState.filter((i) => i !== orderId);
+                        } else {
+                          return prevState.concat(orderId);
+                        }
+                      });
+                    }}
+                  />
+                </div>
+              </td>
+            )}
             <td className="Exchange-list-item-type">
               <Trans>Limit</Trans>
             </td>
@@ -195,19 +197,23 @@ export default function OrdersList(props) {
               {toTokenInfo.symbol}
             </td>
             <td>
-              <Tooltip
-                handle={getExchangeRateDisplay(order.triggerRatio, fromTokenInfo, toTokenInfo)}
-                renderContent={() => t`
+              {!hideActions ? (
+                <Tooltip
+                  handle={getExchangeRateDisplay(order.triggerRatio, fromTokenInfo, toTokenInfo)}
+                  renderContent={() => t`
                   You will receive at least ${formatAmount(
                     order.minOut,
                     toTokenInfo.decimals,
                     toTokenInfo.isStable || toTokenInfo.isUsdg ? 2 : 4,
                     true
                   )} ${
-                  toTokenInfo.symbol
-                } if this order is executed. The execution price may vary depending on swap fees at the time the order is executed.
+                    toTokenInfo.symbol
+                  } if this order is executed. The execution price may vary depending on swap fees at the time the order is executed.
                 `}
-              />
+                />
+              ) : (
+                getExchangeRateDisplay(order.triggerRatio, fromTokenInfo, toTokenInfo)
+              )}
             </td>
             <td>{getExchangeRateDisplay(markExchangeRate, fromTokenInfo, toTokenInfo, true)}</td>
             {!hideActions && renderActions(order)}
@@ -239,22 +245,24 @@ export default function OrdersList(props) {
 
       return (
         <tr className="Exchange-list-item" key={`${order.isLong}-${order.type}-${order.index}`}>
-          <td className="Exchange-list-item-type">
-            <div>
-              <Checkbox
-                isChecked={cancelOrderIdList?.includes(orderId)}
-                setIsChecked={() => {
-                  setCancelOrderIdList((prevState) => {
-                    if (prevState.includes(orderId)) {
-                      return prevState.filter((i) => i !== orderId);
-                    } else {
-                      return prevState.concat(orderId);
-                    }
-                  });
-                }}
-              />
-            </div>
-          </td>
+          {!hideActions && (
+            <td className="Exchange-list-item-type">
+              <div>
+                <Checkbox
+                  isChecked={cancelOrderIdList?.includes(orderId)}
+                  setIsChecked={() => {
+                    setCancelOrderIdList((prevState) => {
+                      if (prevState.includes(orderId)) {
+                        return prevState.filter((i) => i !== orderId);
+                      } else {
+                        return prevState.concat(orderId);
+                      }
+                    });
+                  }}
+                />
+              </div>
+            </td>
+          )}
           <td className="Exchange-list-item-type">{order.type === INCREASE ? t`Limit` : t`Trigger`}</td>
           <td>
             {order.type === DECREASE ? (
--- src/components/Exchange/PositionsList.js
+++ src/components/Exchange/PositionsList.js
@@ -94,6 +94,7 @@ export default function PositionsList(props) {
     minExecutionFeeErrorMessage,
     usdgSupply,
     totalTokenWeights,
+    hideActions,
   } = props;
 
   const [positionToEditKey, setPositionToEditKey] = useState(undefined);
@@ -119,6 +120,7 @@ export default function PositionsList(props) {
   };
 
   const onPositionClick = (position) => {
+    if (hideActions) return;
     helperToast.success(
       t`${select(position.isLong, { true: "Long", false: "Short" })} ${position.indexToken.symbol} market selected`
     );
@@ -309,9 +311,11 @@ export default function PositionsList(props) {
                             );
                           }}
                         />
-                        <span className="edit-icon" onClick={() => editPosition(position)}>
-                          <AiOutlineEdit fontSize={16} />
-                        </span>
+                        {!hideActions && (
+                          <span className="edit-icon" onClick={() => editPosition(position)}>
+                            <AiOutlineEdit fontSize={16} />
+                          </span>
+                        )}
                       </div>
                     </div>
                     <div className="App-card-row">
@@ -432,33 +436,37 @@ export default function PositionsList(props) {
                       <div>${formatAmount(liquidationPrice, USD_DECIMALS, 2, true)}</div>
                     </div>
                   </div>
-                  <div className="App-card-divider"></div>
-                  <div className="App-card-options">
-                    <button
-                      className="App-button-option App-card-option"
-                      disabled={position.size.eq(0)}
-                      onClick={() => sellPosition(position)}
-                    >
-                      <Trans>Close</Trans>
-                    </button>
-                    <button
-                      className="App-button-option App-card-option"
-                      disabled={position.size.eq(0)}
-                      onClick={() => editPosition(position)}
-                    >
-                      <Trans>Edit Collateral</Trans>
-                    </button>
-                    <button
-                      className="Exchange-list-action App-button-option App-card-option"
-                      onClick={() => {
-                        setPositionToShare(position);
-                        setIsPositionShareModalOpen(true);
-                      }}
-                      disabled={position.size.eq(0)}
-                    >
-                      <Trans>Share</Trans>
-                    </button>
-                  </div>
+                  {!hideActions && (
+                    <>
+                      <div className="App-card-divider"></div>
+                      <div className="App-card-options">
+                        <button
+                          className="App-button-option App-card-option"
+                          disabled={position.size.eq(0)}
+                          onClick={() => sellPosition(position)}
+                        >
+                          <Trans>Close</Trans>
+                        </button>
+                        <button
+                          className="App-button-option App-card-option"
+                          disabled={position.size.eq(0)}
+                          onClick={() => editPosition(position)}
+                        >
+                          <Trans>Edit Collateral</Trans>
+                        </button>
+                        <button
+                          className="Exchange-list-action App-button-option App-card-option"
+                          onClick={() => {
+                            setPositionToShare(position);
+                            setIsPositionShareModalOpen(true);
+                          }}
+                          disabled={position.size.eq(0)}
+                        >
+                          <Trans>Share</Trans>
+                        </button>
+                      </div>
+                    </>
+                  )}
                 </div>
               );
             })}
@@ -489,8 +497,12 @@ export default function PositionsList(props) {
             <th>
               <Trans>Liq. Price</Trans>
             </th>
-            <th></th>
-            <th></th>
+            {!hideActions && (
+              <>
+                <th></th>
+                <th></th>
+              </>
+            )}
           </tr>
           {positions.length === 0 && positionsDataIsLoading && (
             <tr>
@@ -669,39 +681,49 @@ export default function PositionsList(props) {
                               label={t`Borrow Fee / Day`}
                               value={`-$${borrowFeeUSD}`}
                             />
-                            <br />
-                            <Trans>Use the Edit Collateral icon to deposit or withdraw collateral.</Trans>
+                            {!hideActions && (
+                              <>
+                                <br />
+                                <Trans>Use the Edit Collateral icon to deposit or withdraw collateral.</Trans>
+                              </>
+                            )}
                           </>
                         );
                       }}
                     />
-                    <span className="edit-icon" onClick={() => editPosition(position)}>
-                      <AiOutlineEdit fontSize={16} />
-                    </span>
+                    {!hideActions && (
+                      <span className="edit-icon" onClick={() => editPosition(position)}>
+                        <AiOutlineEdit fontSize={16} />
+                      </span>
+                    )}
                   </div>
                 </td>
                 <td className="clickable" onClick={() => onPositionClick(position)}>
-                  <Tooltip
-                    handle={`$${formatAmount(position.markPrice, USD_DECIMALS, 2, true)}`}
-                    position="left-bottom"
-                    handleClassName="plain clickable"
-                    renderContent={() => {
-                      return (
-                        <div>
-                          <Trans>
-                            Click on a row to select the position's market, then use the swap box to increase your
-                            position size if needed.
-                          </Trans>
-                          <br />
-                          <br />
-                          <Trans>
-                            Use the "Close" button to reduce your position size, or to set stop-loss / take-profit
-                            orders.
-                          </Trans>
-                        </div>
-                      );
-                    }}
-                  />
+                  {!hideActions ? (
+                    <Tooltip
+                      handle={`$${formatAmount(position.markPrice, USD_DECIMALS, 2, true)}`}
+                      position="left-bottom"
+                      handleClassName="plain clickable"
+                      renderContent={() => {
+                        return (
+                          <div>
+                            <Trans>
+                              Click on a row to select the position's market, then use the swap box to increase your
+                              position size if needed.
+                            </Trans>
+                            <br />
+                            <br />
+                            <Trans>
+                              Use the "Close" button to reduce your position size, or to set stop-loss / take-profit
+                              orders.
+                            </Trans>
+                          </div>
+                        );
+                      }}
+                    />
+                  ) : (
+                    <> ${formatAmount(position.markPrice, USD_DECIMALS, 2, true)}</>
+                  )}
                 </td>
                 <td className="clickable" onClick={() => onPositionClick(position)}>
                   ${formatAmount(position.averagePrice, USD_DECIMALS, 2, true)}
@@ -719,20 +741,22 @@ export default function PositionsList(props) {
                     <Trans>Close</Trans>
                   </button>
                 </td>
-                <td>
-                  <PositionDropdown
-                    handleEditCollateral={() => {
-                      editPosition(position);
-                    }}
-                    handleShare={() => {
-                      setPositionToShare(position);
-                      setIsPositionShareModalOpen(true);
-                    }}
-                    handleMarketSelect={() => {
-                      onPositionClick(position);
-                    }}
-                  />
-                </td>
+                {!hideActions && (
+                  <td>
+                    <PositionDropdown
+                      handleEditCollateral={() => {
+                        editPosition(position);
+                      }}
+                      handleShare={() => {
+                        setPositionToShare(position);
+                        setIsPositionShareModalOpen(true);
+                      }}
+                      handleMarketSelect={() => {
+                        onPositionClick(position);
+                      }}
+                    />
+                  </td>
+                )}
               </tr>
             );
           })}
