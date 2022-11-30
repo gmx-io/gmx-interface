a8ab7b842519dfc3457678c77d4b8f94cef15f6f
vipin <vipineth@gmail.com>
2022-11-30 20:51:53 +0530

    update time interval

--- src/components/TVChartContainer/useDatafeed.js
+++ src/components/TVChartContainer/useDatafeed.js
@@ -81,7 +81,7 @@ export default function useDatafeed() {
           getLiveBar({ chainId, ticker, resolution, isStable }).then((bar) => {
             onRealtimeCallback(bar);
           });
-        }, 5000);
+        }, 1000);
       },
       unsubscribeBars: () => {
         clearInterval(intervalRef.current);
