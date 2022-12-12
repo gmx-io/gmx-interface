2675420d1ec86b2ac1389c4419ea6e68656923ee
dang.duonghai <dang.duonghai@digipaysolution.com>
2022-12-13 01:00:05 +0700

    fix: mobile menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -117,8 +117,8 @@ export function Header({
           >
             <div className="App-header-container-left">
               <div className="App-header-menu-icon-block" onClick={() => setIsDrawerVisible(!isDrawerVisible)}>
-                {!isDrawerVisible && <RiMenuLine className="App-header-menu-icon" />}
-                {isDrawerVisible && <FaTimes className="App-header-menu-icon" />}
+                {!isDrawerVisible && <RiMenuLine className="App-header-menu-icon" color={"#717171"} />}
+                {isDrawerVisible && <FaTimes className="App-header-menu-icon" color={"#717171"} />}
               </div>
               <div className="App-header-link-main clickable" onClick={() => setIsDrawerVisible(!isDrawerVisible)}>
                 <img src={logoImg} className="big" alt="OPEN Logo" />
