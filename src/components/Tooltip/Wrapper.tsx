8db708a53d976708dcd1caccba605927ebbc245a
vipin <vipineth@gmail.com>
2022-12-18 12:57:24 +0530

    fix return from wrapper

--- src/components/Tooltip/Wrapper.tsx
+++ src/components/Tooltip/Wrapper.tsx
@@ -3,5 +3,5 @@ export default function Wrapper({ onDisabled, children }) {
     // For onMouseLeave to work on disabled button https://github.com/react-component/tooltip/issues/18#issuecomment-411476678
     return <div className="Tooltip-disabled-wrapper">{children}</div>;
   }
-  return children;
+  return <>{children}</>;
 }
