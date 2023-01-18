532fa0f179482120c57128933452f4f2dab89cc8
divhead <i.divhead@gmail.com>
2023-01-18 23:53:32 +0400

    (synthetics): refactor calculations

--- src/domain/synthetics/tokens/utils.ts
+++ src/domain/synthetics/tokens/utils.ts
@@ -1,50 +1,11 @@
 import { InfoTokens, TokenInfo } from "domain/tokens";
 import { BigNumber } from "ethers";
-import { convertToTokenAmount, convertToUsd } from "./amountUtils";
 import { TokenAllowancesData, TokenData, TokensData } from "./types";
 
 export function getTokenData(tokensData: TokensData, address?: string) {
   if (!address) return undefined;
 
   return tokensData[address];
-}
-
-export function getUsdFromTokenAmount(
-  tokensData: TokensData,
-  address: string | undefined,
-  amount: BigNumber | undefined,
-  priceType: "maxPrice" | "minPrice"
-) {
-  const tokenData = getTokenData(tokensData, address);
-
-  const prices = tokenData?.prices;
-
-  if (!tokenData || !prices || !amount) {
-    return undefined;
-  }
-
-  return convertToUsd(amount, tokenData.decimals, priceType === "maxPrice" ? prices.maxPrice : prices.minPrice);
-}
-
-export function getTokenAmountFromUsd(
-  tokensData: TokensData,
-  address: string | undefined,
-  usdAmount: BigNumber | undefined,
-  priceType: "maxPrice" | "minPrice"
-) {
-  const tokenData = getTokenData(tokensData, address);
-
-  const prices = tokenData?.prices;
-
-  if (!tokenData || !prices || !usdAmount) {
-    return undefined;
-  }
-
-  return convertToTokenAmount(
-    usdAmount,
-    tokenData.decimals,
-    priceType === "maxPrice" ? prices.maxPrice : prices.minPrice
-  );
 }
 
 export function getTokenAllowance(allowanceData: TokenAllowancesData, address?: string) {
