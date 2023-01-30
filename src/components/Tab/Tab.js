12570be82e7286130b6592b3bb0ff6381739acba
vipin <vipineth@gmail.com>
2023-01-30 22:08:29 +0530

    add animation for inline and block tab

--- src/components/Tab/Tab.js
+++ src/components/Tab/Tab.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { motion, AnimateSharedLayout } from "framer-motion";
 import cx from "classnames";
 
 import "./Tab.css";
@@ -17,15 +17,32 @@ export default function Tab(props) {
 
   return (
     <div className={cx("Tab", type, className)}>
-      {options.map((opt) => {
-        const label = optionLabels && optionLabels[opt] ? optionLabels[opt] : opt;
-        return (
-          <div className={cx("Tab-option", "muted", { active: opt === option })} onClick={() => onClick(opt)} key={opt}>
-            {icons && icons[opt] && <img className="Tab-option-icon" src={icons[opt]} alt={option} />}
-            {label}
-          </div>
-        );
-      })}
+      <AnimateSharedLayout>
+        {options.map((opt) => {
+          const label = optionLabels && optionLabels[opt] ? optionLabels[opt] : opt;
+          return (
+            <div
+              className={cx("Tab-option", "muted", { active: opt === option })}
+              onClick={() => onClick(opt)}
+              key={opt}
+            >
+              {type === "block" && opt === option && (
+                <motion.div
+                  className="Tab-block-slide"
+                  layoutId="block-slide"
+                  animate={{ backgroundColor: "rgba(180, 187, 255, 0.2)" }}
+                  transition={{ duration: 0.25 }}
+                />
+              )}
+              {icons && icons[opt] && <img className="Tab-option-icon" src={icons[opt]} alt={option} />}
+              {label}
+              {type === "inline" && opt === option && (
+                <motion.div className="Tab-inline-slide" layoutId="inline-slide" />
+              )}
+            </div>
+          );
+        })}
+      </AnimateSharedLayout>
     </div>
   );
 }
