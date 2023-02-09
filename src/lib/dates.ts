7d47f2a93eecca9d0dd404358b4a6839f2937471
Vipin <73279781+vipineth@users.noreply.github.com>
2023-02-09 16:32:32 +0530

    Revert "TradingView Implementation"

--- src/lib/dates.ts
+++ src/lib/dates.ts
@@ -1,4 +1,4 @@
-import { addMinutes, format as formatDateFn } from "date-fns";
+import { format as formatDateFn } from "date-fns";
 
 export function formatDateTime(time: number) {
   return formatDateFn(time * 1000, "dd MMM yyyy, h:mm a");
@@ -6,15 +6,6 @@ export function formatDateTime(time: number) {
 
 export function formatDate(time: number) {
   return formatDateFn(time * 1000, "dd MMM yyyy");
-}
-
-export function formatTVDate(date: Date) {
-  // https://github.com/date-fns/date-fns/issues/1401#issuecomment-578580199
-  return formatDateFn(addMinutes(date, date.getTimezoneOffset()), "dd MMM yyyy");
-}
-
-export function formatTVTime(date: Date) {
-  return formatDateFn(addMinutes(date, date.getTimezoneOffset()), "h:mm a");
 }
 
 export function getTimeRemaining(time: number) {
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -3,7 +3,6 @@ import "@web3-react";
 declare global {
   interface Window {
     ethereum?: any;
-    TradingView?: any;
   }
   interface Navigator {
     msSaveBlob?: (blob: any, defaultName?: string) => boolean;
