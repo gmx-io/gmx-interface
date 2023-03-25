4658791aa0958fdd7bce27a9af00bb6ac276a599
vipin <vipineth@gmail.com>
2023-03-25 21:27:04 +0530

    add seperator for inline tab

--- src/components/Tab/Tab.js
+++ src/components/Tab/Tab.js
@@ -20,9 +20,13 @@ export default function Tab(props) {
       {options.map((opt) => {
         const label = optionLabels && optionLabels[opt] ? optionLabels[opt] : opt;
         return (
-          <div className={cx("Tab-option", "muted", { active: opt === option })} onClick={() => onClick(opt)} key={opt}>
+          <div
+            className={cx("Tab-option", { active: opt === option, muted: type === "block" })}
+            onClick={() => onClick(opt)}
+            key={opt}
+          >
             {icons && icons[opt] && <img className="Tab-option-icon" src={icons[opt]} alt={option} />}
-            {label}
+            {type === "inline" ? <span className="muted">{label}</span> : label}
           </div>
         );
       })}
