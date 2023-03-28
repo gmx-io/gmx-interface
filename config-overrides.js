601d3cdbfcbffad58a6e6d88d624db792f311f7d
vipin <vipineth@gmail.com>
2023-03-28 22:10:26 +0530

    remove react-app-rewired

--- config-overrides.js
+++
@@ -1,7 +0,0 @@
-const path = require("path");
-const { override } = require("customize-cra");
-const { babelInclude } = require("customize-cra");
-
-module.exports = override(
-  babelInclude([path.resolve("src"), path.resolve("node_modules/wagmi"), path.resolve("node_modules/@wagmi")])
-);
