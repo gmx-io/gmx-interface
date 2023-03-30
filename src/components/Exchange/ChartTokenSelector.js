ba020ec08491e1620bb744809c904ffa8e9d3b78
vipin <vipineth@gmail.com>
2023-03-30 15:17:22 +0530

    add new tokenselector

--- src/components/Exchange/ChartTokenSelector.js
+++ src/components/Exchange/ChartTokenSelector.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Menu } from "@headlessui/react";
 import { FaChevronDown } from "react-icons/fa";
 import cx from "classnames";
-import "./ChartTokenSelector.css";
-import { LONG, SHORT, SWAP } from "lib/legacy";
+import "./ChartTokenSelector.scss";
+import { LONG, SHORT, SWAP, USD_DECIMALS } from "lib/legacy";
 import { getTokens, getWhitelistedTokens } from "config/tokens";
+import SearchInput from "./SearchInput";
+import { formatAmount } from "lib/numbers";
+
+function addLiquidityToTokens(tokens, infoTokens) {
+  if (!infoTokens) return tokens;
+  return tokens.map((token) => {
+    return {
+      ...token,
+      maxAvailableLong: infoTokens[token.address]?.maxAvailableLong,
+      maxAvailableShort: infoTokens[token.address]?.maxAvailableShort,
+    };
+  });
+}
 
 export default function ChartTokenSelector(props) {
-  const { chainId, selectedToken, onSelectToken, swapOption } = props;
+  const { chainId, selectedToken, onSelectToken, swapOption, infoTokens } = props;
+  const [searchKeyword, setSearchKeyword] = useState("");
 
   const isLong = swapOption === LONG;
   const isShort = swapOption === SHORT;
@@ -17,44 +31,82 @@ export default function ChartTokenSelector(props) {
   const whitelistedTokens = getWhitelistedTokens(chainId);
   const indexTokens = whitelistedTokens.filter((token) => !token.isStable && !token.isWrapped);
   const shortableTokens = indexTokens.filter((token) => token.isShortable);
-
   if (isLong) {
-    options = indexTokens;
+    options = addLiquidityToTokens(indexTokens, infoTokens);
   }
   if (isShort) {
-    options = shortableTokens;
+    options = addLiquidityToTokens(shortableTokens, infoTokens);
   }
 
   const onSelect = async (token) => {
     onSelectToken(token);
   };
 
-  var value = selectedToken;
+  const filteredTokens = options.filter((item) => {
+    return (
+      item.name.toLowerCase().indexOf(searchKeyword.toLowerCase()) > -1 ||
+      item.symbol.toLowerCase().indexOf(searchKeyword.toLowerCase()) > -1
+    );
+  });
+
+  const _handleKeyDown = (e) => {
+    if (e.key === "Enter" && filteredTokens.length > 0) {
+      onSelectToken(filteredTokens[0]);
+    }
+  };
 
   return (
     <Menu>
       <Menu.Button as="div" disabled={isSwap}>
         <button className={cx("App-cta small transparent chart-token-selector", { "default-cursor": isSwap })}>
-          <span className="chart-token-selector--current">{value.symbol} / USD</span>
+          <span className="chart-token-selector--current">{selectedToken.symbol} / USD</span>
           {!isSwap && <FaChevronDown />}
         </button>
       </Menu.Button>
       <div className="chart-token-menu">
         <Menu.Items as="div" className="menu-items chart-token-menu-items">
-          {options.map((option, index) => (
-            <Menu.Item key={index}>
-              <div
-                className="menu-item"
-                onClick={() => {
-                  onSelect(option);
-                }}
-              >
-                <span style={{ marginLeft: 5 }} className="token-label">
-                  {option.symbol} / USD
-                </span>
-              </div>
-            </Menu.Item>
-          ))}
+          <SearchInput
+            className="m-md"
+            value={searchKeyword}
+            setValue={({ target }) => setSearchKeyword(target.value)}
+            onKeyDown={_handleKeyDown}
+          />
+          <div className="divider" />
+          <div className="chart-token-list">
+            <table>
+              <thead>
+                <tr>
+                  <th>Market</th>
+                  <th>Long Liquidity</th>
+                  <th>Short Liquidity</th>
+                </tr>
+              </thead>
+              <tbody>
+                {filteredTokens.map((option, index) => {
+                  return (
+                    <Menu.Item key={index}>
+                      <tr className="">
+                        <td
+                          className="token-item"
+                          onClick={() => {
+                            onSelect(option);
+                          }}
+                        >
+                          <span> {option.symbol} / USD</span>
+                        </td>
+                        <td>
+                          <span>${formatAmount(option.maxAvailableLong, USD_DECIMALS, 2, true)}</span>
+                        </td>
+                        <td>
+                          <span>${formatAmount(option.maxAvailableShort, USD_DECIMALS, 2, true)}</span>
+                        </td>
+                      </tr>
+                    </Menu.Item>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
         </Menu.Items>
       </div>
     </Menu>
