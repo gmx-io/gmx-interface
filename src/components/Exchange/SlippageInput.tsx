929d74eeac299afc3e1e2b0b145be6ce4b083dc9
vipin <vipineth@gmail.com>
2023-04-18 16:28:34 +0530

    update style for slippage

--- src/components/Exchange/SlippageInput.tsx
+++ src/components/Exchange/SlippageInput.tsx
@@ -3,32 +3,39 @@ import "./SlippageInput.scss";
 import { useState } from "react";
 import cx from "classnames";
 
-const MAX_SLIPPAGE = 500;
+const MAX_SLIPPAGE = 100 * 100;
 
 export default function SlippageInput({ placeholderValue, update }) {
   const placeHolderValue = formatAmount(placeholderValue, 2, 2);
-  const [slippageInput, setSlippageInput] = useState("");
+
+  const [slippageInput, setSlippageInput] = useState<number | string>(placeHolderValue);
   const [slippageError, setSlippageError] = useState("");
-  function handleChange(value: string) {
-    setSlippageInput(value);
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     setSlippageError("");
-    const parsed = Math.floor(Number.parseFloat(value) * 100);
-    if (!Number.isInteger(parsed) || parsed < 0 || parsed > MAX_SLIPPAGE) {
-      if (value !== ".") {
-        setSlippageError("Invalid input");
-      }
-    } else {
-      update(parsed);
+    const { value } = event.target;
+
+    if (value === "") {
+      setSlippageInput(value);
+      return;
     }
+
+    const parsedValue = Math.round(Number.parseFloat(value) * 100);
+    if (Number.isNaN(parsedValue)) {
+      return;
+    }
+
+    if (parsedValue >= MAX_SLIPPAGE) {
+      setSlippageError("Slippage must be less than 100%");
+      return;
+    }
+
+    setSlippageInput(value);
   }
 
   return (
     <div className={cx("Slippage-input", { "input-error": slippageError })}>
-      <input
-        value={slippageInput.length > 0 ? slippageInput : ""}
-        placeholder={placeHolderValue}
-        onChange={(e) => handleChange(e.target.value)}
-      />
+      <input value={!!slippageInput ? slippageInput : ""} placeholder={placeHolderValue} onChange={handleChange} />
       <span>%</span>
     </div>
   );
