8b4af06a716f1fdab3442a3326ffc6ccc29ff279
vipin <vipineth@gmail.com>
2023-04-19 22:45:21 +0530

    add slippage list on hover

--- src/components/Exchange/SlippageInput.tsx
+++ src/components/Exchange/SlippageInput.tsx
@@ -5,6 +5,7 @@ import { formatAmount } from "lib/numbers";
 
 const MAX_SLIPPAGE = 99 * 100;
 const HIGH_SLIPPAGE = 2 * 100;
+const SLIPPAGE_LISTS = [0.3, 0.5, 1, 1.5];
 
 function getSlippageText(value: number) {
   return formatAmount(value, 2, 2);
@@ -13,6 +14,7 @@ function getSlippageText(value: number) {
 export default function SlippageInput({ setAllowedSlippage, defaultSlippage }) {
   const [slippageText, setSlippageText] = useState(getSlippageText(defaultSlippage));
   const [slippageError, setSlippageError] = useState("");
+  const [isPanelVisible, setIsPanelVisible] = useState(true);
 
   function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     setSlippageError("");
@@ -44,9 +46,33 @@ export default function SlippageInput({ setAllowedSlippage, defaultSlippage }) {
   }
 
   return (
-    <div className={cx("Slippage-input", { "input-error": slippageError })}>
-      <input value={!!slippageText ? slippageText : ""} placeholder={String(slippageText)} onChange={handleChange} />
-      <span>%</span>
+    <div className="Slippage-input-wrapper">
+      <div className={cx("Slippage-input", { "input-error": slippageError })}>
+        <input
+          onFocus={() => setIsPanelVisible(true)}
+          onBlur={() => setTimeout(() => setIsPanelVisible(false), 100)}
+          value={!!slippageText ? slippageText : ""}
+          placeholder={String(slippageText)}
+          onChange={handleChange}
+        />
+        <span>%</span>
+      </div>
+      {isPanelVisible && (
+        <ul className="Slippage-list">
+          {SLIPPAGE_LISTS.map((slippage) => (
+            <li
+              key={slippage}
+              onClick={() => {
+                setSlippageText(String(slippage));
+                setAllowedSlippage(slippage * 100);
+                setIsPanelVisible(false);
+              }}
+            >
+              {slippage}%
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
