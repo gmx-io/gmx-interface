60328101e5a0ffbf5cc58027564b7cefe858e0fa
vipin <vipineth@gmail.com>
2023-04-29 09:56:22 +0530

    change to HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "regenerator-runtime/runtime";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import App from "./App/App";
 import reportWebVitals from "./reportWebVitals";
 
