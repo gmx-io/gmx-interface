22173ecb1c26778a3962d622b146a3d796407ba7
vipin <vipineth@gmail.com>
2023-05-05 08:58:07 +0530

    test to re-build

--- src/index.tsx
+++ src/index.tsx
@@ -14,6 +14,7 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+// test here
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.info))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
