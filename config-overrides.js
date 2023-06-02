303b0e2d9ff1036d783ae3373ca321c3a5a37ce0
vipin <vipineth@gmail.com>
2023-06-02 17:11:05 +0530

    add po files loader

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,16 @@
-module.exports = function override(config, env) {
+module.exports = function override(config) {
+  // https://github.com/lingui/js-lingui/issues/1195
+  // Adding loader to use for .po files to webpack
+  const loaders = config.module.rules[1].oneOf;
+  loaders.splice(loaders.length - 1, 0, {
+    test: /\.po/,
+    use: [
+      {
+        loader: "@lingui/loader",
+      },
+    ],
+  });
+
   config.resolve.fallback = {
     os: false,
     http: false,
