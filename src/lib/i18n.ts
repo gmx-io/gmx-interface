7b059112d0987ffd79477dc8f5280e8494eac6f0
vipin <vipineth@gmail.com>
2023-06-02 18:40:15 +0530

    remove language list

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -34,85 +34,8 @@ export function isTestLanguage(locale: string) {
   return locale === "pseudo";
 }
 
-// export async function dynamicActivate(locale: string) {
-//   const { messages } = await import(`@lingui/loader!locales/${locale}/messages.po`);
-// if (!isTestLanguage(locale)) {
-//   localStorage.setItem(LANGUAGE_LOCALSTORAGE_KEY, locale);
-// }
-// i18n.load(locale, messages);
-// i18n.activate(locale);
-// }
-
-const catalogs: Record<string, () => Promise<Messages>> = {
-  en: async () => {
-    const { messages } = await import(
-      // @ts-ignore
-      `./file.js!=!@lingui/loader!../locales/en/messages.po`
-    );
-    return messages;
-  },
-  es: async () => {
-    const { messages } = await import(
-      // @ts-ignore
-      `./file.js!=!@lingui/loader!../locales/es/messages.po`
-    );
-    return messages;
-  },
-  zh: async () => {
-    const { messages } = await import(
-      // @ts-ignore
-      `./file.js!=!@lingui/loader!../locales/zh/messages.po`
-    );
-    return messages;
-  },
-  ko: async () => {
-    const { messages } = await import(
-      // @ts-ignore
-      `./file.js!=!@lingui/loader!../locales/ko/messages.po`
-    );
-    return messages;
-  },
-  ru: async () => {
-    const { messages } = await import(
-      // @ts-ignore
-      `./file.js!=!@lingui/loader!../locales/ru/messages.po`
-    );
-    return messages;
-  },
-  ja: async () => {
-    const { messages } = await import(
-      // @ts-ignore
-      `./file.js!=!@lingui/loader!../locales/ja/messages.po`
-    );
-    return messages;
-  },
-  fr: async () => {
-    const { messages } = await import(
-      // @ts-ignore
-      `./file.js!=!@lingui/loader!../locales/fr/messages.po`
-    );
-    return messages;
-  },
-  de: async () => {
-    const { messages } = await import(
-      // @ts-ignore
-      `./file.js!=!@lingui/loader!../locales/de/messages.po`
-    );
-    return messages;
-  },
-  ...(isDevelopment() && {
-    pseudo: async () => {
-      const { messages } = await import(
-        // @ts-ignore
-        `./file.js!=!@lingui/loader!../locales/pseudo/messages.po`
-      );
-      return messages;
-    },
-  }),
-};
-
 export async function dynamicActivate(locale: string) {
-  const messages = await catalogs[locale as any]();
+  const { messages } = await import(`@lingui/loader!locales/${locale}/messages.po`);
   if (!isTestLanguage(locale)) {
     localStorage.setItem(LANGUAGE_LOCALSTORAGE_KEY, locale);
   }
