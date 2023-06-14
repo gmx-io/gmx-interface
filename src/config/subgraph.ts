5e4a3919e50f38e8f3aa5bee80eb8ea56a1c7ce7
divhead <i.divhead@gmail.com>
2023-06-14 19:30:02 +0400

    (synthetics): upd subgraphs

--- src/config/subgraph.ts
+++ src/config/subgraph.ts
@@ -6,10 +6,7 @@ const SUBGRAPH_URLS = {
   [ARBITRUM]: {
     // stats: "https://api.thegraph.com/subgraphs/name/gdev8317/gmx-arbitrum-stats
     stats: "https://api.thegraph.com/subgraphs/name/gmx-io/gmx-stats",
-    // referrals: "https://api.thegraph.com/subgraphs/name/gmx-io/gmx-arbitrum-referrals",
-    // TODO: Test refferals updates
-    referrals:
-      "https://subgraph.satsuma-prod.com/3b2ced13c8d9/gmx/gmx-arbitrum-referrals/version/synts-stats-230509054500-2deb6a4/api",
+    referrals: "https://subgraph.satsuma-prod.com/3b2ced13c8d9/gmx/gmx-arbitrum-referrals/api",
     nissohVault: "https://api.thegraph.com/subgraphs/name/nissoh/gmx-vault",
   },
 
@@ -22,18 +19,13 @@ const SUBGRAPH_URLS = {
   [AVALANCHE]: {
     // stats: "https://api.thegraph.com/subgraphs/name/gdev8317/gmx-avalanche-staging", // testing
     stats: "https://api.thegraph.com/subgraphs/name/gmx-io/gmx-avalanche-stats",
-    // TODO: Test refferals updates
-    // referrals: "https://api.thegraph.com/subgraphs/name/gmx-io/gmx-avalanche-referrals",
-    referrals:
-      "https://subgraph.satsuma-prod.com/3b2ced13c8d9/gmx/gmx-avalanche-referrals/version/synts-stats-230509054250-2deb6a4/api",
+    referrals: "https://subgraph.satsuma-prod.com/3b2ced13c8d9/gmx/gmx-avalanche-referrals/api",
   },
 
   [AVALANCHE_FUJI]: {
     stats: "https://api.thegraph.com/subgraphs/name/gmx-io/gmx-avalanche-stats",
-    referrals:
-      "https://subgraph.satsuma-prod.com/3b2ced13c8d9/gmx/gmx-fuji-referrals/version/synts-stats-230601004554-4a53c5d/api",
-    syntheticsStats:
-      "https://subgraph.satsuma-prod.com/3b2ced13c8d9/gmx/synthetics-fuji-stats/version/synts-stats-230601004808-4a53c5d/api",
+    referrals: "https://subgraph.satsuma-prod.com/3b2ced13c8d9/gmx/gmx-fuji-referrals/api",
+    syntheticsStats: "https://subgraph.satsuma-prod.com/3b2ced13c8d9/gmx/synthetics-fuji-stats/api",
   },
 
   common: {
