4bd8020a9772d5d4859c22fd1b286930d2a0e5bf
divhead <i.divhead@gmail.com>
2023-07-07 02:27:07 +0400

    (synthetics): fix validation

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -160,8 +160,8 @@ export const RPC_PROVIDERS = {
   [TESTNET]: ["https://data-seed-prebsc-1-s1.binance.org:8545/"],
   [ARBITRUM]: [getDefaultArbitrumRpcUrl()],
   [ARBITRUM_GOERLI]: [
-    "https://goerli-rollup.arbitrum.io/rpc",
-    // "https://arb-goerli.g.alchemy.com/v2/cZfd99JyN42V9Clbs_gOvA3GSBZH1-1j",
+    // "https://goerli-rollup.arbitrum.io/rpc",
+    "https://arb-goerli.g.alchemy.com/v2/cZfd99JyN42V9Clbs_gOvA3GSBZH1-1j",
     // "https://endpoints.omniatech.io/v1/arbitrum/goerli/public",
     // "https://arbitrum-goerli.public.blastapi.io",
   ],
