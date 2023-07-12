8d0ca8f96a4cf90587a961d194d3eb56f8b77d52
divhead <i.divhead@gmail.com>
2023-07-12 16:30:55 +0400

    (synthetics): mainnet

--- src/config/features.ts
+++ src/config/features.ts
@@ -1,10 +1,3 @@
-import { ARBITRUM_GOERLI, AVALANCHE_FUJI } from "./chains";
-import { isDevelopment } from "./env";
-
 export function getIsSyntheticsSupported(chainId: number) {
-  if (isDevelopment()) {
-    return [AVALANCHE_FUJI, ARBITRUM_GOERLI].includes(chainId);
-  }
-
-  return false;
+  return true;
 }
