e58eefea2a4cee71e42cbbae1f38c604ab99efcb
divhead <i.divhead@gmail.com>
2023-07-26 13:27:30 +0400

    (synthetics): disable simulations

--- src/domain/synthetics/orders/simulateExecuteOrderTxn.tsx
+++ src/domain/synthetics/orders/simulateExecuteOrderTxn.tsx
@@ -1,18 +1,6 @@
 import { Web3Provider } from "@ethersproject/providers";
-import { Trans, t } from "@lingui/macro";
-import CustomErrors from "abis/CustomErrors.json";
-import DataStore from "abis/DataStore.json";
-import ExchangeRouter from "abis/ExchangeRouter.json";
-import { ToastifyDebug } from "components/ToastifyDebug/ToastifyDebug";
-import { getContract } from "config/contracts";
-import { NONCE_KEY, orderKey } from "config/dataStore";
-import { isDevelopment } from "config/env";
-import { convertTokenAddress } from "config/tokens";
-import { TokenPrices, TokensData, convertToContractPrice, getTokenData } from "domain/synthetics/tokens";
-import { BigNumber, ethers } from "ethers";
-import { callContract } from "lib/contracts";
-import { getErrorMessage } from "lib/contracts/transactionErrors";
-import { helperToast } from "lib/helperToast";
+import { TokenPrices, TokensData } from "domain/synthetics/tokens";
+import { BigNumber } from "ethers";
 
 export type MulticallRequest = { method: string; params: any[] }[];
 
@@ -30,165 +18,167 @@ type SimulateExecuteOrderParams = {
 };
 
 // only for debugging empty reverts
-const RUN_ON_CHAIN = false;
+// const RUN_ON_CHAIN = false;
 
 export async function simulateExecuteOrderTxn(chainId: number, library: Web3Provider, p: SimulateExecuteOrderParams) {
-  const dataStoreAddress = getContract(chainId, "DataStore");
+  // TODO: fix Rabby
+  return;
+  // const dataStoreAddress = getContract(chainId, "DataStore");
 
-  const dataStore = new ethers.Contract(dataStoreAddress, DataStore.abi, library.getSigner());
+  // const dataStore = new ethers.Contract(dataStoreAddress, DataStore.abi, library.getSigner());
 
-  const exchangeRouter = new ethers.Contract(
-    getContract(chainId, "ExchangeRouter"),
-    ExchangeRouter.abi,
-    library.getSigner()
-  );
+  // const exchangeRouter = new ethers.Contract(
+  //   getContract(chainId, "ExchangeRouter"),
+  //   ExchangeRouter.abi,
+  //   library.getSigner()
+  // );
 
-  const blockNumber = await library.getBlockNumber();
-  const nonce = await dataStore.getUint(NONCE_KEY, { blockTag: blockNumber });
-  const nextNonce = nonce.add(1);
-  const nextKey = orderKey(dataStoreAddress, nextNonce);
+  // const blockNumber = await library.getBlockNumber();
+  // const nonce = await dataStore.getUint(NONCE_KEY, { blockTag: blockNumber });
+  // const nextNonce = nonce.add(1);
+  // const nextKey = orderKey(dataStoreAddress, nextNonce);
 
-  const { primaryTokens, primaryPrices, secondaryTokens, secondaryPrices } = getSimulationPrices(
-    chainId,
-    p.tokensData,
-    p.primaryPriceOverrides,
-    p.secondaryPriceOverrides
-  );
+  // const { primaryTokens, primaryPrices, secondaryTokens, secondaryPrices } = getSimulationPrices(
+  //   chainId,
+  //   p.tokensData,
+  //   p.primaryPriceOverrides,
+  //   p.secondaryPriceOverrides
+  // );
 
-  const simulationPayload = [
-    ...p.createOrderMulticallPayload,
-    exchangeRouter.interface.encodeFunctionData(p.method || "simulateExecuteOrder", [
-      nextKey,
-      {
-        primaryTokens: primaryTokens,
-        primaryPrices: primaryPrices,
-        secondaryTokens: secondaryTokens,
-        secondaryPrices: secondaryPrices,
-      },
-    ]),
-  ];
+  // const simulationPayload = [
+  //   ...p.createOrderMulticallPayload,
+  //   exchangeRouter.interface.encodeFunctionData(p.method || "simulateExecuteOrder", [
+  //     nextKey,
+  //     {
+  //       primaryTokens: primaryTokens,
+  //       primaryPrices: primaryPrices,
+  //       secondaryTokens: secondaryTokens,
+  //       secondaryPrices: secondaryPrices,
+  //     },
+  //   ]),
+  // ];
 
-  try {
-    if (RUN_ON_CHAIN && isDevelopment()) {
-      const txn = await callContract(chainId, exchangeRouter, "multicall", [simulationPayload], {
-        value: p.value,
-        gasLimit: 10 ** 7,
-      });
+  // try {
+  //   if (RUN_ON_CHAIN && isDevelopment()) {
+  //     const txn = await callContract(chainId, exchangeRouter, "multicall", [simulationPayload], {
+  //       value: p.value,
+  //       gasLimit: 10 ** 7,
+  //     });
 
-      throw new Error(`debug simulation ended ${txn.hash}`);
-    } else {
-      await exchangeRouter.callStatic.multicall(simulationPayload, {
-        value: p.value,
-        blockTag: blockNumber,
-        gasLimit: 10 ** 7,
-      });
-    }
-  } catch (txnError) {
-    const customErrors = new ethers.Contract(ethers.constants.AddressZero, CustomErrors.abi);
+  //     throw new Error(`debug simulation ended ${txn.hash}`);
+  //   } else {
+  //     await exchangeRouter.callStatic.multicall(simulationPayload, {
+  //       value: p.value,
+  //       blockTag: blockNumber,
+  //       gasLimit: 10 ** 7,
+  //     });
+  //   }
+  // } catch (txnError) {
+  //   const customErrors = new ethers.Contract(ethers.constants.AddressZero, CustomErrors.abi);
 
-    let msg: any = undefined;
+  //   let msg: any = undefined;
 
-    try {
-      const parsedError = customErrors.interface.parseError(txnError.data?.data);
-      const isSimulationPassed = parsedError.name === "EndOfOracleSimulation";
+  //   try {
+  //     const parsedError = customErrors.interface.parseError(txnError.data?.data);
+  //     const isSimulationPassed = parsedError.name === "EndOfOracleSimulation";
 
-      if (isSimulationPassed) {
-        return;
-      }
+  //     if (isSimulationPassed) {
+  //       return;
+  //     }
 
-      const parsedArgs = Object.keys(parsedError.args).reduce((acc, k) => {
-        if (!Number.isNaN(Number(k))) {
-          return acc;
-        }
-        acc[k] = parsedError.args[k].toString();
-        return acc;
-      }, {});
+  //     const parsedArgs = Object.keys(parsedError.args).reduce((acc, k) => {
+  //       if (!Number.isNaN(Number(k))) {
+  //         return acc;
+  //       }
+  //       acc[k] = parsedError.args[k].toString();
+  //       return acc;
+  //     }, {});
 
-      msg = (
-        <div>
-          <Trans>Execute order simulation failed.</Trans>
-          <br />
-          <ToastifyDebug>
-            {parsedError.name} {JSON.stringify(parsedArgs, null, 2)}
-          </ToastifyDebug>
-        </div>
-      );
-    } catch (e) {
-      const walletErrorMessage = getErrorMessage(chainId, txnError, t`Execute order simulation failed.`);
-      msg = walletErrorMessage.failMsg;
-    }
+  //     msg = (
+  //       <div>
+  //         <Trans>Execute order simulation failed.</Trans>
+  //         <br />
+  //         <ToastifyDebug>
+  //           {parsedError.name} {JSON.stringify(parsedArgs, null, 2)}
+  //         </ToastifyDebug>
+  //       </div>
+  //     );
+  //   } catch (e) {
+  //     const walletErrorMessage = getErrorMessage(chainId, txnError, t`Execute order simulation failed.`);
+  //     msg = walletErrorMessage.failMsg;
+  //   }
 
-    if (!msg) {
-      msg = (
-        <div>
-          <Trans>Execute order simulation failed.</Trans>
-          <br />
-          <ToastifyDebug>Unknown Error</ToastifyDebug>
-        </div>
-      );
-    }
+  //   if (!msg) {
+  //     msg = (
+  //       <div>
+  //         <Trans>Execute order simulation failed.</Trans>
+  //         <br />
+  //         <ToastifyDebug>Unknown Error</ToastifyDebug>
+  //       </div>
+  //     );
+  //   }
 
-    helperToast.error(msg);
+  //   helperToast.error(msg);
 
-    throw txnError;
-  }
+  //   throw txnError;
+  // }
 }
 
-function getSimulationPrices(
-  chainId: number,
-  tokensData: TokensData,
-  primaryPricesMap: PriceOverrides,
-  secondaryPricesMap: PriceOverrides
-) {
-  const tokenAddresses = Object.keys(tokensData);
+// function getSimulationPrices(
+//   chainId: number,
+//   tokensData: TokensData,
+//   primaryPricesMap: PriceOverrides,
+//   secondaryPricesMap: PriceOverrides
+// ) {
+//   const tokenAddresses = Object.keys(tokensData);
 
-  const primaryTokens: string[] = [];
-  const primaryPrices: { min: BigNumber; max: BigNumber }[] = [];
-  const secondaryPrices: { min: BigNumber; max: BigNumber }[] = [];
+//   const primaryTokens: string[] = [];
+//   const primaryPrices: { min: BigNumber; max: BigNumber }[] = [];
+//   const secondaryPrices: { min: BigNumber; max: BigNumber }[] = [];
 
-  for (const address of tokenAddresses) {
-    const token = getTokenData(tokensData, address);
+//   for (const address of tokenAddresses) {
+//     const token = getTokenData(tokensData, address);
 
-    if (!token?.prices) {
-      continue;
-    }
+//     if (!token?.prices) {
+//       continue;
+//     }
 
-    const convertedAddress = convertTokenAddress(chainId, address, "wrapped");
+//     const convertedAddress = convertTokenAddress(chainId, address, "wrapped");
 
-    primaryTokens.push(convertedAddress);
+//     primaryTokens.push(convertedAddress);
 
-    const currentPrice = {
-      min: convertToContractPrice(token.prices.minPrice, token.decimals),
-      max: convertToContractPrice(token.prices.maxPrice, token.decimals),
-    };
+//     const currentPrice = {
+//       min: convertToContractPrice(token.prices.minPrice, token.decimals),
+//       max: convertToContractPrice(token.prices.maxPrice, token.decimals),
+//     };
 
-    const primaryOverridedPrice = primaryPricesMap[address];
+//     const primaryOverridedPrice = primaryPricesMap[address];
 
-    if (primaryOverridedPrice) {
-      primaryPrices.push({
-        min: convertToContractPrice(primaryOverridedPrice.minPrice, token.decimals),
-        max: convertToContractPrice(primaryOverridedPrice.maxPrice, token.decimals),
-      });
-    } else {
-      primaryPrices.push(currentPrice);
-    }
+//     if (primaryOverridedPrice) {
+//       primaryPrices.push({
+//         min: convertToContractPrice(primaryOverridedPrice.minPrice, token.decimals),
+//         max: convertToContractPrice(primaryOverridedPrice.maxPrice, token.decimals),
+//       });
+//     } else {
+//       primaryPrices.push(currentPrice);
+//     }
 
-    const secondaryOverridedPrice = secondaryPricesMap[address];
+//     const secondaryOverridedPrice = secondaryPricesMap[address];
 
-    if (secondaryOverridedPrice) {
-      secondaryPrices.push({
-        min: convertToContractPrice(secondaryOverridedPrice.minPrice, token.decimals),
-        max: convertToContractPrice(secondaryOverridedPrice.maxPrice, token.decimals),
-      });
-    } else {
-      secondaryPrices.push(currentPrice);
-    }
-  }
+//     if (secondaryOverridedPrice) {
+//       secondaryPrices.push({
+//         min: convertToContractPrice(secondaryOverridedPrice.minPrice, token.decimals),
+//         max: convertToContractPrice(secondaryOverridedPrice.maxPrice, token.decimals),
+//       });
+//     } else {
+//       secondaryPrices.push(currentPrice);
+//     }
+//   }
 
-  return {
-    primaryTokens,
-    secondaryTokens: primaryTokens,
-    primaryPrices,
-    secondaryPrices,
-  };
-}
+//   return {
+//     primaryTokens,
+//     secondaryTokens: primaryTokens,
+//     primaryPrices,
+//     secondaryPrices,
+//   };
+// }
