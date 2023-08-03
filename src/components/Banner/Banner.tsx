cf770f81d5762d8458507b141b0779560e5f80ae
divhead <i.divhead@gmail.com>
2023-08-03 19:31:03 +0400

    (synthetics): upd subgraphs & substack link

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,24 +1,23 @@
-// import { FiChevronRight } from "react-icons/fi";
-// import cx from "classnames";
+import { FiChevronRight } from "react-icons/fi";
+import cx from "classnames";
 import "./Banner.scss";
-// import sparkle from "img/sparkle.svg";
+import sparkle from "img/sparkle.svg";
 
 function Banner({ className }) {
-  return null;
-  // return (
-  //   <div className={cx("Banner-wrapper", className)}>
-  //     <p className="Banner-text">
-  //       GMX updated!
-  //       <img src={sparkle} alt="sparkle" className="Banner-text-sparkle" />
-  //     </p>
-  //     <div className="Banner-action items-center justify-space-between">
-  //       <a className="Banner-link" href="#a">
-  //         What’s new in V2?
-  //       </a>
-  //       <FiChevronRight fontSize={16} />
-  //     </div>
-  //   </div>
-  // );
+  return (
+    <div className={cx("Banner-wrapper", className)}>
+      <p className="Banner-text">
+        GMX updated!
+        <img src={sparkle} alt="sparkle" className="Banner-text-sparkle" />
+      </p>
+      <div className="Banner-action items-center justify-space-between">
+        <a className="Banner-link" href="https://gmxio.substack.com/p/gmx-v2-beta-is-now-live">
+          What’s new in V2?
+        </a>
+        <FiChevronRight fontSize={16} />
+      </div>
+    </div>
+  );
 }
 
 export default Banner;
--- src/config/subgraph.ts
+++ src/config/subgraph.ts
@@ -5,35 +5,27 @@ import { getSubgraphUrlKey } from "./localStorage";
 const SUBGRAPH_URLS = {
   [ARBITRUM]: {
     stats: "https://subgraph.satsuma-prod.com/3b2ced13c8d9/gmx/gmx-arbitrum-stats/api",
-
-    referrals:
-      "https://subgraph.satsuma-prod.com/3b2ced13c8d9/gmx/gmx-arbitrum-referrals/version/synts-stats-230726004839-065cd0d/api",
-
+    referrals: "https://subgraph.satsuma-prod.com/3b2ced13c8d9/gmx/gmx-arbitrum-referrals/api",
     nissohVault: "https://api.thegraph.com/subgraphs/name/nissoh/gmx-vault",
     syntheticsStats: "https://subgraph.satsuma-prod.com/3b2ced13c8d9/gmx/synthetics-arbitrum-stats/api",
   },
 
   [ARBITRUM_GOERLI]: {
     stats: "https://subgraph.satsuma-prod.com/3b2ced13c8d9/gmx/gmx-arbitrum-stats/api",
-    referrals:
-      "https://subgraph.satsuma-prod.com/3b2ced13c8d9/gmx/gmx-goerli-referrals/version/synts-stats-230726124555-065cd0d/api",
-    syntheticsStats:
-      "https://subgraph.satsuma-prod.com/3b2ced13c8d9/gmx/synthetics-goerli-stats/version/synts-stats-230714033353-42d145e/api",
+    referrals: "https://subgraph.satsuma-prod.com/3b2ced13c8d9/gmx/gmx-goerli-referrals/api",
+    syntheticsStats: "https://subgraph.satsuma-prod.com/3b2ced13c8d9/gmx/synthetics-goerli-stats/api",
   },
 
   [AVALANCHE]: {
     stats: "https://subgraph.satsuma-prod.com/3b2ced13c8d9/gmx/gmx-avalanche-stats/api",
-    referrals:
-      "https://subgraph.satsuma-prod.com/3b2ced13c8d9/gmx/gmx-avalanche-referrals/version/synts-stats-230726004904-065cd0d/api",
+    referrals: "https://subgraph.satsuma-prod.com/3b2ced13c8d9/gmx/gmx-avalanche-referrals/api",
     syntheticsStats: "https://subgraph.satsuma-prod.com/3b2ced13c8d9/gmx/synthetics-avalanche-stats/api",
   },
 
   [AVALANCHE_FUJI]: {
     stats: "https://api.thegraph.com/subgraphs/name/gmx-io/gmx-avalanche-stats",
-    referrals:
-      "https://subgraph.satsuma-prod.com/3b2ced13c8d9/gmx/gmx-fuji-referrals/version/synts-stats-230726124533-065cd0d/api",
-    syntheticsStats:
-      "https://subgraph.satsuma-prod.com/3b2ced13c8d9/gmx/synthetics-fuji-stats/version/synts-stats-230714062025-42d145e/api",
+    referrals: "https://subgraph.satsuma-prod.com/3b2ced13c8d9/gmx/gmx-fuji-referrals/api",
+    syntheticsStats: "https://subgraph.satsuma-prod.com/3b2ced13c8d9/gmx/synthetics-fuji-stats/api",
   },
 
   common: {
