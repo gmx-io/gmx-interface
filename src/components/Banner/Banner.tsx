4b6b84a31a6e220516c4159e09781fbf6f936e3f
divhead <i.divhead@gmail.com>
2023-08-03 18:50:12 +0400

    (synthetics): hide the banner

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,23 +1,24 @@
-import { FiChevronRight } from "react-icons/fi";
-import cx from "classnames";
+// import { FiChevronRight } from "react-icons/fi";
+// import cx from "classnames";
 import "./Banner.scss";
-import sparkle from "img/sparkle.svg";
+// import sparkle from "img/sparkle.svg";
 
 function Banner({ className }) {
-  return (
-    <div className={cx("Banner-wrapper", className)}>
-      <p className="Banner-text">
-        GMX updated!
-        <img src={sparkle} alt="sparkle" className="Banner-text-sparkle" />
-      </p>
-      <div className="Banner-action items-center justify-space-between">
-        <a className="Banner-link" href="#a">
-          What’s new in V2?
-        </a>
-        <FiChevronRight fontSize={16} />
-      </div>
-    </div>
-  );
+  return null;
+  // return (
+  //   <div className={cx("Banner-wrapper", className)}>
+  //     <p className="Banner-text">
+  //       GMX updated!
+  //       <img src={sparkle} alt="sparkle" className="Banner-text-sparkle" />
+  //     </p>
+  //     <div className="Banner-action items-center justify-space-between">
+  //       <a className="Banner-link" href="#a">
+  //         What’s new in V2?
+  //       </a>
+  //       <FiChevronRight fontSize={16} />
+  //     </div>
+  //   </div>
+  // );
 }
 
 export default Banner;
