51087d20f18f687f37d3bc951c61f1159ae53ea5
divhead <i.divhead@gmail.com>
2023-08-14 22:08:52 +0400

    throttle fallbacks

--- src/config/oracleKeeper.ts
+++ src/config/oracleKeeper.ts
@@ -9,22 +9,30 @@ const ORACLE_KEEPER_URLS = {
   [ARBITRUM_GOERLI]: ["https://oracle-api-arb-goerli-xyguy.ondigitalocean.app"],
 
   [AVALANCHE_FUJI]: ["https://gmx-oracle-keeper-ro-avax-fuji-d4il9.ondigitalocean.app"],
-
-  default: ["https://gmx-oracle-keeper-ro-avax-fuji-d4il9.ondigitalocean.app"],
 };
 
 export function getOracleKeeperUrl(chainId: number, index: number) {
-  const urls = ORACLE_KEEPER_URLS[chainId] || ORACLE_KEEPER_URLS.default;
+  const urls = ORACLE_KEEPER_URLS[chainId];
 
-  if (index > urls.length - 1) {
-    return urls[0];
+  if (!urls.length) {
+    throw new Error(`No oracle keeper urls for chain ${chainId}`);
   }
 
-  return urls[index];
+  return urls[index] || urls[0];
+}
+
+export function getOracleKeeperNextIndex(chainId: number, currentIndex: number) {
+  const urls = ORACLE_KEEPER_URLS[chainId];
+
+  if (!urls.length) {
+    throw new Error(`No oracle keeper urls for chain ${chainId}`);
+  }
+
+  return urls[currentIndex + 1] ? currentIndex + 1 : 0;
 }
 
 export function getOracleKeeperRandomIndex(chainId: number, bannedIndexes?: number[]): number {
-  const urls = ORACLE_KEEPER_URLS[chainId] || ORACLE_KEEPER_URLS.default;
+  const urls = ORACLE_KEEPER_URLS[chainId];
 
   if (bannedIndexes?.length) {
     const filteredUrls = urls.filter((url, i) => !bannedIndexes.includes(i));
