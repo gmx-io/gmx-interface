d95c3f44a0c1ad6dd1573e3d48ff18fc9269a457
Rick Chi <wenzhongchi@gmail.com>
2023-08-30 06:02:46 -0400

    Fix/change folder (#16)
    * bugfix: change folder
    
    * bugfix: change folder

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "regenerator-runtime/runtime";
 import { BrowserRouter as Router } from "react-router-dom";
-import App from "./App/App";
+import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
