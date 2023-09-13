6b8ba49c2a82dee57533b1075ab1e93ed8fdb46e
An <angmxio@gmail.com>
2023-09-13 15:57:31 +0400

    fix claims layout

--- src/components/Synthetics/Claims/ClaimableCard.tsx
+++ src/components/Synthetics/Claims/ClaimableCard.tsx
@@ -1,4 +1,5 @@
-import { Trans } from "@lingui/macro";
+import { Trans, t } from "@lingui/macro";
+import Tooltip from "components/Tooltip/Tooltip";
 import { MarketsInfoData, getTotalClaimableFundingUsd } from "domain/synthetics/markets";
 import { formatUsd } from "lib/numbers";
 import useWallet from "lib/wallets/useWallet";
@@ -17,14 +18,22 @@ export function ClaimableCard({ marketsInfoData, onClaimClick }: Props) {
   return (
     <div className="TradeHistoryRow App-box">
       <div className="Claims-row">
-        <div className="Claims-col">
+        <div className="Claims-col Claims-col-title">
           <Trans>Claimable</Trans>
         </div>
         <div className="Claims-col">
           <span className="muted">
             <Trans>Funding fees</Trans>
           </span>
-          <span>{totalUsd}</span>
+          <span>
+            <Tooltip
+              handle={totalUsd}
+              position="left-bottom"
+              renderContent={() => {
+                return t`Positive Funding Fees for a position become claimable after the position is increased, decreased or closed.`;
+              }}
+            />
+          </span>
         </div>
         <div className="Claims-col">
           <span className="muted">
