16632200c1fc55c8ff1db91a9ffdb5e4f634ef78
Ullin Oi <huea.andanda@proton.me>
2023-09-18 19:46:31 +0300

    improved styles

--- src/pages/Leaderboards/Leaderboards.tsx
+++ src/pages/Leaderboards/Leaderboards.tsx
@@ -2,28 +2,16 @@ import React from "react";
 import SEO from "components/Common/SEO";
 import { getPageTitle } from "lib/legacy";
 import { t, Trans } from "@lingui/macro";
-import { useChainId } from "lib/chains";
 import GloballLeaderboards from "components/GlobalLeaderboards/GlobalLeaderboards";
-import { getIcon } from "config/icons";
+import PageTitle from "components/PageTitle/PageTitle";
 
 import "./Leaderboards.css";
 
 export default function Leaderboard() {
-  const { chainId } = useChainId();
-
   return (
     <SEO title={getPageTitle("Leaderboards")}>
       <div className="default-container page-layout Leaderboard">
-        <div className="section-title-block">
-          <div className="section-title-content">
-            <div className="Page-title">
-              <Trans>Leaderboards</Trans> <img alt={t`Chain Icon`} src={getIcon(chainId, "network")} />
-            </div>
-            <div className="Page-description">
-              <Trans>Addresses V2 trading statistics.</Trans>
-            </div>
-          </div>
-        </div>
+        <PageTitle isTop title={t`Leaderboards`} subtitle={<Trans>Addresses V2 trading statistics.</Trans>} />
         <GloballLeaderboards />
       </div>
     </SEO>
