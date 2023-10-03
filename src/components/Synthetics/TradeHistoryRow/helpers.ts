ba1eabc44e8615320b36f17c12b42f0037fc11a5
An <angmxio@gmail.com>
2023-10-03 14:13:43 +0400

    review fix

--- src/components/Synthetics/TradeHistoryRow/helpers.ts
+++ src/components/Synthetics/TradeHistoryRow/helpers.ts
@@ -263,7 +263,6 @@ function getLiquidationTooltipProps(
     { label: t`Borrow Fee`, showDollar: false, value: formatUsd(borrowingFeeUsd?.mul(-1)), className: "text-red" },
     { label: t`Funding Fee`, showDollar: false, value: formatUsd(fundingFeeUsd?.mul(-1)), className: "text-red" },
     { label: t`Position Fee`, showDollar: false, value: formatUsd(positionFeeUsd?.mul(-1)), className: "text-red" },
-    // FIXME do we still want to show priceImpactDiffUsd?
     {
       label: t`Price Impact`,
       showDollar: false,
@@ -357,6 +356,7 @@ function getExecutionPriceTooltipRows(tradeAction: PositionTradeAction): StatsTo
       label: t`Price Impact`,
       showDollar: false,
       value: formatDeltaUsd(tradeAction.priceImpactUsd),
+      className: tradeAction.priceImpactUsd.gt(0) ? "text-green" : "text-red",
     },
   ].filter(Boolean) as StatsTooltipRowProps[];
 }
@@ -373,7 +373,7 @@ function getMarketTooltipRows(tradeAction: PositionTradeAction): StatsTooltipRow
     tradeAction.priceImpactUsd && {
       label: t`Actual Price Impact`,
       showDollar: false,
-      value: formatUsd(tradeAction.priceImpactUsd, { displayDecimals: 2 }),
+      value: formatDeltaUsd(tradeAction.priceImpactUsd),
       className: tradeAction.priceImpactUsd.gt(0) ? "text-green" : "text-red",
     },
   ].filter(Boolean) as StatsTooltipRowProps[];
