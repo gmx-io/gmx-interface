07cbe2977f10dd06a94baf78745d4e4af05d7b7e
An <angmxio@gmail.com>
2023-10-31 16:01:19 +0400

    pre review fixes

--- src/components/AprInfo/AprInfo.tsx
+++ src/components/AprInfo/AprInfo.tsx
@@ -5,14 +5,6 @@ import { formatAmount } from "lib/numbers";
 import { useCallback } from "react";
 
 export function AprInfo({ apr, incentiveApr }: { apr: BigNumber | undefined; incentiveApr: BigNumber | undefined }) {
-  //   if (apr && incentiveApr && incentiveApr.gt(0)) {
-  //     return (
-
-  //     );
-  //   } else {
-  //     const someApr = apr ?? incentiveApr;
-  //     return <>{someApr ? `${formatAmount(someApr, 2, 2)}%` : "..."}</>;
-  //   }
   const totalApr = apr?.add(incentiveApr ?? 0) ?? BigNumber.from(0);
   const aprNode = <>{formatAmount(totalApr, 2, 2)}%</>;
   const renderTooltipContent = useCallback(() => {
