0224f42270a8ba7fbfad0af4c0d2d5dc2fd64f93
An <angmxio@gmail.com>
2023-11-09 11:06:42 +0400

    review fixes

--- src/domain/synthetics/common/useIncentiveStats.ts
+++ src/domain/synthetics/common/useIncentiveStats.ts
@@ -1,11 +1,14 @@
 import useSWR from "swr";
 import { RawIncentivesStats, useOracleKeeperFetcher } from "../tokens";
+import { useChainId } from "lib/chains";
 
-export default function useIncentiveStats(chainId: number) {
+export default function useIncentiveStats(overrideChainId: number) {
+  const { chainId: defaultChainId } = useChainId();
+  const chainId = overrideChainId ?? defaultChainId;
   const oracleKeeperFetcher = useOracleKeeperFetcher(chainId);
 
   return (
-    useSWR<RawIncentivesStats | null>(`incentiveStats-${chainId}`, async () => {
+    useSWR<RawIncentivesStats | null>(["incentiveStats", chainId], async () => {
       if (!oracleKeeperFetcher) {
         return null;
       }
