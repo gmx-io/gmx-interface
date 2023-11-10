c48332be5765e832a722912e815370c3c74e323c
vipin <vipineth@gmail.com>
2023-11-10 23:39:44 +0530

    update skeleton

--- src/components/Skeleton/TokenListSkeleton.tsx
+++ src/components/Skeleton/TokenListSkeleton.tsx
@@ -1,45 +1,52 @@
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import { SkeletonTheme } from "react-loading-skeleton";
+import "./a.css";
 
 function TableRowSkeleton() {
   return (
-    <SkeletonTheme baseColor="#a9a9b0" highlightColor="#76767b">
-      <tr>
-        <td>
-          <div className="items-center">
-            <Skeleton className="mr-xs" height={40} width={40} circle />
-            <div>
-              <Skeleton width={60} height={10} count={2} />
-            </div>
+    <tr>
+      <td>
+        <div className="items-center">
+          <Skeleton className="mr-sm" height={40} width={40} circle />
+          <div>
+            <Skeleton width={60} height={12} />
+            <Skeleton width={40} height={12} />
           </div>
-        </td>
-        <td>
-          <Skeleton count={1} />
-        </td>
-        <td>
-          <Skeleton count={1} />
-        </td>
-        <td>
-          <Skeleton count={1} />
-        </td>
-        <td>
-          <Skeleton count={1} />
-        </td>
-        <td>
-          <Skeleton count={1} />
-        </td>
-      </tr>
-    </SkeletonTheme>
+        </div>
+      </td>
+      <td>
+        <Skeleton width={60} count={1} />
+      </td>
+      <td>
+        <Skeleton width={100} height={12} />
+        <Skeleton width={80} height={12} />
+      </td>
+      <td>
+        <Skeleton width={100} height={12} />
+        <Skeleton width={80} height={12} />
+      </td>
+      <td>
+        <Skeleton width={100} height={12} />
+        <Skeleton width={80} height={12} />
+      </td>
+      <td>
+        <Skeleton count={1} />
+      </td>
+      <td></td>
+    </tr>
   );
 }
 
 function TokenListSkeleton({ count = 8 }) {
   return (
     <>
-      {Array.from({ length: count }).map((_, index) => (
-        <TableRowSkeleton key={index} />
-      ))}
+      <div className="tr-wave" />
+      <SkeletonTheme baseColor="#B4BBFF1A" highlightColor="#B4BBFF1A" enableAnimation={false}>
+        {Array.from({ length: count }).map((_, index) => (
+          <TableRowSkeleton key={index} />
+        ))}
+      </SkeletonTheme>
     </>
   );
 }
