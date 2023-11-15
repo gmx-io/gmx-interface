9d16fdea44d0be8dc202b6d99856181a6a69c9df
Shrikanth <shrikanthreddy@Shrikanths-MacBook-Pro-2.local>
2023-11-15 15:44:07 +0530

    remove unused vars

--- src/config/icons.ts
+++ src/config/icons.ts
@@ -12,13 +12,13 @@ import avalanche from "img/ic_avalanche_24.svg";
 import avalancheTestnet from "img/ic_avalanche_testnet_24.svg";
 import sepoliaTesnet from "img/ic_sepolia_testnet_24.svg";
 
-import gmxIcon from "img/ic_gmx_40.svg";
-import glpIcon from "img/ic_glp_40.svg";
-import tlpIcon from "img/ic_glp_sepolia.svg";
-import gmxArbitrum from "img/ic_gmx_arbitrum.svg";
-import gmxAvax from "img/ic_gmx_avax.svg";
-import glpArbitrum from "img/ic_glp_arbitrum.svg";
-import glpAvax from "img/ic_glp_avax.svg";
+// import gmxIcon from "img/ic_gmx_40.svg";
+// import glpIcon from "img/ic_glp_40.svg";
+// import tlpIcon from "img/ic_glp_sepolia.svg";
+// import gmxArbitrum from "img/ic_gmx_arbitrum.svg";
+// import gmxAvax from "img/ic_gmx_avax.svg";
+// import glpArbitrum from "img/ic_glp_arbitrum.svg";
+// import glpAvax from "img/ic_glp_avax.svg";
 import optimismIcn from "img/icn_opt_24.svg"
 import tmxImg from 'img/ic_tmx.svg';
 import tlpImg from 'img/ic_tlp.svg';
