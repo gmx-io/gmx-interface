756c12603793b05c89c65284a293714ec244c766
An <angmxio@gmail.com>
2023-12-01 15:51:47 +0400

    fix key

--- src/components/Synthetics/TradeFeesRow/TradeFeesRow.tsx
+++ src/components/Synthetics/TradeFeesRow/TradeFeesRow.tsx
@@ -232,7 +232,7 @@ export function TradeFeesRow(p: Props) {
               p.positionFee?.deltaUsd.mul(tradingIncentives.rebatePercent).div(BASIS_POINTS_DIVISOR).mul(-1)
             ),
             className: "text-green",
-            id: "executionFee",
+            id: "rebate",
           }
         : undefined;
 
