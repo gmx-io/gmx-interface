7489aa75162e035eb408987640cdee21fba14580
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2023-12-14 20:32:00 +0400

    fix type in ledger notification

--- src/config/events.ts
+++ src/config/events.ts
@@ -135,7 +135,7 @@ export const appEventsData: EventData[] = [
   },
   {
     id: "ledger-issue",
-    title: "GMX is Unaffected",
+    title: "GMX is unaffected",
     isActive: true,
     validTill: "15 Dec 2023, 16:00",
     bodyText: ["The recent issue with Ledger Connect kit did not affect GMX."],
