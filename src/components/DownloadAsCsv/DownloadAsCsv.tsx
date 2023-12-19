7e615e6187f60b6cb797fb75f2bd0201cd965a45
vipin <vipineth@gmail.com>
2023-12-19 21:50:39 +0530

    refactor

--- src/components/DownloadAsCsv/DownloadAsCsv.tsx
+++ src/components/DownloadAsCsv/DownloadAsCsv.tsx
@@ -1,6 +1,5 @@
 import React, { useCallback } from "react";
 import Button from "components/Button/Button";
-import Papa from "papaparse";
 
 type Props<T> = {
   data: T[];
@@ -15,11 +14,18 @@ function filterFields<T>(data: T, excludedFields: string[]): Partial<T> {
   return result;
 }
 
+function convertToCSV<T>(data: Partial<T>[]): string {
+  const header = Object.keys(data[0]).join(",");
+  const values = data.map((object) => Object.values(object).join(",")).join("\n");
+
+  return `${header}\n${values}`;
+}
+
 export function DownloadAsCsv<T>({ data, excludedFields, fileName, className }: Props<T>) {
   const getCsvUrl = useCallback(
     (data: T[]) => {
       const filteredData = data.map((item) => filterFields(item, excludedFields));
-      const csv = Papa.unparse(filteredData);
+      const csv = convertToCSV(filteredData);
       return `data:application/octet-stream,${encodeURIComponent(csv)}`;
     },
     [excludedFields]
