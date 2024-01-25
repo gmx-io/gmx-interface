7e0c59a0ef49edd858ba67ad791e8f5834032340
vipin <vipineth@gmail.com>
2024-01-25 17:29:36 +0530

    refactor

--- src/components/LeverageSlider/LeverageSlider.tsx
+++ src/components/LeverageSlider/LeverageSlider.tsx
@@ -3,10 +3,10 @@ import Slider, { SliderTooltip, Handle } from "rc-slider";
 import "rc-slider/assets/index.css";
 import "./LeverageSlider.scss";
 import { range } from "lodash";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useMemo } from "react";
 
 const defaultMarks = [2, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
-const DEFAULT_LEVERAGE_KEY = 10;
+const DEFAULT_LEVERAGE_KEY = 20;
 
 type Props = {
   isPositive?: boolean;
@@ -15,11 +15,29 @@ type Props = {
   marks: number[];
 };
 
+type HandleProps = {
+  value: number;
+  dragging: boolean;
+  index: number;
+  keyValueMap: { [key: number]: number };
+};
+
+function getMarksWithLabel(marks: number[]) {
+  return marks.reduce((marks, value, index) => {
+    marks[index * 10] = `${value}x`;
+    return marks;
+  }, {} as { [key: number]: string });
+}
+
 export function LeverageSlider(p: Props) {
   const { onChange, value, marks } = p;
   const finalMarks = marks ?? defaultMarks;
-  const marksLabel = getMarksWithLabel(finalMarks);
-  const { keyValueMap, valueKeyMap } = createMarksValueMap(finalMarks);
+
+  const { marksLabel, keyValueMap, valueKeyMap } = useMemo(() => {
+    const marksLabel = getMarksWithLabel(finalMarks);
+    const { keyValueMap, valueKeyMap } = generateKeyValueMap(finalMarks);
+    return { marksLabel, keyValueMap, valueKeyMap };
+  }, [finalMarks]);
 
   const defaultValue = valueKeyMap[value ?? 0] ?? DEFAULT_LEVERAGE_KEY;
   const max = (finalMarks.length - 1) * 10;
@@ -59,11 +77,12 @@ export function LeverageSlider(p: Props) {
   );
 }
 
-function LeverageSliderHandle({ value, dragging, index, keyValueMap, ...restProps }: any) {
+function LeverageSliderHandle({ value, dragging, index, keyValueMap, ...restProps }: HandleProps) {
+  const displayValue = keyValueMap[value || 0] ?? DEFAULT_LEVERAGE_KEY;
   return (
     <SliderTooltip
       prefixCls="rc-slider-tooltip"
-      overlay={`${parseFloat(keyValueMap[value || 0] ?? DEFAULT_LEVERAGE_KEY).toFixed(2)}x`}
+      overlay={`${parseFloat(displayValue.toString()).toFixed(2)}x`}
       visible={dragging}
       placement="top"
       key={index}
@@ -84,26 +103,14 @@ function generateEquallySpacedArray(min: number, max: number, shouldIncludeMax?:
   return array;
 }
 
-function createOverlappingChunks(array: number[], chunkSize: number): number[][] {
-  return array.slice(0, -1).map((_, index) => array.slice(index, index + chunkSize));
-}
-
-function createMarksValueMap(marks: number[]) {
-  const marksChunks = createOverlappingChunks(marks, 2);
-  const values = marksChunks.flatMap((chunk, index) => {
-    const shouldIncludeMax = index === marksChunks.length - 1;
-    return generateEquallySpacedArray(chunk[0], chunk[1], shouldIncludeMax);
+function generateKeyValueMap(marks: number[]) {
+  const values = marks.slice(0, -1).flatMap((mark, index) => {
+    const shouldIncludeMax = index === marks.length - 2;
+    return generateEquallySpacedArray(mark, marks[index + 1], shouldIncludeMax);
   });
 
   const keyValueMap = Object.fromEntries(values.map((value, index) => [index, value]));
   const valueKeyMap = Object.fromEntries(values.map((value, index) => [value, index]));
 
   return { keyValueMap, valueKeyMap };
-}
-
-function getMarksWithLabel(marks: number[]) {
-  return marks.reduce((marks, value, index) => {
-    marks[index * 10] = `${value}x`;
-    return marks;
-  }, {} as { [key: number]: string });
 }
