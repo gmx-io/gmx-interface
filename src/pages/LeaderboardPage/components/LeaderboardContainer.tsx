4fc407f9cff7d40638a6c39d9a715551d0e9fdad
An <angmxio@gmail.com>
2024-03-13 09:04:46 +0400

    fix countdown

--- src/pages/LeaderboardPage/components/LeaderboardContainer.tsx
+++ src/pages/LeaderboardPage/components/LeaderboardContainer.tsx
@@ -130,7 +130,7 @@ export function LeaderboardContainer() {
             .{wrongNetworkSwitcher}{" "}
             {isMobile && (
               <>
-                <CompetitionCountdown size="mobile" />.
+                <CompetitionCountdown size="mobile" />
               </>
             )}
           </>
