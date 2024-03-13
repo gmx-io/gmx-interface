2367e9ce3d4ca1ef44ef1a2ea763d7566de398d3
midas-myth <midas-myth@proton.me>
2024-03-13 15:09:00 +0000

    Update tests and add screenshots

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,12 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+const BASE_URL = "http://localhost:3010";
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+test("has title", async ({ page }) => {
+  await page.goto(`${BASE_URL}/#/trade`);
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.waitForTimeout(5000);
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // screen shot
+  expect(await page.screenshot()).toMatchSnapshot("screenshot.png");
 });
