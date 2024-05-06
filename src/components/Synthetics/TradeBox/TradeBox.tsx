51534c1d6dc47d73ab73381fe5154a1fceccbfac
midas-myth <midas-myth@proton.me>
2024-05-06 12:47:38 +0000

    Update TradeBox component CSS class name for token label

--- src/components/Synthetics/TradeBox/TradeBox.tsx
+++ src/components/Synthetics/TradeBox/TradeBox.tsx
@@ -912,7 +912,7 @@ export function TradeBox(p: Props) {
                 selectedMarketLabel={
                   toToken && (
                     <>
-                      <span className="inline-items-center">
+                      <span className="inline-flex items-center">
                         <TokenIcon className="mr-8" symbol={toToken.symbol} importSize={24} displaySize={20} />
                         <span className="Token-symbol-text">{toToken.symbol}</span>
                       </span>
