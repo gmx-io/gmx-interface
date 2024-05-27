344fbe96bc27c0f87660404a8f9770c9013ec91a
An <angmxio@gmail.com>
2024-05-27 11:23:14 +0400

    add local-rules/no-logical-bigint rule

--- eslint-local-rules.js
+++ eslint-local-rules.js
@@ -1,4 +1,5 @@
 const { ESLintUtils } = require("@typescript-eslint/experimental-utils");
+const jsxUtils = require("eslint-plugin-react/lib/util/jsx");
 
 const createRule = ESLintUtils.RuleCreator((name) => `https://example.com/rule/${name}`);
 
@@ -30,19 +31,13 @@ module.exports = {
             const tsNode = parserServices.esTreeNodeToTSNodeMap.get(node.argument);
             const type = checker.getTypeAtLocation(tsNode);
             if (
-              // checker.typeToString(type) === "bigint" ||
-              checker.typeToString(type) === "bigint | undefined"
-              // checker.typeToString(type) === "bigint | null"
+              checker.typeToString(type) === "bigint" ||
+              checker.typeToString(type) === "bigint | undefined" ||
+              checker.typeToString(type) === "bigint | null"
             ) {
               context.report({
                 node,
                 messageId: "bigintNegation",
-                fix(fixer) {
-                  return [
-                    fixer.removeRange([node.range[0], node.range[0] + 1]),
-                    fixer.insertTextAfter(node.argument, " === undefined"),
-                  ];
-                },
                 suggest: [
                   {
                     desc: "Use === undefined",
@@ -79,4 +74,64 @@ module.exports = {
       };
     },
   }),
+  "no-logical-bigint": createRule({
+    name: "no-conditional-bigint",
+    meta: {
+      type: "problem",
+      docs: {
+        description: "Disallow using bigint type in logical expressions",
+        category: "Possible Errors",
+        recommended: true,
+      },
+      schema: [],
+      messages: {
+        logicalBigint: "Using bigint in logical expressions is not allowed.",
+      },
+      hasSuggestions: true,
+      fixable: true,
+    },
+    defaultOptions: [],
+    create(context) {
+      return {
+        LogicalExpression(node) {
+          if (checkBigInt(context, node)) {
+            context.report({
+              node,
+              messageId: "logicalBigint",
+            });
+          }
+        },
+        IfStatement(node) {
+          if (checkBigInt(context, node.test)) {
+            context.report({
+              node,
+              messageId: "logicalBigint",
+            });
+          }
+        },
+      };
+    },
+  }),
 };
+
+function checkBigInt(context, node) {
+  if (node.type === "Literal" || node.type === "Identifier" || node.type === "MemberExpression") {
+    const parserServices = ESLintUtils.getParserServices(context);
+    const checker = parserServices.program.getTypeChecker();
+    const tsNode = parserServices.esTreeNodeToTSNodeMap.get(node);
+    const type = checker.getTypeAtLocation(tsNode);
+
+    return (
+      checker.typeToString(type) === "bigint" ||
+      checker.typeToString(type) === "bigint | undefined" ||
+      checker.typeToString(type) === "bigint | null"
+    );
+  }
+
+  if (node.type === "LogicalExpression") {
+    if (node.operator !== "&&" && node.operator !== "||") return false;
+    return checkBigInt(context, node.right) || checkBigInt(context, node.left);
+  }
+
+  return false;
+}
