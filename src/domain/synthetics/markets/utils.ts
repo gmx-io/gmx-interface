2dcf60fccf2fdda419d750a77abed12f3da97767
An <angmxio@gmail.com>
2024-06-07 09:52:12 +0400

    Dynamic Leverage new option and minor bug fix

--- src/domain/synthetics/markets/utils.ts
+++ src/domain/synthetics/markets/utils.ts
@@ -474,13 +474,36 @@ export function getTotalGmInfo(tokensData?: TokensData) {
 
 export function getMaxLeverageByMinCollateralFactor(minCollateralFactor: bigint | undefined) {
   if (minCollateralFactor === undefined) return 100 * BASIS_POINTS_DIVISOR;
-  if (minCollateralFactor >= PRECISION / 100n) return 100 * BASIS_POINTS_DIVISOR;
-  else if (minCollateralFactor >= PRECISION / 150n) return 150 * BASIS_POINTS_DIVISOR;
-  else if (minCollateralFactor >= PRECISION / 200n) return 200 * BASIS_POINTS_DIVISOR;
-  else if (minCollateralFactor >= PRECISION / 250n) return 250 * BASIS_POINTS_DIVISOR;
-  else return 100 * BASIS_POINTS_DIVISOR;
+  if (minCollateralFactor === 0n) return 100 * BASIS_POINTS_DIVISOR;
+  return Number(PRECISION / minCollateralFactor) * BASIS_POINTS_DIVISOR;
 }
 
 export function getMaxAllowedLeverageByMinCollateralFactor(minCollateralFactor: bigint | undefined) {
   return getMaxLeverageByMinCollateralFactor(minCollateralFactor) / 2;
+}
+
+export function getTradeboxLeverageSliderMarks(maxLeverage: number) {
+  const allowedLeverage = Math.round(maxLeverage / 2 / BASIS_POINTS_DIVISOR);
+
+  if (allowedLeverage <= 50) {
+    return [0.1, 1, 2, 5, 10, 15, 25, 50];
+  } else if (allowedLeverage <= 60) {
+    return [0.1, 1, 2, 5, 10, 15, 25, 50, 60];
+  } else if (allowedLeverage <= 70) {
+    return [0.1, 1, 2, 5, 10, 15, 30, 50, 70];
+  } else if (allowedLeverage <= 80) {
+    return [0.1, 1, 2, 5, 10, 15, 30, 60, 80];
+  } else if (allowedLeverage <= 90) {
+    return [0.1, 1, 2, 5, 10, 15, 30, 60, 90];
+  } else if (allowedLeverage <= 100) {
+    return [0.1, 1, 2, 5, 10, 20, 50, 75, 100];
+  } else if (allowedLeverage <= 110) {
+    return [0.1, 1, 2, 5, 10, 25, 50, 75, 100, 110];
+  } else if (allowedLeverage <= 120) {
+    return [0.1, 1, 2, 5, 10, 15, 30, 60, 90, 120];
+  } else if (allowedLeverage <= 125) {
+    return [0.1, 1, 2, 5, 10, 25, 50, 75, 100, 125];
+  }
+
+  return [0.1, 1, 2, 5, 10, 25, 50, 75, 100, allowedLeverage];
 }
