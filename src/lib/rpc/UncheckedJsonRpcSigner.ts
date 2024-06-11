bedd19fa339c4ef44aaf1597488ec8dca284287e
midas-myth <midas-myth@proton.me>
2024-06-11 17:30:03 +0000

    fix: Ensure correct chainId when sending transactions

--- src/lib/rpc/UncheckedJsonRpcSigner.ts
+++ src/lib/rpc/UncheckedJsonRpcSigner.ts
@@ -1,7 +1,17 @@
 import { JsonRpcSigner, TransactionRequest, TransactionResponse } from "ethers";
+import { getRealChainId } from "lib/chains/getRealChainId";
 
 export class UncheckedJsonRpcSigner extends JsonRpcSigner {
   async sendTransaction(transaction: TransactionRequest): Promise<TransactionResponse> {
+    const assumedChainId = Number((await this.provider.getNetwork()).chainId);
+    const realChainId = getRealChainId();
+
+    if (assumedChainId !== realChainId) {
+      throw new Error(
+        `Invalid chainId: wallet is connected to chain ${assumedChainId}, but the app is running on chain ${realChainId}.`
+      );
+    }
+
     return this.sendUncheckedTransaction(transaction).then((hash) => {
       return {
         hash,
