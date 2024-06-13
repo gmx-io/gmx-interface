97a0dc2163dc5a74903bc220bd7fae2816de70b0
Hubért de Lalye <hubert-de-lalye@proton.me>
2024-06-13 13:31:04 +0400

    updated .po files, improved APY tooltip style

--- src/components/Synthetics/GmList/GmList.tsx
+++ src/components/Synthetics/GmList/GmList.tsx
@@ -489,10 +489,12 @@ function ApyTooltipContent() {
           <li className="p-2"> traders' PnL, which is expected to be neutral in the long term</li>
           <li className="p-2"> funding fees, which are exchanged between traders</li>
         </ul>
-        <ExternalLink href="https://docs.gmx.io/docs/providing-liquidity/v2/#token-pricing">
-          Read more about GM token pricing
-        </ExternalLink>
-        .
+        <p className="mb-12">
+          <ExternalLink href="https://docs.gmx.io/docs/providing-liquidity/v2/#token-pricing">
+            Read more about GM token pricing
+          </ExternalLink>
+          .
+        </p>
         <p>
           Check GM pools' performance against other LP Positions in the{" "}
           <ExternalLink href="https://dune.com/gmx-io/gmx-analytics">GMX Dune Dashboard</ExternalLink>.
