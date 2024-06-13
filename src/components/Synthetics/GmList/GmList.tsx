f20a0be763fdc4bc780f025097d7849403a9599f
Hubért de Lalye <hubert-de-lalye@proton.me>
2024-06-13 18:40:06 +0400

    removed unnecessary spaces in ApyTooltipContent

--- src/components/Synthetics/GmList/GmList.tsx
+++ src/components/Synthetics/GmList/GmList.tsx
@@ -485,9 +485,9 @@ function ApyTooltipContent() {
           borrowing fee. It excludes:
         </p>
         <ul className="mb-8 list-disc">
-          <li className="p-2"> price changes of the underlying token(s)</li>
-          <li className="p-2"> traders' PnL, which is expected to be neutral in the long term</li>
-          <li className="p-2"> funding fees, which are exchanged between traders</li>
+          <li className="p-2">price changes of the underlying token(s)</li>
+          <li className="p-2">traders' PnL, which is expected to be neutral in the long term</li>
+          <li className="p-2">funding fees, which are exchanged between traders</li>
         </ul>
         <p className="mb-12">
           <ExternalLink href="https://docs.gmx.io/docs/providing-liquidity/v2/#token-pricing">
