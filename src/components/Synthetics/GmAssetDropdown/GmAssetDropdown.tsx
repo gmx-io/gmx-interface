f4b1373b46653b40e975418dca8067159bdd017a
midas-myth <midas-myth@proton.me>
2024-07-08 15:58:43 +0000

    Move gm asset dropdown to floating ui

--- src/components/Synthetics/GmAssetDropdown/GmAssetDropdown.tsx
+++ src/components/Synthetics/GmAssetDropdown/GmAssetDropdown.tsx
@@ -1,24 +1,29 @@
-import { ReactNode } from "react";
-import cx from "classnames";
-import "./GmAssetDropdown.scss";
-import walletIcon from "img/ic_wallet_24.svg";
+import { Placement, autoUpdate, flip, shift, useFloating } from "@floating-ui/react";
 import { Menu } from "@headlessui/react";
-import { FiChevronDown } from "react-icons/fi";
-import ExternalLink from "components/ExternalLink/ExternalLink";
-import { TokenData, TokensData, getTokenData } from "domain/synthetics/tokens";
-import { MarketInfo, MarketsInfoData, getMarketIndexName, getMarketPoolName } from "domain/synthetics/markets";
-import { getByKey } from "lib/objects";
 import { Trans } from "@lingui/macro";
-import { getIcon } from "config/icons";
-import { useChainId } from "lib/chains";
-import useWallet, { WalletClient } from "lib/wallets/useWallet";
-import { getExplorerUrl } from "config/chains";
+import cx from "classnames";
+import type { ReactNode } from "react";
+import { FiChevronDown } from "react-icons/fi";
 import { createBreakpoint } from "react-use";
+
+import { getExplorerUrl } from "config/chains";
+import { getIcon } from "config/icons";
+import { MarketInfo, MarketsInfoData, getMarketIndexName, getMarketPoolName } from "domain/synthetics/markets";
+import { TokenData, TokensData, getTokenData } from "domain/synthetics/tokens";
+import { useChainId } from "lib/chains";
+import { getByKey } from "lib/objects";
+import useWallet, { WalletClient } from "lib/wallets/useWallet";
+
+import ExternalLink from "components/ExternalLink/ExternalLink";
+
+import walletIcon from "img/ic_wallet_24.svg";
+
+import "./GmAssetDropdown.scss";
 
 type Props = {
   token?: TokenData;
   marketsInfoData?: MarketsInfoData;
-  position?: "left" | "right";
+  position?: Placement;
   tokensData?: TokensData;
 };
 
@@ -40,7 +45,7 @@ function renderMarketName(market?: MarketInfo) {
   );
 }
 
-export default function GmAssetDropdown({ token, marketsInfoData, tokensData, position = "right" }: Props) {
+export default function GmAssetDropdown({ token, marketsInfoData, tokensData, position }: Props) {
   const { chainId } = useChainId();
   const { active, walletClient } = useWallet();
   const chainIcon = getIcon(chainId, "network");
@@ -54,16 +59,20 @@ export default function GmAssetDropdown({ token, marketsInfoData, tokensData, po
   const marketName = renderMarketName(market);
   const isSameCollaterals = market?.isSameCollaterals;
 
+  const { refs, floatingStyles } = useFloating({
+    middleware: [flip(), shift()],
+    strategy: "fixed",
+    placement: breakpoint === "S" ? "bottom" : position ?? "bottom-end",
+    whileElementsMounted: autoUpdate,
+  });
+
   return (
     <div className="AssetDropdown-wrapper GmAssetDropdown">
       <Menu>
-        <Menu.Button as="div" className="dropdown-arrow center-both">
+        <Menu.Button as="div" ref={refs.setReference} className="dropdown-arrow center-both">
           <FiChevronDown size={20} />
         </Menu.Button>
-        <Menu.Items
-          as="div"
-          className={cx("asset-menu-items", breakpoint === "S" ? "center" : { left: position === "left" })}
-        >
+        <Menu.Items as="div" ref={refs.setFloating} style={floatingStyles} className={cx("asset-menu-items")}>
           {market && (
             <Menu.Item as="div">
               <ExternalLink href={`${explorerUrl}address/${token?.address}`} className="asset-item">
