5a0737a0fb332dbec3fd7e2f59eb0b18ab04bcf4
An <angmxio@gmail.com>
2024-07-11 13:39:42 +0400

    rollback subgraph url

--- src/config/subgraph.ts
+++ src/config/subgraph.ts
@@ -21,8 +21,7 @@ const SUBGRAPH_URLS = {
   [AVALANCHE]: {
     stats: "https://subgraph.satsuma-prod.com/3b2ced13c8d9/gmx/gmx-avalanche-stats/api",
     referrals: "https://subgraph.satsuma-prod.com/3b2ced13c8d9/gmx/gmx-avalanche-referrals/api",
-    syntheticsStats:
-      "https://subgraph.satsuma-prod.com/3b2ced13c8d9/gmx/synthetics-avalanche-stats/version/master-240710192902-51167d5/api",
+    syntheticsStats: "https://subgraph.satsuma-prod.com/3b2ced13c8d9/gmx/synthetics-avalanche-stats/api",
     subsquid: "https://gmx.squids.live/gmx-synthetics-avalanche/graphql",
   },
 
