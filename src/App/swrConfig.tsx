5c1cad3b71ce83fcc70097c7d54e8bbd8d82fca3
midas-myth <midas-myth@proton.me>
2024-07-22 18:52:26 +0000

    Fix swr cache issue

--- src/App/swrConfig.tsx
+++ src/App/swrConfig.tsx
@@ -2,7 +2,7 @@ import { swrGCMiddleware } from "lib/swrMiddlewares";
 
 import type { Cache, SWRConfiguration } from "swr";
 
-export const swrCache: Cache = new Map();
+export let swrCache: Cache = new Map();
 
 export const SWRConfigProp: SWRConfiguration = {
   refreshInterval: 5000,
@@ -10,5 +10,8 @@ export const SWRConfigProp: SWRConfiguration = {
   refreshWhenOffline: false,
   // @ts-ignore
   use: [swrGCMiddleware],
-  provider: () => swrCache,
+  provider: () => {
+    swrCache = new Map();
+    return swrCache;
+  },
 };
