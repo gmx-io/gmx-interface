8c9522af1edf7377ab406489ab0357bb99fd5ca3
Divhead <i.divhead@gmail.com>
2024-07-30 20:56:57 +0300

    fixed by review

--- src/components/InterviewModal/InterviewModal.tsx
+++ src/components/InterviewModal/InterviewModal.tsx
@@ -14,20 +14,20 @@ export function InterviewModal({ isVisible, setIsVisible }: Props) {
   const [, copyToClipboard] = useCopyToClipboard();
 
   return (
-    <Modal isVisible={isVisible} setIsVisible={setIsVisible} label={t`Anonymous chat with GMX product manager`}>
-      <div className="max-w-xl">
+    <Modal isVisible={isVisible} setIsVisible={setIsVisible} label={t`Anonymous chat with GMX`}>
+      <div className="flex max-w-xl flex-col items-center">
         <Trans>
           We want your insights to help improve GMX. For security reasons, we won't contact you first. Please send the
           message "I have feedback" to any of our official accounts:
         </Trans>
-        <div className="mb-20 mt-20 flex flex-col gap-8">
-          <div className="flex items-center gap-12">
+        <div className="mb-12 mt-20 flex flex-col gap-8">
+          <div className="flex items-center gap-12 text-14">
             <ArrowBulletIcon />
             <ExternalLink href="https://t.me/GMXPartners">
               <Trans>Telegram account</Trans>
             </ExternalLink>
           </div>
-          <div className="flex items-center gap-12">
+          <div className="flex items-center gap-12 text-14">
             <ArrowBulletIcon />
             <div className="text-gray-300">
               <Trans>
--- src/components/InterviewToast/InterviewToast.tsx
+++ src/components/InterviewToast/InterviewToast.tsx
@@ -8,19 +8,19 @@ type Props = {
 
 export function InterviewToast({ onButtonClick }: Props) {
   return (
-    <div className="text-16">
+    <div>
       <Trans>
         We value your experience and insights and invite you to participate in an anonymous one-on-one chat.
       </Trans>
       <br />
       <br />
       <div onClick={onButtonClick} className="text-xl cursor-pointer text-gray-300 underline">
-        <Trans>Give us your feedback on GMX</Trans>
+        <Trans>Give us your feedback on GMX.</Trans>
       </div>
       <br />
       <Trans>
         As a token of our appreciation, you'll receive a reward of{" "}
-        <span className="font-bold text-green-500">
+        <span className="font-400 text-green-500">
           100 <TokenSymbolWithIcon symbol={getNormalizedTokenSymbol("USDC")} />
         </span>
         .
