77ec25b375da2d8712ce5e8ff7ab87812caa5cf0
Divhead <i.divhead@gmail.com>
2024-08-21 14:04:58 +0300

    disable localStorage for metrics

--- src/lib/metrics/storage.ts
+++ src/lib/metrics/storage.ts
@@ -1,15 +1,16 @@
 const STORAGE: { [key: string]: string } = {};
 
-export function getStorageItem(key: string, fromLocalStorage = true): string | null {
+export function getStorageItem(key: string, fromLocalStorage = false): string | null {
   const value = fromLocalStorage ? localStorage.getItem(key) : STORAGE[key];
-
+  console.log("sa", STORAGE);
   return value;
 }
 
-export function setStorageItem(key: string, value: string, toLocalStorage = true) {
+export function setStorageItem(key: string, value: string, toLocalStorage = false) {
   if (toLocalStorage) {
     localStorage.setItem(key, value);
   } else {
+    console.log("bra");
     STORAGE[key] = value;
   }
 }
