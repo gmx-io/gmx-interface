abea01ea5cf6425d20c5b4a8a3b2c0b5557429a9
micky <onemicky2024@gmail.com>
2024-09-20 12:24:53 +0200

    Revert "Revert "rollout rpc window fallback ab""
    This reverts commit 5f86886f92c952ebc78eea0e3e09b00be37560aa.

--- src/lib/multicall/Multicall.ts
+++ src/lib/multicall/Multicall.ts
@@ -282,7 +282,7 @@ export class Multicall {
       // eslint-disable-next-line no-console
       console.groupEnd();
 
-      if (!isFallbackMode && this.abFlags?.testRpcWindowFallback) {
+      if (!isFallbackMode) {
         this.fallbackRpcSwitcher?.trigger();
       }
 
@@ -373,14 +373,10 @@ export class Multicall {
       isAlchemy: isFallbackMode,
     });
 
-    if (this.abFlags?.testRpcWindowFallback) {
-      if (!isFallbackMode) {
-        this.fallbackRpcSwitcher?.trigger();
-      }
-
-      return await fallbackMulticall(new Error("multicall fallback error")).then(processResponse);
+    if (!isFallbackMode) {
+      this.fallbackRpcSwitcher?.trigger();
     }
 
-    return result;
+    return await fallbackMulticall(new Error("multicall fallback error")).then(processResponse);
   }
 }
