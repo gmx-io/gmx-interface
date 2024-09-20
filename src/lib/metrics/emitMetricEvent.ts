356674caccff9f86df4ff6217cf56cbc04047cc4
Divhead <i.divhead@gmail.com>
2024-09-20 14:14:51 +0300

    fixes by review

--- src/lib/metrics/emitMetricEvent.ts
+++ src/lib/metrics/emitMetricEvent.ts
@@ -3,13 +3,12 @@ import { MetricEventParams } from "./Metrics";
 export const METRIC_WINDOW_EVENT_NAME = "send-metric";
 export const METRIC_WINDOW_COUNTER_EVENT_NAME = "send-counter";
 
-export function emitMetricEvent<T extends MetricEventParams = never>({ event, data, time, isError, isCounter }: T) {
+export function emitMetricEvent<T extends MetricEventParams = never>({ event, data, time, isError }: T) {
   globalThis.dispatchEvent(
     new CustomEvent(METRIC_WINDOW_EVENT_NAME, {
       detail: {
         event: event,
         isError: isError,
-        isCounter,
         data: data,
         time: time,
       },
