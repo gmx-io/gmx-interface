4ac23b7edc27669c26cdbc1b6a677604e7c122e9
micky <onemicky2024@gmail.com>
2024-09-24 17:55:58 +0200

    test alchemy errors experiment

--- src/config/ab.ts
+++ src/config/ab.ts
@@ -1,7 +1,7 @@
 import mapValues from "lodash/mapValues";
 import { AB_FLAG_STORAGE_KEY } from "./localStorage";
 
-type Flag = "testSmartRpcSwitching";
+type Flag = "testSmartRpcSwitching" | "testAlchemyRpcErrorRate";
 
 type AbFlag = {
   enabled: boolean;
@@ -13,6 +13,7 @@ type AbStorage = {
 
 const abFlagsConfig: Record<Flag, number> = {
   testSmartRpcSwitching: 0.5,
+  testAlchemyRpcErrorRate: 0.05,
 };
 
 const flags: Flag[] = Object.keys(abFlagsConfig) as Flag[];
