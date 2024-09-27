b809a3840815a15c41b64f161472e14ae33b77fc
micky <onemicky2024@gmail.com>
2024-09-27 13:08:30 +0200

    rollback alchemy exp

--- src/lib/rpc/bestRpcTracker.ts
+++ src/lib/rpc/bestRpcTracker.ts
@@ -6,7 +6,6 @@ import {
   AVALANCHE,
   AVALANCHE_FUJI,
   getFallbackRpcUrl,
-  getAlchemyArbitrumHttpUrl,
 } from "config/chains";
 import { getRpcProviderKey } from "config/localStorage";
 import { isDebugMode } from "lib/localStorage";
@@ -66,10 +65,6 @@ type RpcTrackerState = {
     };
   };
 };
-
-if (getIsFlagEnabled("testAlchemyRpcErrorRate")) {
-  RPC_PROVIDERS[ARBITRUM].unshift(getAlchemyArbitrumHttpUrl());
-}
 
 const trackerState = initTrackerState();
 let trackerTimeoutId: number | null = null;
