688b2657fc0ca048bcdaed596195a4005b3aa0da
Hubért de Lalye <hubert-de-lalye@proton.me>
2024-11-08 17:51:41 +0400

    updated rainbowkit and wagmi

--- src/config/static/chains.ts
+++ src/config/static/chains.ts
@@ -14,3 +14,5 @@ export const ARBITRUM = 42161;
 export const ARBITRUM_GOERLI = 421613;
 export const FEES_HIGH_BPS = 50;
 export const DEFAULT_ALLOWED_SLIPPAGE_BPS = 30;
+
+export type Chains = typeof AVALANCHE | typeof AVALANCHE_FUJI | typeof ARBITRUM | typeof ARBITRUM_GOERLI;
