d070d9e964ee36d10fb97d9a8f5cac4ee2011799
Divhead <i.divhead@gmail.com>
2024-11-09 17:39:03 +0300

    upd comment

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,6 +2,6 @@ import { USD_DECIMALS } from "config/factors";
 import { expandDecimals } from "lib/numbers";
 
 export const HIGH_SPREAD_THRESHOLD = expandDecimals(1, USD_DECIMALS) / 100n; // 1%;
-export const HIGH_TRADE_VOLUME_FOR_FEEDBACK = expandDecimals(2_000_000, 30); // 1mx
+export const HIGH_TRADE_VOLUME_FOR_FEEDBACK = expandDecimals(2_000_000, 30); // 2mx
 
 export const DAY_OF_THE_WEEK_EPOCH_STARTS_UTC = 3;
