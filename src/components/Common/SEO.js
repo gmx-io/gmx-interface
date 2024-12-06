0bc37bcbbfa4f335ab697bdc2e7bb90375391a34
srslafazan <shainlafazan@Mac.lan>
2024-12-05 17:21:17 -0800

    Add twitter site property

--- src/components/Common/SEO.js
+++ src/components/Common/SEO.js
@@ -22,7 +22,7 @@ function SEO(props) {
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
         <meta name="twitter:card" content="summary_large_image" />
-        {/* <meta name="twitter:site" content="@gmx_io" /> */}
+        <meta name="twitter:site" content="@t3__money" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
