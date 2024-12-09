ff6ef51c8298283fbad5be18f7f460498bc6df1a
Hubért de Lalye <hubert-de-lalye@proton.me>
2024-12-09 17:25:28 +0400

    updated primary stroke and disabled button state

--- src/components/Synthetics/TradeBox/tradeboxConstants.tsx
+++ src/components/Synthetics/TradeBox/tradeboxConstants.tsx
@@ -35,7 +35,16 @@ export const tradeTypeLabels = {
  * @see https://www.notion.so/gmxio/New-Colors-for-Red-Green-and-Disabled-buttons-14c03574745d8070b2edc89744f3eff4
  */
 export const tradeTypeClassNames = {
-  [TradeType.Long]: { active: "!bg-[#1F3445] !text-green-500 border-b border-b-green-500" },
-  [TradeType.Short]: { active: "!bg-[#392A46] !text-red-500 border-b border-b-red-500" },
-  [TradeType.Swap]: { active: "!bg-[#252B57] !text-blue-300 border-b border-b-blue-300" },
+  [TradeType.Long]: {
+    active: "!bg-[#1F3445] !text-green-500 border-b border-b-green-500",
+    regular: "border-b border-b-[transparent]",
+  },
+  [TradeType.Short]: {
+    active: "!bg-[#392A46] !text-red-500 border-b border-b-red-500",
+    regular: "border-b border-b-[transparent]",
+  },
+  [TradeType.Swap]: {
+    active: "!bg-[#252B57] !text-blue-300 border-b border-b-blue-300",
+    regular: "border-b border-b-[transparent]",
+  },
 };
