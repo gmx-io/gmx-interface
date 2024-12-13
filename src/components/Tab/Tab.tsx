d8a44871f4489c6d9a36b0920c1da2b4c13901c7
midas-myth <midas-myth@proton.me>
2024-12-13 14:13:20 +0100

    Enhance instant tradebox curtain

--- src/components/Tab/Tab.tsx
+++ src/components/Tab/Tab.tsx
@@ -1,36 +1,22 @@
 import cx from "classnames";
 import { ReactNode } from "react";
 
-import "./Tab.scss";
+import "./Tab.css";
 
 type Props = {
-  theme?: "green" | "default";
   options: (string | number)[];
   option: string | number | undefined;
   setOption?: (option: any) => void;
   onChange?: (option: any) => void;
   type?: "block" | "inline";
   className?: string;
-  tabOptionClassName?: string;
   optionLabels?: Record<string | number, ReactNode> | string[];
   icons?: Record<string, ReactNode>;
   qa?: string;
 };
 
 export default function Tab(props: Props) {
-  const {
-    options,
-    option,
-    setOption,
-    onChange,
-    type = "block",
-    className,
-    optionLabels,
-    icons,
-    qa,
-    theme = "default",
-    tabOptionClassName,
-  } = props;
+  const { options, option, setOption, onChange, type = "block", className, optionLabels, icons, qa } = props;
   const onClick = (opt) => {
     if (setOption) {
       setOption(opt);
@@ -46,14 +32,7 @@ export default function Tab(props: Props) {
         const label = optionLabels && optionLabels[opt] ? optionLabels[opt] : opt;
         return (
           <div
-            className={cx(
-              "Tab-option",
-              {
-                active: opt === option,
-                "Tab-option-green": theme === "green",
-              },
-              tabOptionClassName
-            )}
+            className={cx("Tab-option flex items-center justify-center gap-8", "muted", { active: opt === option })}
             onClick={() => onClick(opt)}
             key={opt}
             data-qa={`${qa}-tab-${opt}`}
