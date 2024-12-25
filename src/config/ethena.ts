9fdacad4082465da8540ac17980890562b24e9de
midas-myth <midas-myth@proton.me>
2024-12-25 16:26:27 +0100

    Remove ethena incentives

--- src/config/ethena.ts
+++
@@ -1,7 +0,0 @@
-export const ETHENA_DASHBOARD_URL = "https://app.ethena.fi/join";
-
-export const isEthenaSatsIncentivizedMarket = function (marketAddress: string) {
-  return [
-    "0x0Cf1fb4d1FF67A3D8Ca92c9d6643F8F9be8e03E5", // ETH/USD [wstETH-USDe]
-  ].includes(marketAddress);
-};
