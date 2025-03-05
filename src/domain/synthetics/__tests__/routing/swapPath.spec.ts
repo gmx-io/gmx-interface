1073e55033d881555ef51075ada53eaabef6bff0
Divhead <110596516+divhead@users.noreply.github.com>
2025-03-05 17:33:27 +0000

    fix swap tests and add checks

--- src/domain/synthetics/__tests__/routing/swapPath.spec.ts
+++ src/domain/synthetics/__tests__/routing/swapPath.spec.ts
@@ -9,7 +9,6 @@ import {
   getMarketsGraph,
 } from "domain/synthetics/trade";
 import { describe, expect, it } from "vitest";
-
 const marketsKeys = [
   "AVAX-AVAX-USDC",
   "ETH-ETH-USDC",
@@ -106,9 +105,9 @@ describe("swapPath", () => {
         expected: ["ETH-ETH-USDC"],
         expectedPaths: [
           ["ETH-ETH-USDC"],
+          ["SOL-ETH-USDC"],
           ["ETH-ETH-DAI", "SPOT-USDC-DAI"],
           ["ETH-ETH-DAI", "SPOT-DAI-USDC"],
-          ["SOL-ETH-USDC"],
         ],
       },
       {
@@ -134,9 +133,9 @@ describe("swapPath", () => {
         expected: ["ETH-ETH-USDC", "AVAX-AVAX-USDC"],
         expectedPaths: [
           ["ETH-ETH-USDC", "AVAX-AVAX-USDC"],
+          ["SOL-ETH-USDC", "AVAX-AVAX-USDC"],
           ["ETH-ETH-DAI", "SPOT-USDC-DAI", "AVAX-AVAX-USDC"],
           ["ETH-ETH-DAI", "SPOT-DAI-USDC", "AVAX-AVAX-USDC"],
-          ["SOL-ETH-USDC", "AVAX-AVAX-USDC"],
         ],
       },
       {
@@ -211,9 +210,9 @@ describe("swapPath", () => {
         expected: ["SOL-ETH-USDC", "AVAX-AVAX-USDC"],
         expectedPaths: [
           ["ETH-ETH-USDC", "AVAX-AVAX-USDC"],
+          ["SOL-ETH-USDC", "AVAX-AVAX-USDC"],
           ["ETH-ETH-DAI", "SPOT-USDC-DAI", "AVAX-AVAX-USDC"],
           ["ETH-ETH-DAI", "SPOT-DAI-USDC", "AVAX-AVAX-USDC"],
-          ["SOL-ETH-USDC", "AVAX-AVAX-USDC"],
         ],
       },
       {
