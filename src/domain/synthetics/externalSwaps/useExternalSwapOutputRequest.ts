64a8a148c0f4f74b265ca29fdd8c69869fe3bb19
Divhead <110596516+divhead@users.noreply.github.com>
2025-03-05 21:18:45 +0000

    fix enabled flag

--- src/domain/synthetics/externalSwaps/useExternalSwapOutputRequest.ts
+++ src/domain/synthetics/externalSwaps/useExternalSwapOutputRequest.ts
@@ -45,70 +45,74 @@ export function useExternalSwapOutputRequest({
   const tokensKey = `${tokenInAddress}:${tokenOutAddress};`;
   const prevTokensKey = usePrevious(tokensKey);
 
-  const { data, isLoading, error } = useSWR<{ quote: ExternalSwapOutput; requestKey: string }>(debouncedKey, {
-    keepPreviousData: prevTokensKey === tokensKey,
-    fetcher: async (requestKey: string) => {
-      try {
-        if (
-          !tokenInAddress ||
-          !tokenOutAddress ||
-          amountIn === undefined ||
-          slippage === undefined ||
-          gasPrice === undefined
-        ) {
-          throw new Error("Invalid swap parameters");
-        }
+  const { data, isLoading, error } = useSWR<{ quote: ExternalSwapOutput; requestKey: string }>(
+    enabled ? debouncedKey : null,
+    {
+      keepPreviousData: enabled && prevTokensKey === tokensKey,
+      fetcher: async (requestKey: string) => {
+        try {
+          if (
+            !tokenInAddress ||
+            !tokenOutAddress ||
+            amountIn === undefined ||
+            slippage === undefined ||
+            gasPrice === undefined
+          ) {
+            throw new Error("Invalid swap parameters");
+          }
 
-        const result = await Promise.race([
-          getOpenOceanTxnData({
-            chainId,
-            senderAddress: getContract(chainId, "ExternalHandler"),
-            receiverAddress: getContract(chainId, "OrderVault"),
-            tokenInAddress: convertTokenAddress(chainId, tokenInAddress, "wrapped"),
-            tokenOutAddress: convertTokenAddress(chainId, tokenOutAddress, "wrapped"),
+          const result = await Promise.race([
+            getOpenOceanTxnData({
+              chainId,
+              senderAddress: getContract(chainId, "ExternalHandler"),
+              receiverAddress: getContract(chainId, "OrderVault"),
+              tokenInAddress: convertTokenAddress(chainId, tokenInAddress, "wrapped"),
+              tokenOutAddress: convertTokenAddress(chainId, tokenOutAddress, "wrapped"),
+              amountIn,
+              gasPrice,
+              slippage,
+            }),
+            sleep(5000).then(() => {
+              throw new Error("External swap request timeout");
+            }),
+          ]);
+
+          if (!result) {
+            throw new Error("Failed to fetch open ocean txn data");
+          }
+
+          const quote: ExternalSwapOutput = {
+            aggregator: ExternalSwapAggregator.OpenOcean,
+            inTokenAddress: tokenInAddress,
+            outTokenAddress: tokenOutAddress,
             amountIn,
-            gasPrice,
-            slippage,
-          }),
-          sleep(5000).then(() => {
-            throw new Error("External swap request timeout");
-          }),
-        ]);
+            amountOut: result.outputAmount,
+            usdIn: result.usdIn,
+            usdOut: result.usdOut,
+            priceIn: result.priceIn,
+            priceOut: result.priceOut,
+            feesUsd:
+              result.usdIn !== undefined && result.usdOut !== undefined ? result.usdIn - result.usdOut : undefined,
+            txnData: {
+              to: result.to,
+              data: result.data,
+              value: result.value,
+              estimatedGas: result.estimatedGas,
+            },
+          };
 
-        if (!result) {
-          throw new Error("Failed to fetch open ocean txn data");
+          return {
+            quote,
+            requestKey,
+          };
+        } catch (error) {
+          // eslint-disable-next-line no-console
+          console.error("Error fetching external swap quote", error);
+          throw error;
         }
-
-        const quote: ExternalSwapOutput = {
-          aggregator: ExternalSwapAggregator.OpenOcean,
-          inTokenAddress: tokenInAddress,
-          outTokenAddress: tokenOutAddress,
-          amountIn,
-          amountOut: result.outputAmount,
-          usdIn: result.usdIn,
-          usdOut: result.usdOut,
-          priceIn: result.priceIn,
-          priceOut: result.priceOut,
-          feesUsd: result.usdIn !== undefined && result.usdOut !== undefined ? result.usdIn - result.usdOut : undefined,
-          txnData: {
-            to: result.to,
-            data: result.data,
-            value: result.value,
-            estimatedGas: result.estimatedGas,
-          },
-        };
-
-        return {
-          quote,
-          requestKey,
-        };
-      } catch (error) {
-        // eslint-disable-next-line no-console
-        console.error("Error fetching external swap quote", error);
-        throw error;
-      }
-    },
-  });
+      },
+    }
+  );
 
   const { tokensAllowanceData } = useTokensAllowanceData(chainId, {
     spenderAddress: data?.quote?.txnData?.to,
