066124feba5c11ba2916264b8b0bb407efc5b98a
Qwinn <qwinndev@proton.me>
2025-03-10 19:17:23 +0200

    Change market to pool in PositionItem

--- src/components/Synthetics/PositionItem/PositionItem.tsx
+++ src/components/Synthetics/PositionItem/PositionItem.tsx
@@ -397,7 +397,7 @@ export function PositionItem(p: Props) {
               renderContent={() => (
                 <div>
                   <StatsTooltipRow
-                    label={t`Market`}
+                    label={t`Pool`}
                     value={
                       <div className="flex items-center">
                         <span>{indexName && indexName}</span>
@@ -576,7 +576,7 @@ export function PositionItem(p: Props) {
               )}
               <div className="App-card-row">
                 <div className="label">
-                  <Trans>Market</Trans>
+                  <Trans>Pool</Trans>
                 </div>
                 <div>
                   <div className="flex items-start">
