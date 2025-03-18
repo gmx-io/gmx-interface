55604f13db7d4dc513efef3dfabed0f5e6a2b3b7
divhead <110596516+divhead@users.noreply.github.com>
2025-03-18 14:29:06 +0300

    change to 15bps

--- src/config/externalSwaps.ts
+++ src/config/externalSwaps.ts
@@ -5,7 +5,7 @@ import { DEBUG_SWAP_SETTINGS_KEY } from "./localStorage";
 import { deserializeBigIntsInObject, serializeBigIntsInObject } from "lib/numbers";
 
 // Enable external swap if price impact delta is less than this threshold
-export const SWAP_PRICE_IMPACT_FOR_EXTERNAL_SWAP_THRESHOLD_BPS = -20n;
+export const SWAP_PRICE_IMPACT_FOR_EXTERNAL_SWAP_THRESHOLD_BPS = -15n;
 
 export const HIGH_EXTERNAL_SWAP_FEES_BPS = 200; // 2%
 
