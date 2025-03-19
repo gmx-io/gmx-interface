646ba8133d22211747541f9ecd63aa45931454c8
Qwinn <qwinndev@proton.me>
2025-03-19 17:44:14 +0200

    Use common links styles in tooltip

--- src/components/Synthetics/TradeBox/components/TradeBoxLongShortInfoIcon.tsx
+++ src/components/Synthetics/TradeBox/components/TradeBoxLongShortInfoIcon.tsx
@@ -5,33 +5,34 @@ import Tooltip from "components/Tooltip/Tooltip";
 
 import InfoCircleOutlineIcon from "img/ic_info_circle_outline.svg?react";
 
+const UL_CLASS_NAME = "text-body-medium flex list-disc flex-col gap-2";
+const LI_CLASS_NAME = "p-0";
+
 export default function TradeBoxLongShortInfoIcon({ isLong, isMobile }: { isLong: boolean; isMobile: boolean }) {
   const longMarketTooltipContent = (
-    <ul className="text-body-medium flex list-disc flex-col gap-2">
-      <li className="p-0">
+    <ul className={UL_CLASS_NAME}>
+      <li className={LI_CLASS_NAME}>
         <Trans>Long Market: Increase a long position at the current price.</Trans>
       </li>
-      <li className="p-0">
-        <Trans>Long Limit: Increase a long position when the price is below the trigger price.</Trans>
-        <ExternalLink className="!text-blue-300 !no-underline" href="https://docs.gmx.io/docs/trading/v2/#limit-orders">
+      <li className={LI_CLASS_NAME}>
+        <Trans>Long Limit: Increase a long position when the price is below the trigger price.</Trans>{" "}
+        <ExternalLink href="https://docs.gmx.io/docs/trading/v2/#limit-orders">
           <Trans>Read more</Trans>
         </ExternalLink>
         .
       </li>
-      <li className="p-0">
-        <Trans>Long TP/SL: Decrease a long position when the trigger price is reached.</Trans>
+      <li className={LI_CLASS_NAME}>
+        <Trans>Long TP/SL: Decrease a long position when the trigger price is reached.</Trans>{" "}
         <ExternalLink
-          className="!text-blue-300 !no-underline"
           href="https://docs.gmx.io/docs/trading/v2/#take-profit-and-stop-loss-orders"
         >
           <Trans>Read more</Trans>
         </ExternalLink>
         .
       </li>
-      <li className="p-0">
-        <Trans>Long Stop Market: Increase a long position when the price is above the trigger price.</Trans>
+      <li className={LI_CLASS_NAME}>
+        <Trans>Long Stop Market: Increase a long position when the price is above the trigger price.</Trans>{" "}
         <ExternalLink
-          className="!text-blue-300 !no-underline"
           href="https://docs.gmx.io/docs/trading/v2/#stop-market-orders"
         >
           <Trans>Read more</Trans>
@@ -42,31 +43,29 @@ export default function TradeBoxLongShortInfoIcon({ isLong, isMobile }: { isLong
   );
 
   const shortMarketTooltipContent = (
-    <ul className="text-body-medium flex list-disc flex-col gap-2">
-      <li className="p-0">
+    <ul className={UL_CLASS_NAME}>
+      <li className={LI_CLASS_NAME}>
         <Trans>Short Market: Increase a short position at the current price.</Trans>
       </li>
-      <li className="p-0">
-        <Trans>Short Limit: Increase a short position when the price is above the trigger price.</Trans>
-        <ExternalLink className="!text-blue-300 !no-underline" href="https://docs.gmx.io/docs/trading/v2/#limit-orders">
+      <li className={LI_CLASS_NAME}>
+        <Trans>Short Limit: Increase a short position when the price is above the trigger price.</Trans>{" "}
+        <ExternalLink href="https://docs.gmx.io/docs/trading/v2/#limit-orders">
           <Trans>Read more</Trans>
         </ExternalLink>
         .
       </li>
-      <li className="p-0">
-        <Trans>Short TP/SL: Decrease a short position when the trigger price is reached.</Trans>
+      <li className={LI_CLASS_NAME}>
+        <Trans>Short TP/SL: Decrease a short position when the trigger price is reached.</Trans>{" "}
         <ExternalLink
-          className="!text-blue-300 !no-underline"
           href="https://docs.gmx.io/docs/trading/v2/#take-profit-and-stop-loss-orders"
         >
           <Trans>Read more</Trans>
         </ExternalLink>
         .
       </li>
-      <li className="p-0">
-        <Trans>Short Stop Market: Increase a short position when the price is below the trigger price.</Trans>
+      <li className={LI_CLASS_NAME}>
+        <Trans>Short Stop Market: Increase a short position when the price is below the trigger price.</Trans>{" "}
         <ExternalLink
-          className="!text-blue-300 !no-underline"
           href="https://docs.gmx.io/docs/trading/v2/#stop-market-orders"
         >
           <Trans>Read more</Trans>
