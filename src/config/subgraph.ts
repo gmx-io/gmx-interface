489258de1b5a00222c6eca049e1f29cf4baec4b7
midas-myth <midas-myth@proton.me>
2025-04-22 15:57:28 +0200

    Update Arbitrum Sepolia Subgraph URL

--- src/config/subgraph.ts
+++ src/config/subgraph.ts
@@ -26,7 +26,7 @@ const SUBGRAPH_URLS = {
   },
 
   [ARBITRUM_SEPOLIA]: {
-    subsquid: "https://gmx-test.squids.live/multichain-arbitrum-sepolia@0a9748/api/graphql",
+    subsquid: "https://gmx-test.squids.live/multichain-arbitrum-sepolia@ef9d10/api/graphql",
     // subsquid: "http://localhost:4000/graphql",
   },
 
