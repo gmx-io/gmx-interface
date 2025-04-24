e51b6a10cbb6d9da41f27d2a5ec720ccfcf37ae0
Hubért de Lalye <hubert-de-lalye@proton.me>
2025-04-24 14:49:55 +0400

    updated testUtil.ts in sdk

--- sdk/src/utils/testUtil.ts
+++ sdk/src/utils/testUtil.ts
@@ -19,7 +19,7 @@ export const arbitrumSdkConfig: GmxSdkConfig = {
   oracleUrl: "https://arbitrum-api.gmxinfra.io",
   rpcUrl: "https://arb1.arbitrum.io/rpc",
   walletClient: client,
-  subsquidUrl: "https://gmx.squids.live/gmx-synthetics-arbitrum@8a30a5/api/graphql",
+  subsquidUrl: "https://gmx.squids.live/gmx-synthetics-arbitrum@e27d93/api/graphql",
 };
 
 export const arbitrumSdk = new GmxSdk(arbitrumSdkConfig);
