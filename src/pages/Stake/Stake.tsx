ffd2ce02d93953606a152e4c750d4c0a8754750e
Qwinn <qwinndev@proton.me>
2025-04-29 13:55:10 +0200

    Stake page update

--- src/pages/Stake/Stake.tsx
+++ src/pages/Stake/Stake.tsx
@@ -9,23 +9,19 @@ import { getIncentivesV2Url } from "config/links";
 import { usePendingTxns } from "context/PendingTxnsContext/PendingTxnsContext";
 import useIncentiveStats from "domain/synthetics/common/useIncentiveStats";
 import { getTotalGmInfo, useMarketTokensData } from "domain/synthetics/markets";
-import { useGmMarketsApy } from "domain/synthetics/markets/useGmMarketsApy";
-import { useAnyAirdroppedTokenTitle } from "domain/synthetics/tokens/useAirdroppedTokenTitle";
 import { useLpInterviewNotification } from "domain/synthetics/userFeedback/useLpInterviewNotification";
 import { useChainId } from "lib/chains";
 import { contractFetcher } from "lib/contracts";
-import { PLACEHOLDER_ACCOUNT } from "lib/legacy";
+import { PLACEHOLDER_ACCOUNT, getPageTitle } from "lib/legacy";
 import { formatAmount } from "lib/numbers";
 import useWallet from "lib/wallets/useWallet";
 import { bigMath } from "sdk/utils/bigmath";
 
+import SEO from "components/Common/SEO";
 import ExternalLink from "components/ExternalLink/ExternalLink";
 import Footer from "components/Footer/Footer";
 import { InterviewModal } from "components/InterviewModal/InterviewModal";
 import PageTitle from "components/PageTitle/PageTitle";
-import { GlvList } from "components/Synthetics/GmList/GlvList";
-import { GmList } from "components/Synthetics/GmList/GmList";
-import UserIncentiveDistributionList from "components/Synthetics/UserIncentiveDistributionList/UserIncentiveDistributionList";
 
 import { EscrowedGmxCard } from "./EscrowedGmxCard";
 import { GlpCard } from "./GlpCard";
@@ -34,7 +30,6 @@ import { StakeModal } from "./StakeModal";
 import { TotalRewardsCard } from "./TotalRewardsCard";
 import { UnstakeModal } from "./UnstakeModal";
 import { useProcessedData } from "./useProcessedData";
-import { Vesting } from "./Vesting";
 
 import "./Stake.css";
 
@@ -71,8 +66,6 @@ export default function Stake() {
     }
   }, [incentiveStats?.lp?.isActive, incentiveStats?.trading?.isActive]);
 
-  const incentivesToken = useAnyAirdroppedTokenTitle();
-
   const { setPendingTxns } = usePendingTxns();
 
   const [isStakeGmxModalVisible, setIsStakeGmxModalVisible] = useState(false);
@@ -96,13 +89,6 @@ export default function Stake() {
   const feeGmxTrackerAddress = getContract(chainId, "FeeGmxTracker");
 
   const { marketTokensData } = useMarketTokensData(chainId, { isDeposit: false });
-  const {
-    marketsTokensApyData,
-    marketsTokensIncentiveAprData,
-    glvTokensIncentiveAprData,
-    marketsTokensLidoAprData,
-    glvApyInfoData,
-  } = useGmMarketsApy(chainId);
 
   const { data: sbfGmxBalance } = useSWR(
     [`StakeV2:sbfGmxBalance:${active}`, chainId, feeGmxTrackerAddress, "balanceOf", account ?? PLACEHOLDER_ACCOUNT],
@@ -196,6 +182,8 @@ export default function Stake() {
 
   return (
     <div className="default-container page-layout">
+      <SEO title={getPageTitle(t`Stake`)} />
+
       <StakeModal
         isVisible={isStakeGmxModalVisible}
         setIsVisible={setIsStakeGmxModalVisible}
@@ -251,16 +239,17 @@ export default function Stake() {
 
       <PageTitle
         isTop
-        title={t`Earn`}
+        title={t`Stake`}
         qa="earn-page"
         subtitle={
           <div>
             <Trans>
-              Stake <ExternalLink href="https://docs.gmx.io/docs/tokenomics/gmx-token">GMX</ExternalLink> and buy{" "}
-              <ExternalLink href="https://docs.gmx.io/docs/providing-liquidity/v2">GM</ExternalLink> or{" "}
-              <ExternalLink href="https://docs.gmx.io/docs/providing-liquidity/v1">GLP</ExternalLink> to earn rewards.
+              Deposit <ExternalLink href="https://docs.gmx.io/docs/tokenomics/gmx-token">GMX</ExternalLink>,{" "}
+              <ExternalLink href="https://docs.gmx.io/docs/providing-liquidity/gmx-token">esGMX</ExternalLink> and{" "}
+              <ExternalLink href="https://docs.gmx.io/docs/providing-liquidity/v1">GLP</ExternalLink> tokens to earn
+              rewards.
             </Trans>
-            {earnMsg && <div className="Page-descriptionж">{earnMsg}</div>}
+            {earnMsg && <div className="Page-description">{earnMsg}</div>}
             {incentivesMessage}
           </div>
         }
@@ -289,58 +278,6 @@ export default function Stake() {
         </div>
       </div>
 
-      <div className="mt-15">
-        <PageTitle
-          title={<Trans>Select a GLV Vault</Trans>}
-          showNetworkIcon={false}
-          subtitle={
-            <Trans>
-              Yield-optimized vaults enabling trading across multiple markets, backed by the tokens listed in brackets.
-            </Trans>
-          }
-        />
-        <GlvList
-          marketsTokensApyData={marketsTokensApyData}
-          marketsTokensIncentiveAprData={marketsTokensIncentiveAprData}
-          glvTokensIncentiveAprData={glvTokensIncentiveAprData}
-          marketsTokensLidoAprData={marketsTokensLidoAprData}
-          glvTokensApyData={glvApyInfoData}
-          shouldScrollToTop
-          isDeposit={false}
-        />
-        <PageTitle
-          title={t`Select a GM Pool`}
-          showNetworkIcon={false}
-          subtitle={
-            <Trans>Pools that enable trading for a single market, backed by the tokens listed in brackets.</Trans>
-          }
-        />
-        <GmList
-          marketsTokensApyData={marketsTokensApyData}
-          marketsTokensIncentiveAprData={marketsTokensIncentiveAprData}
-          glvTokensIncentiveAprData={undefined}
-          marketsTokensLidoAprData={marketsTokensLidoAprData}
-          glvTokensApyData={undefined}
-          isDeposit={false}
-          shouldScrollToTop
-        />
-      </div>
-
-      <Vesting processedData={processedData} />
-
-      <div className="mt-10">
-        <PageTitle
-          title={t`Incentives & Prizes`}
-          subtitle={
-            incentiveStats?.lp?.isActive || incentiveStats?.trading?.isActive ? (
-              <Trans>Earn {incentivesToken} token incentives by purchasing GM tokens or trading in GMX V2.</Trans>
-            ) : (
-              <Trans>Earn prizes by participating in GMX Trading Competitions.</Trans>
-            )
-          }
-        />
-      </div>
-      <UserIncentiveDistributionList />
       <Footer />
 
       <InterviewModal type="lp" isVisible={isLpInterviewModalVisible} setIsVisible={setIsLpInterviewModalVisible} />
