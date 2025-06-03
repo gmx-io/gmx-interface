2552460212f205d089a3268779bb53719c5e2a6e
Divhead <110596516+divhead@users.noreply.github.com>
2025-06-03 06:03:54 -0700

    add ab flag

--- src/components/OneClickPromoBanner/OneClickPromoBanner.tsx
+++ src/components/OneClickPromoBanner/OneClickPromoBanner.tsx
@@ -2,8 +2,8 @@ import { Trans } from "@lingui/macro";
 import { useCallback } from "react";
 import { useLocalStorage } from "react-use";
 
+import { getIsFlagEnabled } from "config/ab";
 import { getOneClickTradingPromoHiddenKey } from "config/localStorage";
-import { useSubaccountContext } from "context/SubaccountContext/SubaccountContextProvider";
 import { useChainId } from "lib/chains";
 
 import { ColorfulBanner } from "components/ColorfulBanner/ColorfulBanner";
@@ -12,14 +12,12 @@ import OneClickIcon from "img/ic_one_click.svg?react";
 
 export function OneClickPromoBanner({ openSettings, isShort }: { openSettings: () => void; isShort?: boolean }) {
   const { chainId } = useChainId();
-  const subaccountState = useSubaccountContext();
   const [isOneClickPromoHidden, setIsOneClickPromoHidden] = useLocalStorage(
     getOneClickTradingPromoHiddenKey(chainId),
     false
   );
 
-  const shouldShow =
-    subaccountState.subaccountConfig && !subaccountState.subaccountConfig.isNew && !isOneClickPromoHidden;
+  const shouldShow = getIsFlagEnabled("testOneClickPromo") && !isOneClickPromoHidden;
 
   const onClickEnable = useCallback(() => {
     openSettings();
