be3fb8e67e7a13625fee8a44e9dc8e58b9c5c028
Qwinn <qwinndev@proton.me>
2025-07-01 15:40:02 +0900

    Use BTC icon for bBTC token

--- src/components/TokenIcon/TokenIcon.tsx
+++ src/components/TokenIcon/TokenIcon.tsx
@@ -1,17 +1,11 @@
 import cx from "classnames";
 
-import { BOTANIX } from "config/chains";
-import { useChainId } from "lib/chains";
 import { importImage } from "lib/legacy";
 
 import "./TokenIcon.scss";
 
-function getIconUrlPath(symbol, size: 24 | 40, chainId: number) {
+function getIconUrlPath(symbol, size: 24 | 40) {
   if (!symbol || !size) return;
-
-  if (symbol === "BTC" && chainId === BOTANIX) {
-    return `ic_bbtc_${size}.svg`;
-  }
 
   return `ic_${symbol.toLowerCase()}_${size}.svg`;
 }
@@ -25,8 +19,7 @@ type Props = {
 };
 
 function TokenIcon({ className, symbol, displaySize, importSize = 24, badge }: Props) {
-  const { chainId } = useChainId();
-  const iconPath = getIconUrlPath(symbol, importSize, chainId);
+  const iconPath = getIconUrlPath(symbol, importSize);
   const classNames = cx("Token-icon inline", className);
 
   if (!iconPath) return <></>;
@@ -55,14 +48,14 @@ function TokenIcon({ className, symbol, displaySize, importSize = 24, badge }: P
         <span className="absolute -bottom-8 -right-8 flex flex-row items-center justify-center !text-white">
           <img
             className="z-20 -mr-10 rounded-[100%] border border-slate-800"
-            src={importImage(getIconUrlPath(badge[0], 24, chainId))}
+            src={importImage(getIconUrlPath(badge[0], 24))}
             alt={badge[0]}
             width={20}
             height={20}
           />
           <img
             className="z-10 rounded-[100%] border border-slate-800"
-            src={importImage(getIconUrlPath(badge[1], 24, chainId))}
+            src={importImage(getIconUrlPath(badge[1], 24))}
             alt={badge[0]}
             width={20}
             height={20}
