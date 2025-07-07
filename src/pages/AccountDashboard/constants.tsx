7fe733437635895f8da635e767bc5c42f56a7135
Qwinn <qwinndev@proton.me>
2025-07-07 10:49:42 +0200

    Fix botanix accounts tabs

--- src/pages/AccountDashboard/constants.tsx
+++ src/pages/AccountDashboard/constants.tsx
@@ -1,6 +1,6 @@
 import invert from "lodash/invert";
 
-import { ARBITRUM, AVALANCHE, AVALANCHE_FUJI } from "config/chains";
+import { ARBITRUM, AVALANCHE, AVALANCHE_FUJI, BOTANIX } from "config/chains";
 
 export const NETWORK_QUERY_PARAM = "network";
 export const VERSION_QUERY_PARAM = "v";
@@ -9,6 +9,7 @@ export const NETWORK_SLUGS_ID_MAP = {
   arbitrum: ARBITRUM,
   avalanche: AVALANCHE,
   avalanche_fuji: AVALANCHE_FUJI,
+  botanix: BOTANIX,
 };
 
 export const NETWORK_ID_SLUGS_MAP = invert(NETWORK_SLUGS_ID_MAP);
