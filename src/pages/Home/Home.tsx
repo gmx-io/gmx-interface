1e3aeb27f97ba820f15b386e76357424afe6e8bd
M Dev <mdev_fe@pm.me>
2025-07-13 19:43:37 +0200

    feat: add modal to home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,13 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { userAnalytics } from "lib/userAnalytics";
 import { LandingPageViewEvent } from "lib/userAnalytics/types";
 
 import { HeroSection } from "./sections/HeroSection/HeroSection";
 import { LaunchSection } from "./sections/LaunchSection/LaunchSection";
+import { RedirectModal } from "./sections/RedirectModal/RedirectModal";
 
-type Props = {
-  showRedirectModal: (to: string) => void;
-};
-
-export default function Home({ showRedirectModal }: Props) {
+export default function Home(_) {
   useEffect(() => {
     userAnalytics.pushEvent<LandingPageViewEvent>(
       {
@@ -23,10 +20,17 @@ export default function Home({ showRedirectModal }: Props) {
     );
   }, []);
 
+  const [redirectModalTo, setRedirectModalTo] = useState<string | null>(null);
+
+  const showRedirectModal = (to: string) => {
+    setRedirectModalTo(to);
+  };
+
   return (
     <div className="proportional-nums text-white">
       <HeroSection showRedirectModal={showRedirectModal} />
       <LaunchSection showRedirectModal={showRedirectModal} />
+      {redirectModalTo && <RedirectModal onClose={() => setRedirectModalTo(null)} to={redirectModalTo} />}
     </div>
   );
 }
