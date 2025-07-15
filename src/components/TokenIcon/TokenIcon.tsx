fa517d9d8696a69b84e9ccbde55c95cf20d96ff4
Hubért de Lalye <hubert-de-lalye@proton.me>
2025-07-15 13:43:21 +0400

    removed unused code

--- src/components/TokenIcon/TokenIcon.tsx
+++ src/components/TokenIcon/TokenIcon.tsx
@@ -24,14 +24,12 @@ function TokenIcon({ className, symbol, displaySize, importSize = 24, badge }: P
 
   if (!iconPath) return <></>;
 
-  const imageSource = importImage(iconPath);
-
   let sub;
   const img = (
     <img
       data-qa="token-icon"
       className={classNames}
-      src={imageSource}
+      src={importImage(iconPath)}
       alt={symbol}
       width={displaySize}
       height={displaySize}
@@ -46,28 +44,18 @@ function TokenIcon({ className, symbol, displaySize, importSize = 24, badge }: P
         </span>
       );
     } else {
-      const badge0Path = getIconUrlPath(badge[0], 24);
-      const badge1Path = getIconUrlPath(badge[1], 24);
-
-      if (!badge0Path || !badge1Path) {
-        return img;
-      }
-
-      const badge0Source = importImage(badge0Path);
-      const badge1Source = importImage(badge1Path);
-
       sub = (
         <span className="absolute -bottom-8 -right-8 flex flex-row items-center justify-center !text-white">
           <img
             className="z-20 -mr-10 rounded-[100%] border border-slate-800"
-            src={badge0Source || ""}
+            src={importImage(getIconUrlPath(badge[0], 24))}
             alt={badge[0]}
             width={20}
             height={20}
           />
           <img
             className="z-10 rounded-[100%] border border-slate-800"
-            src={badge1Source || ""}
+            src={importImage(getIconUrlPath(badge[1], 24))}
             alt={badge[0]}
             width={20}
             height={20}
