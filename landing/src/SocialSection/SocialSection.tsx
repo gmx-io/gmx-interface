272cd136fc24cbdf807ef550b76ceb35ea85eb8f
M Dev <mdev_fe@pm.me>
2025-07-28 14:02:28 +0200

    feat: add SocialSection component to Home page

---
+++ landing/src/SocialSection/SocialSection.tsx
@@ -0,0 +1,9 @@
+export function SocialSection() {
+  return (
+    <section className="bg-fiord-700 border-fiord-500 flex w-full border-t-0 px-16 pt-0 text-white sm:border-t-[0.5px] sm:px-80 sm:py-[120px]">
+      <div className="mx-auto flex w-[1200px] flex-col gap-28 overflow-hidden sm:gap-44">
+        <div className="flex w-full flex-row items-end justify-between gap-16"></div>
+      </div>
+    </section>
+  );
+}
