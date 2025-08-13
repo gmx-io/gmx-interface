f79147a510ef58be7cb48195fc67732692b7def9
Hubért de Lalye <hubert-de-lalye@proton.me>
2025-08-13 14:43:28 +0400

    arranged files regarding claims

--- src/components/Synthetics/UserIncentiveDistributionList/ClaimableAmounts.tsx
+++ src/components/Synthetics/UserIncentiveDistributionList/ClaimableAmounts.tsx
@@ -1,140 +1,29 @@
 import { Trans } from "@lingui/macro";
 import { useCallback, useMemo, useState } from "react";
-import { ImSpinner2 } from "react-icons/im";
 import Skeleton from "react-loading-skeleton";
 import { useLocalStorage } from "react-use";
-import useSWR from "swr";
-import { encodeFunctionData } from "viem";
 
 import { getContract } from "config/contracts";
+import { getClaimTermsAcceptedKey } from "config/localStorage";
 import { useSettings } from "context/SettingsContext/SettingsContextProvider";
 import { selectChainId } from "context/SyntheticsStateContext/selectors/globalSelectors";
 import { useSelector } from "context/SyntheticsStateContext/utils";
-import useUserClaimableAmounts, { GLP_DISTRIBUTION_ID } from "domain/synthetics/common/useUserClaimableAmounts";
+import { createClaimAmountsTransaction } from "domain/synthetics/claims/createClaimTransaction";
+import { useClaimExecutionFee } from "domain/synthetics/claims/useClaimExecutionFee";
+import { useClaimFundsTransactionCallback } from "domain/synthetics/claims/useClaimFundsTransactionCallback";
+import useUserClaimableAmounts, { GLP_DISTRIBUTION_ID } from "domain/synthetics/claims/useUserClaimableAmounts";
 import { estimateExecutionGasPrice, getExecutionFeeBufferBps } from "domain/synthetics/fees/utils/executionFee";
 import { useTokenBalances } from "domain/synthetics/tokens";
-import { estimateGasLimit } from "lib/gas/estimateGasLimit";
-import { getGasPrice } from "lib/gas/gasPrice";
-import { helperToast } from "lib/helperToast";
 import { formatBalanceAmount, formatUsd } from "lib/numbers";
-import { sendWalletTransaction, TxnEventName } from "lib/transactions";
-import { WalletSigner } from "lib/wallets";
+import { getByKey } from "lib/objects";
 import { useIsSafeAccount } from "lib/wallets/useIsSmartAccount";
 import useWallet from "lib/wallets/useWallet";
-import ClaimHandlerAbi from "sdk/abis/ClaimHandler.json";
-import { getToken, NATIVE_TOKEN_ADDRESS } from "sdk/configs/tokens";
+import { NATIVE_TOKEN_ADDRESS } from "sdk/configs/tokens";
 
 import { AlertInfoCard } from "components/AlertInfo/AlertInfoCard";
 import Button from "components/Button/Button";
 import Checkbox from "components/Checkbox/Checkbox";
-import { ToastifyDebug } from "components/ToastifyDebug/ToastifyDebug";
 import TooltipWithPortal from "components/Tooltip/TooltipWithPortal";
-
-function getCallData(tokens: string[], account: string, signature: string, distributionId: bigint) {
-  const params = tokens.map((token) => ({
-    token,
-    distributionId,
-    termsSignature: signature as `0x${string}`,
-  }));
-
-  return encodeFunctionData({
-    abi: ClaimHandlerAbi.abi,
-    functionName: "claimFunds",
-    args: [params, account],
-  });
-}
-
-function createClaimAmountsTransaction(data: {
-  tokens: string[];
-  chainId: number;
-  signer: WalletSigner;
-  account: string;
-  signature: string;
-  distributionId: bigint;
-  claimableTokenTitles: Record<string, string>;
-}) {
-  const { tokens, chainId, signer, account, signature, distributionId, claimableTokenTitles } = data;
-  const callData = getCallData(tokens, account, signature, distributionId);
-
-  return sendWalletTransaction({
-    chainId,
-    signer,
-    to: getContract(chainId, "ClaimHandler"),
-    callData,
-    value: 0n,
-    callback: async (event) => {
-      switch (event.event) {
-        case TxnEventName.Submitted:
-          helperToast.success(
-            <div className="flex flex-col gap-10">
-              <div className="text-body-medium font-bold">
-                <Trans>Processing your claim…</Trans>
-              </div>
-              <div className="flex flex-row gap-10 text-gray-200">
-                <Trans>This may take a few minutes.</Trans>
-                <ImSpinner2 width={60} height={60} className="spin size-15 text-white" />
-              </div>
-            </div>
-          );
-
-          return;
-        case TxnEventName.Error:
-          helperToast.error(
-            <div className="flex flex-col gap-10">
-              <div className="text-body-medium font-bold">
-                <Trans>Failed to claim funds</Trans>
-              </div>
-              <ToastifyDebug error={event.data.error.message ?? "Unknown error"} />
-            </div>
-          );
-
-          return;
-        case TxnEventName.Sent:
-          helperToast.success(
-            <div className="flex flex-col gap-10">
-              <div className="text-body-medium font-bold">
-                <Trans>Funds claimed</Trans>
-              </div>
-              <Trans>
-                Claimed{" "}
-                {tokens.map((token) => `${claimableTokenTitles[token]} ${getToken(chainId, token).symbol}`).join(", ")}{" "}
-                successfully
-              </Trans>
-            </div>
-          );
-
-          return;
-      }
-    },
-  });
-}
-
-const useClaimExecutionFee = (
-  account: string | undefined,
-  claimableTokens: string[],
-  chainId: number,
-  claimTermsAcceptedSignature: string | undefined,
-  signer: WalletSigner | undefined
-) => {
-  const enabled = Boolean(claimTermsAcceptedSignature && signer && account && claimableTokens.length > 0);
-
-  return useSWR(enabled ? [account, claimableTokens, chainId, claimTermsAcceptedSignature, signer] : null, {
-    refreshInterval: 0,
-    fetcher: async () => {
-      const gasPrice = await getGasPrice(signer!.provider!, chainId);
-
-      const callData = getCallData(claimableTokens, account!, claimTermsAcceptedSignature!, GLP_DISTRIBUTION_ID);
-      const gasLimit = await estimateGasLimit(signer!.provider!, {
-        to: getContract(chainId, "ClaimHandler"),
-        data: callData,
-        from: account!,
-        value: undefined,
-      });
-
-      return gasLimit * ("gasPrice" in gasPrice ? gasPrice.gasPrice : gasPrice.maxFeePerGas);
-    },
-  });
-};
 
 export default function ClaimableAmounts() {
   const { account, signer } = useWallet();
@@ -153,7 +42,7 @@ export default function ClaimableAmounts() {
   const [isClaiming, setIsClaiming] = useState(false);
 
   const [claimTermsAcceptedSignature, setClaimTermsAcceptedSignature] = useLocalStorage(
-    `claimTermsAcceptedSignature:${account}:${GLP_DISTRIBUTION_ID}:${claimTerms}`,
+    getClaimTermsAcceptedKey(chainId, account, GLP_DISTRIBUTION_ID, claimTerms),
     ""
   );
 
@@ -163,13 +52,14 @@ export default function ClaimableAmounts() {
     );
   }, [claimableAmounts]);
 
-  const { data: executionFee } = useClaimExecutionFee(
+  const { data: executionFee } = useClaimExecutionFee({
     account,
     claimableTokens,
     chainId,
     claimTermsAcceptedSignature,
-    signer
-  );
+    signer,
+    distributionId: GLP_DISTRIBUTION_ID,
+  });
 
   const signClaimTerms = useCallback(async () => {
     const message = `${claimTerms}\ndistributionId ${GLP_DISTRIBUTION_ID}\ncontract ${getContract(chainId, "ClaimHandler").toLowerCase()}\nchainId ${chainId}`;
@@ -179,6 +69,12 @@ export default function ClaimableAmounts() {
       setClaimTermsAcceptedSignature(signature);
     }
   }, [setClaimTermsAcceptedSignature, signer, claimTerms, chainId]);
+
+  const claimFundsTransactionCallback = useClaimFundsTransactionCallback({
+    tokens: claimableTokens,
+    chainId,
+    claimableTokenTitles,
+  });
 
   const claimAmounts = useCallback(async () => {
     if (claimTerms && !claimTermsAcceptedSignature) {
@@ -199,6 +95,7 @@ export default function ClaimableAmounts() {
         signature: claimTermsAcceptedSignature ?? "",
         distributionId: GLP_DISTRIBUTION_ID,
         claimableTokenTitles,
+        callback: claimFundsTransactionCallback,
       });
       mutateClaimableAmounts(true);
       setClaimTermsAcceptedSignature("");
@@ -215,10 +112,11 @@ export default function ClaimableAmounts() {
     setClaimTermsAcceptedSignature,
     mutateClaimableAmounts,
     claimTerms,
+    claimFundsTransactionCallback,
   ]);
 
   const { balancesData } = useTokenBalances(chainId);
-  const userNativeTokenBalance = balancesData?.[NATIVE_TOKEN_ADDRESS];
+  const userNativeTokenBalance = getByKey(balancesData, NATIVE_TOKEN_ADDRESS);
 
   const { isButtonDisabled, buttonText, buttonTooltipText, hasAvailableFundsToCoverExecutionFee } = useMemo(() => {
     let isButtonDisabled = false;
---
+++ src/domain/synthetics/claims/useClaimFundsTransactionCallback.tsx
@@ -0,0 +1,65 @@
+import { Trans } from "@lingui/macro";
+import { useCallback } from "react";
+import { ImSpinner2 } from "react-icons/im";
+
+import { helperToast } from "lib/helperToast";
+import { TxnEvent, TxnEventName, WalletTxnCtx } from "lib/transactions";
+import { getToken } from "sdk/configs/tokens";
+
+import { ToastifyDebug } from "components/ToastifyDebug/ToastifyDebug";
+
+export function useClaimFundsTransactionCallback(data: {
+  tokens: string[];
+  chainId: number;
+  claimableTokenTitles: Record<string, string>;
+}) {
+  const { tokens, chainId, claimableTokenTitles } = data;
+
+  return useCallback(
+    async (event: TxnEvent<WalletTxnCtx>) => {
+      switch (event.event) {
+        case TxnEventName.Submitted:
+          helperToast.success(
+            <div className="flex flex-col gap-10">
+              <div className="text-body-medium font-bold">
+                <Trans>Processing your claim…</Trans>
+              </div>
+              <div className="flex flex-row gap-10 text-gray-200">
+                <Trans>This may take a few minutes.</Trans>
+                <ImSpinner2 width={60} height={60} className="spin size-15 text-white" />
+              </div>
+            </div>
+          );
+
+          return;
+        case TxnEventName.Error:
+          helperToast.error(
+            <div className="flex flex-col gap-10">
+              <div className="text-body-medium font-bold">
+                <Trans>Failed to claim funds</Trans>
+              </div>
+              <ToastifyDebug error={event.data.error.message ?? "Unknown error"} />
+            </div>
+          );
+
+          return;
+        case TxnEventName.Sent:
+          helperToast.success(
+            <div className="flex flex-col gap-10">
+              <div className="text-body-medium font-bold">
+                <Trans>Funds claimed</Trans>
+              </div>
+              <Trans>
+                Claimed{" "}
+                {tokens.map((token) => `${claimableTokenTitles[token]} ${getToken(chainId, token).symbol}`).join(", ")}{" "}
+                successfully
+              </Trans>
+            </div>
+          );
+
+          return;
+      }
+    },
+    [tokens, chainId, claimableTokenTitles]
+  );
+}
