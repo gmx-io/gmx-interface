4098a74c6f785c1debd6d13c56699f9b0dc0ceb3
M Dev <mdev_fe@pm.me>
2025-08-18 11:50:22 +0200

    feat: add hooks for pools data, total volume, and traders to enhance data retrieval and user metrics

---
+++ landing/src/hooks/useTotalVolume.ts
@@ -0,0 +1,39 @@
+import { gql } from "@apollo/client";
+import useSWR from "swr";
+
+import { getServerUrl } from "config/backend";
+import { getTotalVolumeSum } from "lib/legacy";
+import { getSyntheticsGraphClient } from "lib/subgraph";
+import { ARBITRUM, AVALANCHE } from "sdk/configs/chainIds";
+
+export function useTotalVolume() {
+  const clientArbitum = getSyntheticsGraphClient(ARBITRUM)!;
+  const clientAvalanche = getSyntheticsGraphClient(AVALANCHE)!;
+  return useSWR(["volumeInfos"], async () => {
+    const totalArbitumVolumeReq = fetchTotalVolumeByChainId(ARBITRUM);
+    const totalAvalancheVolumeReq = fetchTotalVolumeByChainId(AVALANCHE);
+    const query = {
+      query: gql`
+        query VolumeInfos {
+          volumeInfos(where: { period: "total" }) {
+            volumeUsd
+          }
+        }
+      `,
+    };
+    const syntheticsArbitumReq = clientArbitum.query(query);
+    const syntheticsAvalancheReq = clientAvalanche.query(query);
+    const [totalArbitumVolumeRes, totalAvalancheVolumeRes, syntheticsArbitumRes, syntheticsAvalancheRes] =
+      await Promise.all([totalArbitumVolumeReq, totalAvalancheVolumeReq, syntheticsArbitumReq, syntheticsAvalancheReq]);
+    const totalArbitumFromApi = getTotalVolumeSum(totalArbitumVolumeRes) ?? 0n;
+    const totalAvalancheFromApi = getTotalVolumeSum(totalAvalancheVolumeRes) ?? 0n;
+    const syntheticsArbitum = BigInt(syntheticsArbitumRes.data?.volumeInfos[0].volumeUsd ?? 0n);
+    const syntheticsAvalanche = BigInt(syntheticsAvalancheRes.data?.volumeInfos[0].volumeUsd ?? 0n);
+    return totalArbitumFromApi + totalAvalancheFromApi + syntheticsArbitum + syntheticsAvalanche;
+  });
+}
+
+function fetchTotalVolumeByChainId(chainId: number) {
+  const url = getServerUrl(chainId, "/total_volume");
+  return fetch(url).then((res) => res.json());
+}
