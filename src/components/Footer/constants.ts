6583f20dd0d17ce1009c14894bacfb3223e1e861
Divhead <110596516+divhead@users.noreply.github.com>
2025-09-04 05:19:57 -0700

    remove anouncements and footer links

--- src/components/Footer/constants.ts
+++ src/components/Footer/constants.ts
@@ -1,5 +1,3 @@
-import { t } from "@lingui/macro";
-
 import discordIcon from "img/ic_discord.svg";
 import githubIcon from "img/ic_github.svg";
 import substackIcon from "img/ic_substack.svg";
@@ -22,14 +20,14 @@ type SocialLink = {
 export function getFooterLinks(isHome) {
   const FOOTER_LINKS: { home: Link[]; app: Link[] } = {
     home: [
-      { label: t`Terms and Conditions`, link: "/terms-and-conditions" },
-      { label: t`Referral Terms`, link: "/referral-terms" },
-      { label: t`Media Kit`, link: "https://docs.gmx.io/docs/community/media-kit", external: true },
+      // { label: t`Terms and Conditions`, link: "/terms-and-conditions" },
+      // { label: t`Referral Terms`, link: "/referral-terms" },
+      // { label: t`Media Kit`, link: "https://docs.gmx.io/docs/community/media-kit", external: true },
       // { label: "Jobs", link: "/jobs", isAppLink: true },
     ],
     app: [
-      { label: t`Media Kit`, link: "https://docs.gmx.io/docs/community/media-kit", external: true },
-      { label: t`Charts by TradingView`, link: "https://www.tradingview.com/", external: true },
+      // { label: t`Media Kit`, link: "https://docs.gmx.io/docs/community/media-kit", external: true },
+      // { label: t`Charts by TradingView`, link: "https://www.tradingview.com/", external: true },
       // { label: "Jobs", link: "/jobs" },
     ],
   };
