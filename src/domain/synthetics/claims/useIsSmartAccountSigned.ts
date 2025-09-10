dca82f58419e4d798f64d9ed3f6c0103dff76f83
Hubért de Lalye <hubert-de-lalye@proton.me>
2025-09-10 17:08:08 +0400

    fixed in PR

--- src/domain/synthetics/claims/useIsSmartAccountSigned.ts
+++ src/domain/synthetics/claims/useIsSmartAccountSigned.ts
@@ -1,71 +1,41 @@
-import { Contract } from "ethers";
-import { useEffect, useState } from "react";
+import useSWR from "swr";
+import type { Address } from "viem";
+import { usePublicClient } from "wagmi";
 
 import { WalletSigner } from "lib/wallets";
+import { abis } from "sdk/abis";
 
-const CLAIM_TERMS_HASH = "0x09335c037e73849fe301478f91674da05757165154d1dccb1b881c1846938f3d";
+const CLAIM_TERMS_HASH = "0x09335c037e73849fe301478f91674da05757165154d1dccb1b881c1846938f3dp";
+/**
+ * keccak256("isValidSignature(bytes32,bytes)")
+ * @see https://eips.ethereum.org/EIPS/eip-1271
+ */
 const VALID_SIGNATURE_RESPONSE = "0x1626ba7e";
-const abi = [
-  {
-    name: "isValidSignature",
-    inputs: [
-      {
-        internalType: "bytes32",
-        name: "_hash",
-        type: "bytes32",
-      },
-      {
-        internalType: "bytes",
-        name: "_signature",
-        type: "bytes",
-      },
-    ],
-    outputs: [
-      {
-        internalType: "bytes4",
-        name: "magicValue",
-        type: "bytes4",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-];
 
-export const useIsSmartAccountSignedClaimTerms = ({ account, signer }: { account?: string; signer?: WalletSigner }) => {
-  const [isValidSignature, setIsValidSignature] = useState(false);
+export const useIsSmartAccountSignedClaimTerms = ({
+  account,
+  signer: _signer,
+}: {
+  account?: string;
+  signer?: WalletSigner;
+}) => {
+  const publicClient = usePublicClient();
 
-  useEffect(() => {
-    const check = async () => {
-      if (!account) {
+  const { data } = useSWR(account && publicClient && [account, publicClient, "isValidSmartAccountSignature"], {
+    fetcher: async () => {
+      if (!account || !publicClient) {
         return;
       }
+      const response = await publicClient.readContract({
+        address: account as Address,
+        abi: abis.SmartAccount,
+        functionName: "isValidSignature",
+        args: [CLAIM_TERMS_HASH, "0x"],
+      });
 
-      if (!signer) {
-        return;
-      }
+      return response === VALID_SIGNATURE_RESPONSE;
+    },
+  });
 
-      const contract = new Contract(account, abi, signer);
-
-      try {
-        const response = await contract.isValidSignature(CLAIM_TERMS_HASH, "0x");
-        const isValid = response === VALID_SIGNATURE_RESPONSE;
-        setIsValidSignature(isValid);
-      } catch (error) {
-        setIsValidSignature(false);
-      }
-    };
-
-    if (!account) {
-      return;
-    }
-
-    if (!signer) {
-      return;
-    }
-
-    check();
-  }, [account, signer]);
-
-  return isValidSignature;
+  return data;
 };
