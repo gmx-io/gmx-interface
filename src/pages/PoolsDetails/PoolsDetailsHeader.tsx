b8f72338d90c8b30fe268bf327ce433ef5fb9faf
midas-myth <midas-myth@proton.me>
2025-09-15 21:45:41 +0200

    Multichain change pools header

--- src/pages/PoolsDetails/PoolsDetailsHeader.tsx
+++ src/pages/PoolsDetails/PoolsDetailsHeader.tsx
@@ -20,7 +20,7 @@ import { convertToUsd, TokenData } from "domain/synthetics/tokens";
 import { useChainId } from "lib/chains";
 import { formatAmountHuman, formatBalanceAmount, formatUsd } from "lib/numbers";
 import { getByKey } from "lib/objects";
-import { usePoolsIsMobilePage } from "pages/Pools/usePoolsIsMobilePage";
+import { useBreakpoints } from "lib/useBreakpoints";
 import { AnyChainId, getChainName } from "sdk/configs/chains";
 import { getNormalizedTokenSymbol } from "sdk/configs/tokens";
 
@@ -28,6 +28,9 @@ import Button from "components/Button/Button";
 import { useMultichainMarketTokenBalancesRequest } from "components/Synthetics/GmxAccountModal/hooks";
 import { SyntheticsInfoRow } from "components/Synthetics/SyntheticsInfoRow";
 import TokenIcon from "components/TokenIcon/TokenIcon";
+
+import Buy16Icon from "img/ic_buy_16.svg?react";
+import Sell16Icon from "img/ic_sell_16.svg?react";
 
 import { PoolsDetailsMarketAmount } from "./PoolsDetailsMarketAmount";
 
@@ -55,7 +58,7 @@ export function PoolsDetailsHeader({ glvOrMarketInfo, marketToken }: Props) {
   const userEarnings = useUserEarnings(chainId, srcChainId);
   const marketEarnings = getByKey(userEarnings?.byMarketAddress, marketToken?.address);
 
-  const isMobile = usePoolsIsMobilePage();
+  const { isMobile, isTablet } = useBreakpoints();
 
   const { totalBalance, tokenBalancesData, isBalanceDataLoading } = useMultichainMarketTokenBalancesRequest(
     chainId,
@@ -86,96 +89,116 @@ export function PoolsDetailsHeader({ glvOrMarketInfo, marketToken }: Props) {
     setIsOpen((isOpen) => !isOpen);
   }, []);
 
+  const glOrGm = isGlv ? "GLV" : "GM";
+
   return (
     <div
-      className={cx("flex rounded-8 bg-slate-900 px-16 py-20 max-md:px-12 max-md:py-12", {
-        "flex-col gap-18": isMobile,
-        "items-center gap-28": !isMobile,
-      })}
-    >
-      {glvOrMarketInfo ? (
-        <>
-          <div
-            className={cx("flex items-center justify-between ", {
-              "border-b-1/2 border-slate-600 pb-18": isOpen && isMobile,
-            })}
-          >
-            <div className="flex items-center gap-20">
-              {iconName ? (
-                <TokenIcon
-                  symbol={iconName}
-                  displaySize={40}
-                  importSize={40}
-                  badge={
-                    isGlv
-                      ? getGlvMarketShortening(chainId, getGlvOrMarketAddress(glvOrMarketInfo))
-                      : ([glvOrMarketInfo.longToken.symbol, glvOrMarketInfo.shortToken.symbol] as const)
-                  }
-                />
-              ) : null}
-              <div className={cx("flex flex-col gap-4 pr-20 font-medium")}>
-                <div className="text-body-large">{isGlv ? "GLV" : `GM: ${getMarketIndexName(glvOrMarketInfo)}`}</div>
-                <div className="text-body-small text-typography-secondary">{`[${getMarketPoolName(glvOrMarketInfo)}]`}</div>
-              </div>
-            </div>
-            {isMobile ? (
-              <Button className="flex h-32 w-32 items-center justify-center" variant="secondary" onClick={handleToggle}>
-                <FaChevronDown className={cx({ "rotate-180": isOpen })} />
-              </Button>
-            ) : null}
-          </div>
-          {!isOpen && isMobile ? null : (
-            <div className="flex gap-14 max-md:flex-col">
-              <PoolsDetailsMarketAmount
-                label={<Trans>TVL (Supply)</Trans>}
-                value={formatAmountHuman(marketTotalSupplyUsd, USD_DECIMALS, true, 2)}
-                secondaryValue={
-                  typeof marketTotalSupply === "bigint" && typeof marketToken?.decimals === "number"
-                    ? `${formatAmountHuman(marketTotalSupply, marketToken?.decimals, false, 2)} ${isGlv ? "GLV" : "GM"}`
-                    : undefined
-                }
-              />
-
-              {typeof totalBalance === "bigint" && typeof marketToken?.decimals === "number" ? (
-                <PoolsDetailsMarketAmount
-                  label={<Trans>Wallet</Trans>}
-                  value={formatUsd(marketBalanceUsd)}
-                  secondaryValue={`${formatBalanceAmount(totalBalance, marketToken?.decimals, undefined, {
-                    showZero: true,
-                  })} ${isGlv ? "GLV" : "GM"}`}
-                  tooltipContent={
-                    <div>
-                      {sortedTokenBalancesDataArray.map(({ chainId, balance }) => {
-                        const chainName = chainId === 0 ? t`GMX Account` : getChainName(chainId);
-
-                        return (
-                          <SyntheticsInfoRow
-                            key={chainId}
-                            label={chainName}
-                            value={formatBalanceAmount(balance, marketToken?.decimals, undefined, {
-                              showZero: true,
-                            })}
-                          />
-                        );
-                      })}
-                      {isBalanceDataLoading ? <ImSpinner2 className="animate-spin" /> : null}
-                    </div>
-                  }
-                />
-              ) : null}
-
-              {marketEarnings ? (
-                <PoolsDetailsMarketAmount
-                  label={<Trans>Total Earned Fees</Trans>}
-                  value={formatUsd(marketEarnings?.total)}
-                />
-              ) : null}
-            </div>
-          )}
-        </>
-      ) : (
-        <div>...</div>
+      className={cx(
+        "flex justify-between gap-18 rounded-8 bg-slate-900 px-16 py-20 max-lg:flex-col max-md:px-12 max-md:py-12"
       )}
+    >
+      <div
+        className={cx("flex", {
+          "flex-col gap-18": isMobile,
+          "items-center gap-28": !isMobile,
+        })}
+      >
+        {!glvOrMarketInfo ? (
+          <div>...</div>
+        ) : (
+          <>
+            <div
+              className={cx("flex items-center justify-between ", {
+                "border-b-1/2 border-slate-600 pb-18": isOpen && isMobile,
+              })}
+            >
+              <div className="flex items-center gap-20">
+                {iconName && (
+                  <TokenIcon
+                    symbol={iconName}
+                    displaySize={40}
+                    importSize={40}
+                    badge={
+                      isGlv
+                        ? getGlvMarketShortening(chainId, getGlvOrMarketAddress(glvOrMarketInfo))
+                        : ([glvOrMarketInfo.longToken.symbol, glvOrMarketInfo.shortToken.symbol] as const)
+                    }
+                  />
+                )}
+                <div className={cx("flex flex-col gap-4 pr-20 font-medium")}>
+                  <div className="text-body-large">{isGlv ? "GLV" : `GM: ${getMarketIndexName(glvOrMarketInfo)}`}</div>
+                  <div className="text-body-small text-typography-secondary">{`[${getMarketPoolName(glvOrMarketInfo)}]`}</div>
+                </div>
+              </div>
+              {isMobile && (
+                <Button
+                  className="flex h-32 w-32 items-center justify-center"
+                  variant="secondary"
+                  onClick={handleToggle}
+                >
+                  <FaChevronDown className={cx({ "rotate-180": isOpen })} />
+                </Button>
+              )}
+            </div>
+            {(isOpen || !isMobile) && (
+              <div className="flex gap-14 max-md:flex-col">
+                <PoolsDetailsMarketAmount
+                  label={<Trans>TVL (Supply)</Trans>}
+                  value={formatAmountHuman(marketTotalSupplyUsd, USD_DECIMALS, true, 2)}
+                  secondaryValue={
+                    typeof marketTotalSupply === "bigint" && typeof marketToken?.decimals === "number"
+                      ? `${formatAmountHuman(marketTotalSupply, marketToken?.decimals, false, 2)} ${isGlv ? "GLV" : "GM"}`
+                      : undefined
+                  }
+                />
+                {typeof totalBalance === "bigint" && typeof marketToken?.decimals === "number" && (
+                  <PoolsDetailsMarketAmount
+                    label={<Trans>Balance</Trans>}
+                    value={formatUsd(marketBalanceUsd)}
+                    secondaryValue={`${formatBalanceAmount(totalBalance, marketToken?.decimals, undefined, {
+                      showZero: true,
+                    })} ${isGlv ? "GLV" : "GM"}`}
+                    tooltipContent={
+                      <div>
+                        {sortedTokenBalancesDataArray.map(({ chainId, balance }) => {
+                          const chainName = chainId === 0 ? t`GMX Account` : getChainName(chainId);
+
+                          return (
+                            <SyntheticsInfoRow
+                              key={chainId}
+                              label={chainName}
+                              value={formatBalanceAmount(balance, marketToken?.decimals, undefined, {
+                                showZero: true,
+                              })}
+                            />
+                          );
+                        })}
+                        {isBalanceDataLoading && <ImSpinner2 className="animate-spin" />}
+                      </div>
+                    }
+                  />
+                )}
+                {marketEarnings && (
+                  <PoolsDetailsMarketAmount
+                    label={<Trans>Total Earned Fees</Trans>}
+                    value={formatUsd(marketEarnings?.total)}
+                  />
+                )}
+              </div>
+            )}
+          </>
+        )}
+      </div>
+      <div className={cx("flex items-center gap-8 max-lg:w-full lg:gap-12")}>
+        <Button className="min-w-max basis-full" variant="secondary" size={isTablet ? "small" : "medium"}>
+          {isTablet && <Sell16Icon className="size-16" />}
+          <Trans>Withdraw {glOrGm}</Trans>
+        </Button>
+        <Button className="min-w-max basis-full" variant="secondary" size={isTablet ? "small" : "medium"}>
+          {isTablet && <Buy16Icon className="size-16" />}
+          <Trans>Deposit {glOrGm}</Trans>
+        </Button>
+      </div>
     </div>
   );
 }
