c0bd8a1d99b3c32534845afcfd38839c5dbc6f95
qwinndev <qwinndev@proton.me>
2025-09-15 09:02:54 +0200

    Update snapshot

--- src/components/Synthetics/TradeHistory/TradeHistoryRow/utils.spec.ts
+++ src/components/Synthetics/TradeHistory/TradeHistoryRow/utils.spec.ts
@@ -190,44 +190,44 @@ describe("TradeHistoryRow helpers", () => {
 
     expect(formatPositionMessage(executeOrderIncreaseLong, minCollateralUsd)).toMatchInlineSnapshot(`
       {
-        "acceptablePrice": ">  $ 0.82764",
+        "acceptablePrice": ">  $ ‍0.82764",
         "action": "Execute Limit",
-        "executionPrice": "$ 0.83711",
+        "executionPrice": "$ ‍0.83711",
         "fullMarket": "ARB/USD [ARB-USDC]",
         "indexName": "ARB/USD",
         "indexTokenSymbol": "ARB",
         "isLong": false,
         "market": "Short ARB/USD",
-        "marketPrice": "< $ 0.00001",
+        "marketPrice": "< $ ‍0.00001",
         "poolName": "ARB-USDC",
-        "price": "< $ 0.00001",
+        "price": "< $ ‍0.00001",
         "priceComment": [
           "Mark price for the order.",
           "",
           {
             "key": "Order Trigger Price",
-            "value": ">  $ 0.83600",
+            "value": ">  $ ‍0.83600",
           },
           {
             "key": "Order Acceptable Price",
-            "value": ">  $ 0.82764",
+            "value": ">  $ ‍0.82764",
           },
           {
             "key": "Order Execution Price",
-            "value": "$ 0.83711",
+            "value": "$ ‍0.83711",
           },
           {
             "key": "Price Impact",
             "value": {
               "state": "error",
-              "text": "-$ 16.82",
+              "text": "-$ ‍16.82",
             },
           },
           "",
           "Order execution price takes into account price impact.",
         ],
-        "priceImpact": "-$ 16.82",
-        "size": "+$ 2,070.19",
+        "priceImpact": "-$ ‍16.82",
+        "size": "+$ ‍2,070.19",
         "timestamp": "18 Sep 2023, 16:43",
         "timestampISO": "2023-09-18T16:43:18+04:00",
       }
@@ -235,7 +235,7 @@ describe("TradeHistoryRow helpers", () => {
 
     expect(formatPositionMessage(frozenOrderIncreaseShort, minCollateralUsd)).toMatchInlineSnapshot(`
       {
-        "acceptablePrice": ">  $ 26,937.90",
+        "acceptablePrice": ">  $ ‍26,937.90",
         "action": "Failed Limit",
         "actionComment": undefined,
         "executionPrice": undefined,
@@ -253,16 +253,16 @@ describe("TradeHistoryRow helpers", () => {
           "",
           {
             "key": "Order Trigger Price",
-            "value": ">  $ 27,210.00",
+            "value": ">  $ ‍27,210.00",
           },
           {
             "key": "Order Acceptable Price",
-            "value": ">  $ 26,937.90",
+            "value": ">  $ ‍26,937.90",
           },
           undefined,
         ],
-        "priceImpact": "-$ 9,488.99",
-        "size": "+$ 1,348.83",
+        "priceImpact": "-$ ‍9,488.99",
+        "size": "+$ ‍1,348.83",
         "timestamp": "18 Sep 2023, 15:14",
         "timestampISO": "2023-09-18T15:14:09+04:00",
       }
@@ -290,18 +290,18 @@ describe("TradeHistoryRow helpers", () => {
     expect(formatPositionMessage(liquidated, minCollateralUsd)).toMatchInlineSnapshot(`
       {
         "action": "Liquidated",
-        "executionPrice": "$ 6.1063",
+        "executionPrice": "$ ‍6.1063",
         "fullMarket": "LINK/USD [LINK-USDC]",
         "indexName": "LINK/USD",
         "indexTokenSymbol": "LINK",
         "isActionError": true,
         "isLong": false,
         "market": "Short LINK/USD",
-        "marketPrice": "$ 6.0907",
-        "pnl": "-$ 126.32",
+        "marketPrice": "$ ‍6.0907",
+        "pnl": "-$ ‍126.32",
         "pnlState": "error",
         "poolName": "LINK-USDC",
-        "price": "$ 6.0907",
+        "price": "$ ‍6.0907",
         "priceComment": [
           "Mark price for the liquidation.",
           "",
@@ -309,75 +309,75 @@ describe("TradeHistoryRow helpers", () => {
           "",
           {
             "key": "Order Execution Price",
-            "value": "$ 6.1063",
+            "value": "$ ‍6.1063",
           },
           "",
           "Order execution price takes into account price impact.",
           "",
           {
             "key": "Initial Collateral",
-            "value": "214.78 USDC ($ 214.78)",
+            "value": "214.78 USDC ($ ‍214.78)",
           },
           {
             "key": "PnL",
             "value": {
               "state": "error",
-              "text": "-$ 126.32",
+              "text": "-$ ‍126.32",
             },
           },
           {
             "key": "Borrow Fee",
             "value": {
               "state": "error",
-              "text": "$ 0.00",
+              "text": "$ ‍0.00",
             },
           },
           {
             "key": "Funding Fee",
             "value": {
               "state": "error",
-              "text": "$ 0.00",
+              "text": "$ ‍0.00",
             },
           },
           {
             "key": "Close Fee",
             "value": {
               "state": "error",
-              "text": "-$ 4.51",
+              "text": "-$ ‍4.51",
             },
           },
           "",
           {
             "key": "Min. Required Collateral",
-            "value": "< $ 0.01",
+            "value": "< $ ‍0.01",
           },
           {
             "key": "Collateral at Liquidation",
-            "value": "$ 83.95",
+            "value": "$ ‍83.95",
           },
           "",
           {
             "key": "Price Impact",
             "value": {
               "state": "error",
-              "text": "-$ 16.82",
+              "text": "-$ ‍16.82",
             },
           },
           {
             "key": "Liquidation Fee",
             "value": {
               "state": "error",
-              "text": "-$ 1.05",
+              "text": "-$ ‍1.05",
             },
           },
           "",
           {
             "key": "Returned Collateral",
-            "value": "$ 66.08",
+            "value": "$ ‍66.08",
           },
         ],
         "priceImpact": undefined,
-        "size": "-$ 6,441.90",
+        "size": "-$ ‍6,441.90",
         "timestamp": "04 Sep 2023, 06:38",
         "timestampISO": "2023-09-04T06:38:49+04:00",
       }
