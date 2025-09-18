458053bf1aea0a2a986743f02143c13edd1e34bf
Divhead <110596516+divhead@users.noreply.github.com>
2025-09-18 05:55:13 -0700

    remove unused contracts

--- sdk/src/abis/index.ts
+++ sdk/src/abis/index.ts
@@ -12,7 +12,6 @@ import GelatoRelayRouter from "./GelatoRelayRouter.json";
 import GlpManager from "./GlpManager.json";
 import GlvReader from "./GlvReader.json";
 import GlvRouter from "./GlvRouter.json";
-import GMT from "./GMT.json";
 import GmxMigrator from "./GmxMigrator.json";
 import GovToken from "./GovToken.json";
 import LayerZeroProvider from "./LayerZeroProvider.json";
@@ -26,20 +25,11 @@ import MultichainSubaccountRouter from "./MultichainSubaccountRouter.json";
 import MultichainTransferRouter from "./MultichainTransferRouter.json";
 import MultichainUtils from "./MultichainUtils.json";
 import MultichainVault from "./MultichainVault.json";
-import OrderBook from "./OrderBook.json";
-import OrderBookReader from "./OrderBookReader.json";
-import OrderExecutor from "./OrderExecutor.json";
-import PositionManager from "./PositionManager.json";
-import PositionRouter from "./PositionRouter.json";
-import Reader from "./Reader.json";
-import ReaderV2 from "./ReaderV2.json";
 import ReferralStorage from "./ReferralStorage.json";
 import RelayParams from "./RelayParams.json";
 import RewardReader from "./RewardReader.json";
 import RewardRouter from "./RewardRouter.json";
 import RewardTracker from "./RewardTracker.json";
-import RouterV2 from "./Router-v2.json";
-import Router from "./Router.json";
 import SmartAccount from "./SmartAccount.json";
 import StBTC from "./StBTC.json";
 import SubaccountGelatoRelayRouter from "./SubaccountGelatoRelayRouter.json";
@@ -57,8 +47,6 @@ import VaultV2 from "./VaultV2.json";
 import VaultV2b from "./VaultV2b.json";
 import Vester from "./Vester.json";
 import WETH from "./WETH.json";
-import YieldFarm from "./YieldFarm.json";
-import YieldToken from "./YieldToken.json";
 
 export type AbiId =
   | "AbstractSubaccountApprovalNonceable"
@@ -77,7 +65,6 @@ export type AbiId =
   | "GlpManager"
   | "GlvReader"
   | "GlvRouter"
-  | "GMT"
   | "GmxMigrator"
   | "GovToken"
   | "LayerZeroProvider"
@@ -91,20 +78,11 @@ export type AbiId =
   | "MultichainTransferRouter"
   | "MultichainUtils"
   | "MultichainVault"
-  | "OrderBook"
-  | "OrderBookReader"
-  | "OrderExecutor"
-  | "PositionManager"
-  | "PositionRouter"
-  | "Reader"
-  | "ReaderV2"
   | "ReferralStorage"
   | "RelayParams"
   | "RewardReader"
   | "RewardRouter"
   | "RewardTracker"
-  | "Router"
-  | "RouterV2"
   | "SmartAccount"
   | "StBTC"
   | "SubaccountGelatoRelayRouter"
@@ -122,9 +100,7 @@ export type AbiId =
   | "VaultV2"
   | "VaultV2b"
   | "Vester"
-  | "WETH"
-  | "YieldFarm"
-  | "YieldToken";
+  | "WETH";
 
 /** Copied from ethers to enable compatibility with GMX UI */
 interface JsonFragmentType {
@@ -184,7 +160,6 @@ export const abis: Record<AbiId, readonly (Abi[number] & JsonFragment)[]> = {
   GlpManager: GlpManager.abi,
   GlvReader: GlvReader.abi,
   GlvRouter: GlvRouter.abi,
-  GMT: GMT.abi,
   GmxMigrator: GmxMigrator.abi,
   GovToken: GovToken.abi,
   LayerZeroProvider: LayerZeroProvider.abi,
@@ -198,20 +173,11 @@ export const abis: Record<AbiId, readonly (Abi[number] & JsonFragment)[]> = {
   MultichainTransferRouter: MultichainTransferRouter.abi,
   MultichainUtils: MultichainUtils.abi,
   MultichainVault: MultichainVault.abi,
-  OrderBook: OrderBook.abi,
-  OrderBookReader: OrderBookReader.abi,
-  OrderExecutor: OrderExecutor.abi,
-  PositionManager: PositionManager.abi,
-  PositionRouter: PositionRouter.abi,
-  Reader: Reader.abi,
-  ReaderV2: ReaderV2.abi,
   ReferralStorage: ReferralStorage.abi,
   RelayParams: RelayParams.abi,
   RewardReader: RewardReader.abi,
   RewardRouter: RewardRouter.abi,
   RewardTracker: RewardTracker.abi,
-  Router: Router.abi,
-  RouterV2: RouterV2.abi,
   SmartAccount: SmartAccount.abi,
   StBTC: StBTC.abi,
   SubaccountGelatoRelayRouter: SubaccountGelatoRelayRouter.abi,
@@ -229,6 +195,4 @@ export const abis: Record<AbiId, readonly (Abi[number] & JsonFragment)[]> = {
   VaultV2b: VaultV2b.abi,
   Vester: Vester.abi,
   WETH: WETH.abi,
-  YieldFarm: YieldFarm.abi,
-  YieldToken: YieldToken.abi,
 } satisfies Record<AbiId, any> as any;
