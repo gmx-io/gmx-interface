4506706075c441a285407ff6b8ce3d72d5c758f3
qwinndev <qwinndev@proton.me>
2025-09-23 15:09:15 +0200

    Add discover block bg

--- src/pages/Earn/EarnDiscovery/EarnDiscovery.tsx
+++ src/pages/Earn/EarnDiscovery/EarnDiscovery.tsx
@@ -1,21 +1,43 @@
 import { Trans } from "@lingui/macro";
 
+import { useBreakpoints } from "lib/useBreakpoints";
+
+import discoverBg from "img/discover_bg.png";
+import discoverBgMobile from "img/discover_bg_mobile.png";
+
 import EarnPageLayout from "../EarnPageLayout";
 import EarnDocumentation from "./components/EarnDocumentation";
 import EarnFaq from "./components/EarnFaq";
 import EarnProductCard from "./components/EarnProductCard";
 import EarnYieldOverview from "./components/EarnYieldOverview";
 
+const DISCOVER_STYLE = {
+  backgroundImage: `url(${discoverBg})`,
+  backgroundSize: "100% 100%",
+  backgroundRepeat: "no-repeat",
+};
+
+const DISCOVER_MOBILE_STYLE = {
+  backgroundImage: `url(${discoverBgMobile})`,
+  backgroundSize: "100% 100%",
+  backgroundRepeat: "no-repeat",
+};
+
 export default function EarnDiscoveryPage() {
+  const { isDesktop } = useBreakpoints();
+
   return (
     <EarnPageLayout>
       <div className="flex gap-8 max-md:flex-col">
         <div className="flex grow flex-col gap-8">
-          <div className="flex flex-col gap-8 rounded-8 bg-slate-900 p-20">
+          <div
+            className="flex flex-col gap-8 rounded-8 bg-slate-900 p-20"
+            style={isDesktop ? DISCOVER_MOBILE_STYLE : DISCOVER_STYLE}
+          >
             <h4 className="text-16 font-medium text-typography-primary">
               <Trans>Discover GMX Earn</Trans>
             </h4>
-            <p className="text-13 text-typography-secondary">
+            <p className="max-w-[600px] text-13 text-typography-secondary">
               <Trans>
                 Welcome to GMX Earn. Learn how GMX, GLV, and GM work together to power a decentralized and efficient
                 trading experience. Start your journey by understanding how each token fits into the bigger picture —
