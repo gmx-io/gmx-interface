84201767ca547b5b71c9343914dae287ce5589c9
qwinndev <qwinndev@proton.me>
2025-10-08 15:12:58 +0200

    Update stats with new treasury value

--- src/pages/Dashboard/StatsCard.tsx
+++ src/pages/Dashboard/StatsCard.tsx
@@ -1,5 +1,5 @@
 import { Trans } from "@lingui/macro";
-import { useEffect, useMemo } from "react";
+import { useMemo } from "react";
 
 import { ARBITRUM, AVALANCHE, BOTANIX } from "config/chains";
 import { USD_DECIMALS } from "config/factors";
@@ -7,34 +7,15 @@ import { useTotalVolume, useV1FeesInfo } from "domain/stats";
 import { useTreasuryAllChains } from "domain/stats/treasury/useTreasuryAllChains";
 import useUniqueUsers from "domain/stats/useUniqueUsers";
 import useV2Stats from "domain/synthetics/stats/useV2Stats";
-import { useInfoTokens } from "domain/tokens";
-import { useChainId } from "lib/chains";
-import { GLP_DECIMALS } from "lib/legacy";
-import { expandDecimals, formatAmountHuman, formatUsd } from "lib/numbers";
+import { formatAmountHuman } from "lib/numbers";
 import { sumBigInts } from "lib/sumBigInts";
-import useWallet from "lib/wallets/useWallet";
-import { getTokenBySymbol } from "sdk/configs/tokens";
-import { bigMath } from "sdk/utils/bigmath";
 
 import { AppCard, AppCardSection } from "components/AppCard/AppCard";
 import ChainsStatsTooltipRow from "components/StatsTooltip/ChainsStatsTooltipRow";
 import TooltipComponent from "components/Tooltip/Tooltip";
+import TooltipWithPortal from "components/Tooltip/TooltipWithPortal";
 
-import type { ChainStats } from "./useDashboardChainStatsMulticall";
-
-const ethTreasuryFund = expandDecimals(350 + 148 + 384, 18);
-const glpTreasuryFund = expandDecimals(660001, 18);
-const usdcTreasuryFund = expandDecimals(784598 + 200000, 30);
-
-export function StatsCard({
-  statsArbitrum,
-  statsAvalanche,
-}: {
-  statsArbitrum?: ChainStats;
-  statsAvalanche?: ChainStats;
-}) {
-  const { active } = useWallet();
-  const { chainId } = useChainId();
+export function StatsCard() {
   const v1TotalVolume = useTotalVolume();
 
   const v2ArbitrumOverview = useV2Stats(ARBITRUM);
@@ -42,14 +23,6 @@ export function StatsCard({
   const v2BotanixOverview = useV2Stats(BOTANIX);
 
   const uniqueUsers = useUniqueUsers();
-
-  const suppliesArbitrum = statsArbitrum?.reader.tokenBalancesWithSupplies;
-  const suppliesAvalanche = statsAvalanche?.reader.tokenBalancesWithSupplies;
-
-  const { infoTokens: infoTokensArbitrum } = useInfoTokens(undefined, ARBITRUM, active, undefined, undefined);
-  const { infoTokens: infoTokensAvax } = useInfoTokens(undefined, AVALANCHE, active, undefined, undefined);
-  const infoTokens = chainId === ARBITRUM ? infoTokensArbitrum : infoTokensAvax;
-  const eth = infoTokens[getTokenBySymbol(chainId === ARBITRUM ? ARBITRUM : AVALANCHE, "ETH").address];
 
   const v1ArbitrumTotalFees = useV1FeesInfo(ARBITRUM);
   const v1AvalancheTotalFees = useV1FeesInfo(AVALANCHE);
@@ -63,44 +36,19 @@ export function StatsCard({
 
   // #endregion Fees
 
-  // #region Treasury
-  const glpTvlArbitrum = statsArbitrum?.glp.aum;
-  const glpTvlAvalanche = statsAvalanche?.glp.aum;
-
-  const glpTvl = chainId === ARBITRUM ? glpTvlArbitrum : glpTvlAvalanche;
-
-  const glpSupplyArbitrum = suppliesArbitrum?.glpSupply;
-  const glpSupplyAvalanche = suppliesAvalanche?.glpSupply;
-
-  const glpSupply = chainId === ARBITRUM ? glpSupplyArbitrum : glpSupplyAvalanche;
-
-  const glpPrice =
-    glpTvl !== undefined && glpTvl > 0n && glpSupply !== undefined
-      ? bigMath.mulDiv(glpTvl, expandDecimals(1, GLP_DECIMALS), glpSupply)
-      : expandDecimals(1, USD_DECIMALS);
-
-  let totalTreasuryFundUsd: bigint | undefined = undefined;
-
-  if (eth && eth.contractMinPrice !== undefined && glpPrice !== undefined) {
-    const ethTreasuryFundUsd = bigMath.mulDiv(ethTreasuryFund, eth.contractMinPrice, expandDecimals(1, eth.decimals));
-
-    const glpTreasuryFundUsd = bigMath.mulDiv(glpTreasuryFund, glpPrice, expandDecimals(1, 18));
-
-    totalTreasuryFundUsd = ethTreasuryFundUsd + glpTreasuryFundUsd + usdcTreasuryFund;
-  }
-
   const treasuryData = useTreasuryAllChains();
 
-  useEffect(() => {
-    // eslint-disable-next-line no-console
-    console.log(
-      "treasuryData",
-      formatUsd(treasuryData?.totalUsd, { maxThreshold: "9999999999999999999999999" }),
-      treasuryData
-    );
+  const treasuryWithoutGmxUsd = useMemo(() => {
+    return treasuryData?.tokens
+      .filter((token) => token.token?.symbol !== "GMX")
+      .reduce((acc, token) => acc + token.usdValue, 0n);
   }, [treasuryData]);
 
-  // #endregion Treasury
+  const gmxInTreasuryUsd = useMemo(() => {
+    return treasuryData?.tokens
+      .filter((token) => token.token?.symbol === "GMX")
+      .reduce((acc, token) => acc + token.usdValue, 0n);
+  }, [treasuryData]);
 
   const totalFeesEntries = useMemo(
     () => ({
@@ -220,7 +168,16 @@ export function StatsCard({
             <Trans>Treasury</Trans>
           </div>
           <div>
-            <span className="numbers">{formatAmountHuman(totalTreasuryFundUsd, USD_DECIMALS, true, 2)}</span>
+            <TooltipWithPortal
+              handle={formatAmountHuman(treasuryData?.totalUsd, USD_DECIMALS, true, 2)}
+              handleClassName="numbers"
+              content={
+                <div>
+                  <div>In other tokens: {formatAmountHuman(treasuryWithoutGmxUsd, USD_DECIMALS, true, 2)}</div>
+                  <div>In GMX: {formatAmountHuman(gmxInTreasuryUsd, USD_DECIMALS, true, 2)}</div>
+                </div>
+              }
+            />
           </div>
         </div>
       </AppCardSection>
