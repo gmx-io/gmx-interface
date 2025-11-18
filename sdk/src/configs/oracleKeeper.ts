27803940b18d8b7c24f54ae8e7f788df02ca56c5
Divhead <110596516+divhead@users.noreply.github.com>
2025-11-18 04:11:28 -0800

    fallback by default

--- sdk/src/configs/oracleKeeper.ts
+++ sdk/src/configs/oracleKeeper.ts
@@ -1,22 +1,19 @@
 import { ARBITRUM, ARBITRUM_SEPOLIA, AVALANCHE, AVALANCHE_FUJI, BOTANIX, ContractsChainId } from "./chains";
 
 const ORACLE_KEEPER_URLS: Record<ContractsChainId, string> = {
-  [ARBITRUM]: "https://arbitrum-api.gmxinfra.io",
+  [ARBITRUM]: "https://arbitrum-api.gmxinfra2.io",
 
-  [AVALANCHE]: "https://avalanche-api.gmxinfra.io",
+  [AVALANCHE]: "https://avalanche-api.gmxinfra2.io",
 
   [AVALANCHE_FUJI]: "https://synthetics-api-avax-fuji-upovm.ondigitalocean.app",
 
-  [BOTANIX]: "https://botanix-api.gmxinfra.io",
+  [BOTANIX]: "https://botanix-api.gmxinfra2.io",
 
   [ARBITRUM_SEPOLIA]: "https://dolphin-app-a2dup.ondigitalocean.app",
 };
 
 const ORACLE_KEEPER_FALLBACK_URLS: Record<ContractsChainId, string[]> = {
-  [ARBITRUM]: [
-    "https://arbitrum-api-fallback.gmxinfra2.io",
-    // "https://arbitrum-api-fallback.gmxinfra.io",
-  ],
+  [ARBITRUM]: ["https://arbitrum-api-fallback.gmxinfra.io", "https://arbitrum-api-fallback.gmxinfra2.io"],
 
   [AVALANCHE]: ["https://avalanche-api-fallback.gmxinfra.io", "https://avalanche-api-fallback.gmxinfra2.io"],
 
