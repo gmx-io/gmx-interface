dd27ce14d0d28abbb82139e64ced5d7cf36273ac
Hubért de Lalye <hubert-de-lalye@proton.me>
2025-12-12 14:56:08 +0400

    updated sqd urls

--- src/config/indexers.ts
+++ src/config/indexers.ts
@@ -10,7 +10,7 @@ const INDEXER_URLS = {
       "https://api.goldsky.com/api/public/project_cmgptuc4qhclc01rh9s4q554a/subgraphs/gmx-arbitrum-referrals/master-240506225935-51167d5/gn",
     syntheticsStats:
       "https://api.goldsky.com/api/public/project_cmgptuc4qhclc01rh9s4q554a/subgraphs/synthetics-arbitrum-stats/master-250410222518-4486206/gn",
-    subsquid: "https://gmx-test.squids.live/gmx-synthetics-arbitrum@v1/api/graphql",
+    subsquid: "https://gmx.squids.live/gmx-synthetics-arbitrum@ba9869/api/graphql",
   },
 
   [AVALANCHE]: {
@@ -20,7 +20,7 @@ const INDEXER_URLS = {
       "https://api.goldsky.com/api/public/project_cmgptuc4qhclc01rh9s4q554a/subgraphs/gmx-avalanche-referrals/master-240415215829-f6877d6/gn",
     syntheticsStats:
       "https://api.goldsky.com/api/public/project_cmgptuc4qhclc01rh9s4q554a/subgraphs/synthetics-avalanche-stats/master-250410222549-4486206/gn",
-    subsquid: "https://gmx.squids.live/gmx-synthetics-avalanche:prod/api/graphql",
+    subsquid: "https://gmx.squids.live/gmx-synthetics-avalanche@ba9869/api/graphql",
   },
 
   [AVALANCHE_FUJI]: {
@@ -32,11 +32,11 @@ const INDEXER_URLS = {
   },
 
   [ARBITRUM_SEPOLIA]: {
-    subsquid: "https://gmx.squids.live/gmx-synthetics-arb-sepolia:prod/api/graphql",
+    subsquid: "https://gmx.squids.live/gmx-synthetics-arb-sepolia@ba9869/api/graphql",
   },
 
   [BOTANIX]: {
-    subsquid: "https://gmx.squids.live/gmx-synthetics-botanix:prod/api/graphql",
+    subsquid: "https://gmx.squids.live/gmx-synthetics-botanix@ba9869/api/graphql",
     stats:
       "https://api.goldsky.com/api/public/project_cmgptuc4qhclc01rh9s4q554a/subgraphs/synthetics-botanix-stats/botanix-250617091016-f7b3bb5/gn",
     syntheticsStats:
