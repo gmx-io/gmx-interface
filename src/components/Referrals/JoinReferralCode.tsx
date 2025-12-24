53761927eadcce465274358121bf677c36174600
Tim <timgmxio@gmail.com>
2025-12-24 16:11:50 +0300

    FEDEV-3232: Add Version Check to CTA Buttons

--- src/components/Referrals/JoinReferralCode.tsx
+++ src/components/Referrals/JoinReferralCode.tsx
@@ -32,6 +32,7 @@ import { helperToast } from "lib/helperToast";
 import { formatUsd, numberToBigint } from "lib/numbers";
 import { useJsonRpcProvider } from "lib/rpc";
 import { sendWalletTransaction } from "lib/transactions";
+import { useHasOutdatedUi } from "lib/useHasOutdatedUi";
 import { useThrottledAsync } from "lib/useThrottledAsync";
 import useWallet from "lib/wallets/useWallet";
 import { DEFAULT_EXPRESS_ORDER_DEADLINE_DURATION } from "sdk/configs/express";
@@ -249,6 +250,7 @@ function ReferralCodeFormMultichain({
   const debouncedReferralCode = useDebounce(referralCode, 300);
   const settlementChainPublicClient = usePublicClient({ chainId });
   const { tokenChainDataArray: multichainTokens } = useMultichainTradeTokensRequest(chainId, account);
+  const hasOutdatedUi = useHasOutdatedUi();
 
   const simulationSigner = useMemo(() => {
     if (!signer?.provider) {
@@ -539,7 +541,12 @@ function ReferralCodeFormMultichain({
   };
   const isEdit = type === "edit";
 
-  if (isEdit && debouncedReferralCode === userReferralCodeString) {
+  if (hasOutdatedUi) {
+    buttonState = {
+      text: t`Page outdated, please refresh`,
+      disabled: true,
+    };
+  } else if (isEdit && debouncedReferralCode === userReferralCodeString) {
     buttonState = {
       text: t`Same as current active code`,
       disabled: true,
