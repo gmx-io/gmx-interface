07d15f2ec5a3ea9c23af4ab8a14da09112fea488
midas-myth <midas-myth@proton.me>
2025-12-29 15:05:20 +0100

    Enhance deposit button visibility logic by incorporating non-EOA account check

--- src/domain/multichain/useGmxAccountShowDepositButton.tsx
+++ src/domain/multichain/useGmxAccountShowDepositButton.tsx
@@ -1,11 +1,15 @@
 import { useChainId } from "lib/chains";
+import { useIsNonEoaAccountOnAnyChain } from "lib/wallets/useAccountType";
 
 import { useAvailableToTradeAssetSettlementChain } from "components/GmxAccountModal/hooks";
 
 export function useGmxAccountShowDepositButton() {
   const { srcChainId } = useChainId();
   const { gmxAccountUsd, isGmxAccountLoading } = useAvailableToTradeAssetSettlementChain();
-  const shouldShowDepositButton = !isGmxAccountLoading && gmxAccountUsd === 0n && srcChainId !== undefined;
+  const isNonEoaAccountOnAnyChain = useIsNonEoaAccountOnAnyChain();
+
+  const shouldShowDepositButton =
+    !isGmxAccountLoading && gmxAccountUsd === 0n && srcChainId !== undefined && !isNonEoaAccountOnAnyChain;
 
   return { shouldShowDepositButton };
 }
