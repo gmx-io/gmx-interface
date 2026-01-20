0626b1ff79b1134f7314c826dface4eca30a534d
divhead <110596516+divhead@users.noreply.github.com>
2026-01-20 15:44:46 +0000

    fix RawMarketInfo type

--- sdk/src/utils/markets/api.ts
+++ sdk/src/utils/markets/api.ts
@@ -1,11 +1,11 @@
-import { MarketInfo, RawMarketInfo } from "types/markets";
+import { RawMarketInfo } from "types/markets";
 import { TokenData } from "types/tokens";
 import { IHttp } from "utils/http/types";
 import { deserializeBigIntsInObject } from "utils/numbers";
 
 export async function fetchApiMarketsInfo(ctx: { api: IHttp }): Promise<RawMarketInfo[]> {
   const mInfos: any[] = await ctx.api.fetchJson("/markets/info");
-  return mInfos.map((mInfo) => deserializeBigIntsInObject(mInfo, { handleInts: true })) as MarketInfo[];
+  return mInfos.map((mInfo) => deserializeBigIntsInObject(mInfo, { handleInts: true })) as RawMarketInfo[];
 }
 
 export async function fetchApiTokensData(ctx: { api: IHttp }): Promise<TokenData[]> {
