a646e108147d915a63fae3cb9f72db19a938ffee
qwinndev <qwinndev@proton.me>
2026-02-06 14:53:45 +0200

    Fix other buttons in tabs

--- src/components/Tabs/RegularTab.tsx
+++ src/components/Tabs/RegularTab.tsx
@@ -51,6 +51,7 @@ export default function RegularTab<V extends string | number>({
   if (type === "inline-primary") {
     return (
       <Button
+        type="button"
         variant={isActive ? "primary" : "secondary"}
         onClick={() => onOptionClick?.(option.value)}
         key={option.value}
@@ -66,6 +67,7 @@ export default function RegularTab<V extends string | number>({
   if (type === "inline") {
     return (
       <Button
+        type="button"
         variant={isActive ? "secondary" : "ghost"}
         onClick={() => onOptionClick?.(option.value)}
         key={option.value}
@@ -82,6 +84,7 @@ export default function RegularTab<V extends string | number>({
 
   return (
     <button
+      type="button"
       className={cx(
         `-mb-[0.5px] flex items-baseline justify-center gap-8 border-b-[2.5px] border-b-[transparent] px-20 pb-9 pt-11
         font-medium first:rounded-tl-8 last:rounded-tr-8 hover:text-typography-primary`,
