fca6d96c9aed8e8ff15d639fd1695138d93ac1d6
divhead <110596516+divhead@users.noreply.github.com>
2026-03-11 12:17:50 +0000

    support api methods

--- sdk/src/utils/markets/api.ts
+++ sdk/src/utils/markets/api.ts
@@ -2,7 +2,7 @@ import { IHttp } from "utils/http/types";
 import { deserializeBigIntsInObject } from "utils/numbers";
 import { TokenData } from "utils/tokens/types";
 
-import { RawMarketInfo } from "./types";
+import { MarketTicker, MarketWithTiers, RawMarketInfo } from "./types";
 
 export async function fetchApiMarketsInfo(ctx: { api: IHttp }): Promise<RawMarketInfo[]> {
   const mInfos: any[] = await ctx.api.fetchJson("/markets/info");
@@ -12,4 +12,22 @@ export async function fetchApiMarketsInfo(ctx: { api: IHttp }): Promise<RawMarke
 export async function fetchApiTokensData(ctx: { api: IHttp }): Promise<TokenData[]> {
   const tInfos: any[] = await ctx.api.fetchJson("/tokens/info");
   return tInfos.map((tInfo) => deserializeBigIntsInObject(tInfo, { handleInts: true })) as TokenData[];
+}
+
+export async function fetchApiMarkets(ctx: { api: IHttp }): Promise<MarketWithTiers[]> {
+  const markets: any[] = await ctx.api.fetchJson("/markets");
+  return markets.map((m) => deserializeBigIntsInObject(m, { handleInts: true })) as MarketWithTiers[];
+}
+
+export async function fetchApiMarketsTickers(
+  ctx: { api: IHttp },
+  params?: { addresses?: string[]; symbols?: string[] }
+): Promise<MarketTicker[]> {
+  const tickers: any[] = await ctx.api.fetchJson("/markets/tickers", {
+    query: {
+      addresses: params?.addresses,
+      symbols: params?.symbols,
+    },
+  });
+  return tickers.map((t) => deserializeBigIntsInObject(t, { handleInts: true })) as MarketTicker[];
 }
