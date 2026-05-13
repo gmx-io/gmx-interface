4c98f7495383be23a1cc4fa49c9342cd8ce70de7
qwinndev <qwinndev@proton.me>
2026-05-13 17:31:24 +0200

    add local dev privy app id

--- src/config/privy.ts
+++ src/config/privy.ts
@@ -1,1 +1,4 @@
-export const PRIVY_APP_ID = "cmov9f3bn00ga0bkwnxy3k96v";
+const PRIVY_PROD_APP_ID = "cmov9f3bn00ga0bkwnxy3k96v";
+const PRIVY_LOCAL_DEV_APP_ID = "cmp46in9h014m0cl5uajtozex";
+
+export const PRIVY_APP_ID = import.meta.env.DEV ? PRIVY_LOCAL_DEV_APP_ID : PRIVY_PROD_APP_ID;
