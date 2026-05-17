281876127e9fce0001ad614eece9bc028a713a11
divhead <110596516+divhead@users.noreply.github.com>
2026-05-17 20:11:33 +0000

    cleanup

--- src/domain/multichain/estimateMultichainDepositNetworkComposeGas.ts
+++ src/domain/multichain/estimateMultichainDepositNetworkComposeGas.ts
@@ -1,29 +1,16 @@
-import { Address } from "abitype";
-import { EstimateContractGasParameters, PublicClient, StateOverride, toHex, zeroAddress, zeroHash } from "viem";
+import { PublicClient } from "viem";
 
-import type { ContractsChainId, SettlementChainId, SourceChainId } from "config/chains";
-import { tryGetContract } from "config/contracts";
-import { FAKE_INPUT_AMOUNT_MAP, getLayerZeroEndpointId, getStargatePoolAddress } from "config/multichain";
+import type { ContractsChainId, SourceChainId } from "config/chains";
+import { FAKE_INPUT_AMOUNT_MAP, getStargatePoolAddress } from "config/multichain";
 import { PLATFORM_TOKEN_DECIMALS } from "context/PoolsDetailsContext/selectors";
 import { isGlvAddress } from "domain/synthetics/markets/glv";
 import { expandDecimals } from "lib/numbers";
-import { abis } from "sdk/abis";
 import { isMarketTokenAddress } from "sdk/configs/markets";
 import { convertTokenAddress, getToken, isValidTokenSafe } from "sdk/configs/tokens";
-import { composeDepositMessage, encodeOftComposeMsg, getLzEndpoint } from "sdk/utils/multichain/codecs";
 import { estimateMultichainDepositComposeGas as estimateSdk } from "sdk/utils/multichain/estimateComposeGas";
-import { OVERRIDE_ERC20_BYTECODE, RANDOM_SLOT } from "sdk/utils/multichain/stateOverrides";
 import { createViemRpc } from "sdk/utils/rpc/createViemRpc";
 
 import { CodecUiHelper, MultichainAction } from "./codecs/CodecUiHelper";
-
-export type EstimateMultichainDepositNetworkComposeGasParameters = {
-  action?: MultichainAction;
-  chainId: ContractsChainId;
-  account: string;
-  srcChainId: SourceChainId;
-  tokenAddress: string;
-};
 
 function resolveFakeAmount(chainId: ContractsChainId, unwrappedTokenAddress: string): bigint {
   if (isValidTokenSafe(chainId, unwrappedTokenAddress)) {
@@ -34,65 +21,6 @@ function resolveFakeAmount(chainId: ContractsChainId, unwrappedTokenAddress: str
     return expandDecimals(10, PLATFORM_TOKEN_DECIMALS);
   }
   return 10n ** 18n;
-}
-
-export function getEstimateMultichainDepositNetworkComposeGasParameters({
-  action,
-  chainId,
-  account,
-  srcChainId,
-  tokenAddress: maybeWrappedTokenAddress,
-}: EstimateMultichainDepositNetworkComposeGasParameters): EstimateContractGasParameters<
-  typeof abis.LayerZeroProvider,
-  "lzCompose"
-> {
-  const unwrappedTokenAddress = convertTokenAddress(chainId, maybeWrappedTokenAddress, "native");
-
-  const innerData = action ? CodecUiHelper.encodeMultichainComposeActionData(action) : undefined;
-  const composeFromWithMsg = composeDepositMessage(chainId as SettlementChainId, account, innerData);
-
-  const sourceChainEndpointId = getLayerZeroEndpointId(srcChainId);
-  if (!getLayerZeroEndpointId(chainId)) {
-    throw new Error("Stargate endpoint ID not found");
-  }
-  if (!sourceChainEndpointId) {
-    throw new Error("Stargate endpoint ID not found");
-  }
-
-  const fakeAmount = resolveFakeAmount(chainId, unwrappedTokenAddress);
-
-  const message = encodeOftComposeMsg(0n, sourceChainEndpointId, fakeAmount, composeFromWithMsg);
-
-  const stargatePool = getStargatePoolAddress(chainId, unwrappedTokenAddress);
-  if (!stargatePool) {
-    throw new Error(`Stargate pool not found for token: ${unwrappedTokenAddress} on chain: ${chainId}`);
-  }
-
-  const address = tryGetContract(chainId, "LayerZeroProvider");
-  if (!address) {
-    throw new Error("LayerZero provider not found");
-  }
-
-  const stateOverride: StateOverride = [];
-
-  if (unwrappedTokenAddress !== zeroAddress) {
-    stateOverride.push({
-      address: unwrappedTokenAddress as Address,
-      code: OVERRIDE_ERC20_BYTECODE,
-      stateDiff: [{ slot: RANDOM_SLOT, value: zeroHash }],
-    });
-  } else {
-    stateOverride.push({ address, balance: fakeAmount * 2n });
-  }
-
-  return {
-    address,
-    abi: abis.LayerZeroProvider,
-    functionName: "lzCompose",
-    args: [stargatePool, toHex(0, { size: 32 }), message, zeroAddress, "0x"],
-    account: getLzEndpoint(chainId),
-    stateOverride,
-  };
 }
 
 export async function estimateMultichainDepositNetworkComposeGas({
