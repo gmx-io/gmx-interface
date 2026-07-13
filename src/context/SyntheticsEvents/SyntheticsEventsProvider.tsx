3d44c0e58dcc19837b0ff2c02e06d452495b6eae
qwinndev <qwinndev@proton.me>
2026-07-13 11:34:48 +0200

    Move order metadata to dataList FEDEV-3983

--- src/context/SyntheticsEvents/SyntheticsEventsProvider.tsx
+++ src/context/SyntheticsEvents/SyntheticsEventsProvider.tsx
@@ -58,7 +58,7 @@ import { TokenApproveResultEvent } from "lib/userAnalytics/types";
 import useWallet from "lib/wallets/useWallet";
 import { getToken, getWrappedToken, NATIVE_TOKEN_ADDRESS } from "sdk/configs/tokens";
 import { StatusCode } from "sdk/utils/gelatoRelay";
-import { decodeTwapUiFeeReceiver } from "sdk/utils/twap/uiFeeReceiver";
+import { decodeOrderTwapParams } from "sdk/utils/twap/uiFeeReceiver";
 
 import { getInsufficientExecutionFeeToastContent, InvalidSignatureToastContent } from "components/Errors/errorToasts";
 import { FeesSettlementStatusNotification } from "components/StatusNotification/FeesSettlementStatusNotification";
@@ -194,7 +194,7 @@ export function SyntheticsEventsProvider({ children }: { children: ReactNode }) 
       updateNativeTokenBalance();
 
       const uiFeeReceiver = eventData.addressItems.items.uiFeeReceiver;
-      const twapParams = decodeTwapUiFeeReceiver(uiFeeReceiver);
+      const twapParams = decodeOrderTwapParams(eventData.bytes32Items.arrayItems.dataList, uiFeeReceiver);
 
       const data: OrderCreatedEventData = {
         account: eventData.addressItems.items.account,
@@ -1232,9 +1232,7 @@ export function SyntheticsEventsProvider({ children }: { children: ReactNode }) 
 
   const [multichainTransferProgress, setMultichainTransferProgress] = useState<
     MultichainTransferProgress<string> | undefined
-  >(
-    undefined
-  );
+  >(undefined);
 
   useMultichainTransferProgressView(multichainTransferProgress);
 
