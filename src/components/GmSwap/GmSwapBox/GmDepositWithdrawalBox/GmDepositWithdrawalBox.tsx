e1bdb5efdd5ad1c741e832df1dbddb6ea5b0b38a
Timofei Domnikov <timgmxio@gmail.com>
2026-09-23 01:28:07 +0300

    Add Pay and Receive modal titles to pool asset selectors PRO-4065

--- src/components/GmSwap/GmSwapBox/GmDepositWithdrawalBox/GmDepositWithdrawalBox.tsx
+++ src/components/GmSwap/GmSwapBox/GmDepositWithdrawalBox/GmDepositWithdrawalBox.tsx
@@ -503,6 +503,7 @@ export function GmSwapBoxDepositWithdrawal() {
                   {firstTokenAddress && isSingle && isDeposit ? (
                     <MultichainTokenSelectorForLp
                       chainId={chainId}
+                      label={t`Pay`}
                       tokenAddress={firstTokenAddress}
                       payChainId={
                         paySource === "gmxAccount"
@@ -534,6 +535,7 @@ export function GmSwapBoxDepositWithdrawal() {
                   ) : isWithdrawal && firstTokenAddress && isSingle && tokenOptions.length > 1 ? (
                     <TokenSelector
                       chainId={chainId}
+                      label={t`Receive`}
                       tokenAddress={firstTokenAddress}
                       onSelectToken={(token) => {
                         handleFirstTokenSelect(token.address as ERC20Address | NativeTokenSupportedAddress);
