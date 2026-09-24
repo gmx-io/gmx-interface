f527c05542848af726525ad042a9c924cf104b64
qwinndev <qwinndev@proton.me>
2026-09-24 13:48:28 +0300

    Update homepage spheres and hero glow PRO-4028

--- landing/src/pages/Home/RewardsSection/RewardsOrbit.tsx
+++ landing/src/pages/Home/RewardsSection/RewardsOrbit.tsx
@@ -7,7 +7,8 @@ import innerOrbit from "img/rewards-landing/promo-orbit-inner.svg";
 import middleOrbit from "img/rewards-landing/promo-orbit-middle.svg";
 import outerOrbit from "img/rewards-landing/promo-orbit-outer.svg";
 
-const MULTIPLIERS = [10, 3, 8, 4, 5, 10, 6, 4, 7];
+const MULTIPLIERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const STARTING_PHASE = 0.84;
 
 export function RewardsOrbit() {
   const orbitRef = useRef<HTMLDivElement>(null);
@@ -21,9 +22,9 @@ export function RewardsOrbit() {
       <img className="home-rewards-orbit-outer" src={outerOrbit} alt="" />
       {MULTIPLIERS.map((multiplier, index) => (
         <span
-          key={index}
+          key={multiplier}
           className="home-rewards-sphere"
-          style={{ "--sphere-phase": index / MULTIPLIERS.length } as CSSProperties}
+          style={{ "--sphere-phase": (STARTING_PHASE + index / MULTIPLIERS.length) % 1 } as CSSProperties}
         >
           <span>{multiplier}x</span>
         </span>
